ff5f74b18955ef495fbf38fda88352a374b71429
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-03-10 17:50:05 +0000

    Adds bindings for images
    * Closes #15

--- python/__init__.py
+++ python/__init__.py
@@ -1,4 +1,5 @@
 """ Purify package """
 __docformat__ = "restructuredtext en"
-__all__ = ['read_visibility']
+__all__ = ['read_visibility', 'Image']
 from .visibility import read_visibility
+from .image import Image
