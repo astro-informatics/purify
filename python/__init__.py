3033f60bc51912aee5124e781d30db8d8b607be7
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-03-13 15:40:29 +0000

    Move functors.pyx to measurements.pyx

--- python/__init__.py
+++ python/__init__.py
@@ -3,4 +3,4 @@ __docformat__ = "restructuredtext en"
 __all__ = ['read_visibility', 'Image', 'kernels']
 from .visibility import read_visibility
 from .image import Image
-from .functor import kernels
+from .measurements import kernels
