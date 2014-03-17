34a642045c055a16c0d44d2aa4a836800ca02287
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-03-17 11:46:40 +0000

    Adding parameter structures for solvers
    Parameter structures are currently mostly equivalent to the existing C structures.
    The parameters themselves are traits.

---
+++ python/params.py
@@ -0,0 +1,131 @@
+""" Defines parameters mix-in classes
+
+    Parameters are defined as *traits*. This means that they can be set to only a subset of possible
+    values, rather than just any python object. For instance, acceptable values for a tolerance
+    criteria might be positive real numbers. Anything else should raise an expection. In a way,
+    *traits* are akin to types in C. 
+
+    The parameters are grouped in a way to reflect the C parameter structures.
+"""
+__all__ = ['ConjugateGradient', 'SDMM', 'RW', 'TVProx']
+__docformat__ = "restructuredtext en"
+
+def verbosity():
+    """ Defines a verbosity property.
+
+        There are three levels of verbosity, 'off', 'medium', and 'high'. 
+    """
+    doc = """ (off|0|None|False) | (summary|medium|1|True) | (convergence|high|2) """ 
+    def get(self):
+        return ['off', 'medium', 'high'][self._verbose]
+    def set(self, value):
+        if hasattr(value, 'lower'): value = value.lower()
+
+        if value in [0, None, 'off', 'none']: value = 0
+        elif value in [1, 'summary', 'medium']: value = 1
+        elif value in [2, 'convergence', 'high']: value = 2
+        else: raise ValueError("Verbose can be one of off, medium, high")
+        self._verbose = value
+    return property(get, set, doc=doc)
+
+def _set_positive(self, value, name, cast):
+    value = cast(value)
+    if value <= 0: raise ValueError("%s cannot be negative or null" % name)
+    setattr(self, "_%s" % name, value)
+
+def positive_number(name, cast, doc=None):
+    """ returns property for strictly positive numbers """
+    return property(
+        lambda x: getattr(x, "_%s" % name),
+        lambda x, v: _set_positive(x, v, name, cast),
+        doc
+    )
+def positive_real(name, doc=None):
+    """ Parameter must be a strictly positive real number """
+    return positive_number(name, float, doc)
+def positive_int(name, doc=None):
+    """ Parameter must be a strictly positive integer number """
+    return positive_number(name, int, doc)
+def positive_real_or_none(name, doc):
+    """ A property that can be a positive real number, or None
+
+        None can be used to indicate the property should be computed somehow.
+    """
+    _name = "_%s" % name
+    return property(
+        lambda x: getattr(x, _name),
+        lambda x, v: setattr(x, _name, None) if v is None else _set_positive(x, v, name, float),
+        doc
+    )
+
+def boolean(name, doc=None):
+    """ A boolean property """
+    return property(
+        lambda x: getattr(x, "_%s" % name),
+        lambda x, v: setattr(x, "_%s" % name, True if v else False), 
+        doc = doc
+    )
+
+class ConjugateGradient(object):
+    """ Contains conjugate gradient parameters. """
+    max_iter = positive_int('max_iter', "Maximum number of Conjugate-gradient iterations")
+    tolerance = positive_real('radius', "Conjugate gradient convergence criteria")
+
+    def __init__(self, max_iter=100, tolerance=1e-6):
+        self.max_iter = max_iter
+        self.tolerance = tolerance
+
+class TVProx(object):
+    verbose = verbosity()
+    max_iter = positive_int('max_iter', "Maximum number of iterations")
+    relative_variation = positive_real( 'relative_variation',
+                                        "Acceptable relative variation in the solution" )
+    def __init__(self, verbose='high', max_iter=50, relative_variation=1e-4):
+        self.verbose = verbose
+        self.max_iter = max_iter
+        self.relative_variation = relative_variation
+
+class SDMM(TVProx):
+    """ SDMM related parameters """
+
+    gamma = positive_real_or_none('gamma', "SDMM convergence criteria")
+    radius = positive_real('radius', "L2 ball radius")
+    relative_radius = positive_real('relative_radius', "Defines range of the radius")
+
+    def __init__( self, verbose='high', max_iter=300, gamma=None, relative_variation=1e-3,
+                  radius=None, relative_radius=1e-2, cg_max_iter=100, cg_tolerance=1e-6 ):
+
+        super(SDMM, self).__init__( verbose=verbose, max_iter=max_iter,
+                                    relative_variation=relative_variation )
+        self.gamma = gamma
+        self.radius = radius
+        self.relative_radius = relative_radius
+        self.cg = ConjugateGradient(max_iter=cg_max_iter, tolerance=cg_tolerance)
+        """ Conjugate gradient parameters """
+
+class RW(TVProx):
+    """ Reweighted L1 optimization problem """
+
+    sigma = positive_real_or_none( 'sigma', 
+                                   "Standard deviation of the noise in the representation domain" )
+    warm_start = boolean('warm_start', "If True, use provided starting point")
+
+    def __init__( self, verbose='high', max_iter=5, sigma=None, relative_variation=1e-3,
+                  warm_start=True ):
+        super(RW, self).__init__( verbose=verbose, max_iter=max_iter,
+                                  relative_variation=relative_variation )
+        self.sigma = sigma
+        self.warm_start = warm_start
+
+
+class TV_SDMM(SDMM):
+    """ Parameters for the TV optimisation problem using SDMM """
+    def __init__( self, verbose='high', max_iter=300, gamma=None, relative_variation=1e-3,
+                  radius=None, relative_radius=1e-2, cg_max_iter=100, cg_tolerance=1e-6,
+                  tv_verbose='high', tv_max_iter=50, tv_relative_variation=1e-4):
+        super(TV_SDMM, self).__init__( verbose=verbose, max_iter=max_iter, gamma=gamma,
+                                       relative_variation=relative_variation, radius=radius,
+                                       relative_radius=relative_radius, cg_max_iter=cg_max_iter,
+                                       cg_tolerance=cg_tolerance )
+        self.tv = TVProx( verbose=tv_verbose, max_iter=tv_max_iter,
+                          relative_variation=tv_relative_variation )
---
+++ python/tests/test_params.py
@@ -0,0 +1,152 @@
+""" Test parameters """
+def assert_positive_integer(object, name):
+    from nose.tools import assert_equal, assert_raises
+
+    setattr(object, name, 2) 
+    assert_equal(getattr(object, name), 2)
+    
+    setattr(object, name, 1.4)
+    assert_equal(getattr(object, name), 1)
+    
+    with assert_raises(ValueError): setattr(object, name, 0)
+    with assert_raises(ValueError): setattr(object, name, -1)
+
+def assert_positive_real(object, name):
+    from nose.tools import assert_almost_equal, assert_raises
+    
+    setattr(object, name, 2) 
+    assert_almost_equal(getattr(object, name), 2)
+    
+    setattr(object, name, 1e-2)
+    assert_almost_equal(getattr(object, name), 1e-2)
+    
+    with assert_raises(ValueError): setattr(object, name, 0)
+    with assert_raises(ValueError): setattr(object, name, -1)
+
+def assert_positive_real_or_none(object, name):
+    from nose.tools import assert_true
+    
+    assert_positive_real(object, name)
+    setattr(object, name, None)
+    assert_true(getattr(object, name) is None)
+
+def assert_verbosity(param):
+    from nose.tools import assert_equal, assert_raises
+    for key, values in { 'off': [0, None, 'oFf', False],
+                         'medium': [1, 'meDium', 'sUMMary', True],
+                         'high': [2, 'high', 'convergence'] }.iteritems():
+        for value in values:
+            param.verbose = value
+            assert_equal(param.verbose, key)
+
+    with assert_raises(ValueError): param.verbose = 'med'
+    with assert_raises(ValueError): param.verbose = 0.1
+
+def assert_boolean(param, name):
+    from nose.tools import assert_true
+
+    setattr(param, name, True) 
+    assert_true(getattr(param, name) is True)
+    setattr(param, name, "not empty") 
+    assert_true(getattr(param, name) is True)
+
+    setattr(param, name, 0) 
+    assert_true(getattr(param, name) is False)
+    setattr(param, name, "") 
+    assert_true(getattr(param, name) is False)
+
+def assert_tvprox(param): 
+    assert_verbosity(param)
+    assert_positive_integer(param, 'max_iter')
+    assert_positive_real(param, 'relative_variation')
+
+def assert_sdmm(param):
+    assert_tvprox(param)
+    assert_positive_real_or_none(param, 'gamma')
+    assert_positive_real(param, 'relative_radius')
+    assert_positive_real(param, 'radius')
+    assert_positive_integer(param.cg, 'max_iter')
+    assert_positive_real(param.cg, 'tolerance')
+
+def test_cg():
+    from nose.tools import assert_equal, assert_almost_equal
+    from purify.params import ConjugateGradient
+ 
+    cg = ConjugateGradient(10, 1e-3)
+    assert_equal(cg.max_iter, 10)
+    assert_almost_equal(cg.tolerance, 1e-3)
+ 
+    assert_positive_integer(cg, "max_iter")
+    assert_positive_real(cg, "tolerance")
+
+def test_sdmm():
+    from nose.tools import assert_equal, assert_almost_equal
+    from purify.params import SDMM
+
+    param = SDMM( verbose='medium', max_iter=30, gamma=1e-2, relative_variation=1e-4,
+                  radius=1e-2, relative_radius=1e-4, cg_max_iter=10, cg_tolerance=1e-4 )
+
+    assert_equal(param.verbose, 'medium')
+    assert_equal(param.max_iter, 30)
+    assert_almost_equal(param.gamma, 1e-2)
+    assert_almost_equal(param.relative_variation, 1e-4)
+    assert_almost_equal(param.radius, 1e-2)
+    assert_almost_equal(param.relative_radius, 1e-4)
+    assert_equal(param.cg.max_iter, 10)
+    assert_almost_equal(param.cg.tolerance, 1e-4)
+
+    assert_sdmm(param)
+
+def test_rw():
+    from nose.tools import assert_equal, assert_almost_equal, assert_true
+    from purify.params import RW
+
+    param = RW( verbose='medium', max_iter=30, sigma=1e-2, relative_variation=1e-4,
+                warm_start=False)
+
+    assert_equal(param.verbose, 'medium')
+    assert_equal(param.max_iter, 30)
+    assert_almost_equal(param.sigma, 1e-2)
+    assert_almost_equal(param.relative_variation, 1e-4)
+    assert_true(not param.warm_start)
+
+    assert_tvprox(param)
+    assert_positive_real_or_none(param, 'sigma')
+    assert_boolean(param, 'warm_start')
+
+
+def test_tvprox():
+    from nose.tools import assert_equal, assert_almost_equal
+    from purify.params import TVProx
+
+    param = TVProx(verbose='medium', max_iter=30, relative_variation=1e-4)
+    assert_equal(param.verbose, 'medium')
+    assert_equal(param.max_iter, 30)
+    assert_almost_equal(param.relative_variation, 1e-4)
+
+    assert_tvprox(param)
+
+def test_tv_sdmm():
+
+    from nose.tools import assert_equal, assert_almost_equal
+    from purify.params import TV_SDMM
+
+    param = TV_SDMM( verbose='medium', max_iter=30, gamma=1e-2, relative_variation=1e-4,
+                     radius=1e-2, relative_radius=1e-4, cg_max_iter=10, cg_tolerance=1e-4,
+                     tv_verbose='medium', tv_relative_variation=1e-4, tv_max_iter=30 )
+
+    assert_equal(param.verbose, 'medium')
+    assert_equal(param.max_iter, 30)
+    assert_almost_equal(param.gamma, 1e-2)
+    assert_almost_equal(param.relative_variation, 1e-4)
+    assert_almost_equal(param.radius, 1e-2)
+    assert_almost_equal(param.relative_radius, 1e-4)
+    assert_equal(param.cg.max_iter, 10)
+    assert_almost_equal(param.cg.tolerance, 1e-4)
+
+    assert_equal(param.tv.verbose, 'medium')
+    assert_equal(param.tv.max_iter, 30)
+    assert_almost_equal(param.tv.relative_variation, 1e-4)
+
+    assert_sdmm(param)
+    assert_tvprox(param.tv)
