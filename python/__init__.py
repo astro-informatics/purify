36defeee41a90dae8042d47d98e85852d29b37d0
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-03-19 17:27:21 +0000

    Function to read images
    Sets images into top left corner of an image with height and width a power of
    two.

--- python/__init__.py
+++ python/__init__.py
@@ -1,7 +1,30 @@
 """ Purify package """
 __docformat__ = "restructuredtext en"
-__all__ = ['read_visibility', 'Image', 'kernels', 'MeasurementOperator', 'SparsityOperator']
+__all__ = ['read_visibility', 'Image', 'kernels', 'MeasurementOperator',
+           'SparsityOperator', 'read_image']
 from .visibility import read_visibility
 from .image import Image
 from .measurements import kernels, MeasurementOperator
 from .sparsity_ops import SparsityOperator
+
+
+def read_image(path, power_of_two=True):
+    """ Reads an image from disk
+
+        Parameters:
+            path: str
+                Path to image file
+            power_of_two: boolean
+                If true, then image size is a power of two. Actual image is in
+                top left corner (0:n, 0:m range)
+    """
+    from numpy import zeros, log, array, ceil
+    image = Image(path).pixels
+    if power_of_two:
+        shape = array(image.shape).astype('double')
+        shape = 2**ceil(log(shape) / log(2.0))
+        shape = tuple(shape)
+        topleft = zeros(shape, dtype=image.dtype)
+        topleft[:image.shape[0], :image.shape[1]] = image
+        image = topleft
+    return topleft
