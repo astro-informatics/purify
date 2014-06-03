f27d78ebb891604c4446f8382f98b6560411954d
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-06-03 09:51:39 +0100

    Simplifies guessing of parameters

--- python/purify.cparams.pyx
+++ python/purify.cparams.pyx
@@ -4,6 +4,35 @@ cdef double _default_sigma(measurements) except *:
     nvis = float(len(measurements))
     return norm(measurements) * 10.0**-(1.5) / sqrt(nvis)
 
+cdef double guess_gamma_penalty(
+        sdmm, visibility, MeasurementOperator measurements ):
+    """ Evaluates the convergence criteria from the input """
+    from numpy import max, product, sqrt
+    xout = measurements.adjoint(visibility)
+    cdef double nvis = float(len(visibility))
+    cdef double npixels = product(measurements.sizes.image)
+    cdef double scale = sqrt(npixels / nvis)
+    cdef:
+        double gamma = max((xout if sdmm.tv_norm else sdmm.analyze(xout)).real)
+    return  gamma * 1e-3 * scale
+
+cdef double guess_radius( sdmm, visibility,
+                MeasurementOperator measurements ):
+    from numpy import product, sqrt
+    cdef double nvis = float(len(visibility))
+    cdef double npixels = product(measurements.sizes.image)
+    cdef double scale = sqrt(npixels / nvis)
+    cdef double sigma = _default_sigma(visibility)
+    return sqrt(nvis + 2.0 * sqrt(nvis)) * sigma
+
+cdef double guess_sigma( sdmm, visibility,
+                MeasurementOperator measurements ):
+    from numpy import product, sqrt
+    cdef double nvis = float(len(visibility))
+    cdef double npixels = product(measurements.sizes.image)
+    return _default_sigma(visibility) \
+            * nvis / npixels / sqrt(len(float(sdmm)))
+
 cdef void _convert_prox_tvparam(sopt_prox_tvparam* c_params, pyinput):
     """ Sets c parameters from python object """
     c_params.verbose = {'off': 0, 'medium': 1, 'high': 2}[pyinput.verbose]
@@ -16,28 +45,19 @@ cdef void _convert_l1param( sopt_l1_sdmmparam* c_params, sdmm,
     """ Sets c parameters from python object """
     from numpy import max, product, sqrt
     from numpy.linalg import norm
-    _convert_prox_tvparam(<sopt_prox_tvparam*>c_params, sdmm)
-    c_params.rel_obj = c_params.gamma # Order changes depending on structure
-    nelements = product(measurements.sizes.image)
-    if sdmm.gamma is not None: c_params.gamma = sdmm.gamma
-    else:
-        scale = sqrt(nelements) / sqrt(float(len(visibility)))
-        xout = measurements.adjoint(visibility)
-        if sdmm.tv_norm: c_params.gamma = 1e-3 * max(xout.real) * scale
-        else: c_params.gamma = 1e-3 * max(sdmm.analyze(xout).real) * scale
-    if sdmm.radius is not None: c_params.epsilon = sdmm.radius
-    else:
-        nvis = float(len(visibility))
-        sigma = _default_sigma(visibility) / scale
-        c_params.epsilon = sqrt(nvis + 2.0 * sqrt(nvis)) * sigma \
-                           / nvis * nelements
-
-    c_params.epsilon_tol = sdmm.relative_radius
-    c_params.cg_max_iter = sdmm.cg.max_iter
-    c_params.cg_tol = sdmm.cg.tolerance
     if visibility.dtype != 'complex':
         raise NotImplemented('Cannot purify real visibilities')
-    c_params.real_data = 0
+
+    _convert_prox_tvparam(<sopt_prox_tvparam*>c_params, sdmm)
+
+    c_params.gamma = sdmm.gamma if sdmm.gamma is not None \
+        else guess_gamma_penalty(sdmm, visibility, measurements)
+    c_params.epsilon = sdmm.radius if sdmm.radius is not None \
+        else guess_radius(sdmm, visibility, measurements)
+    c_params.epsilon_tol = sdmm.relative_radius
+    c_params.cg_max_iter = sdmm.cg.max_iter
+    c_params.cg_tol      = sdmm.cg.tolerance
+    c_params.real_data   = 0
 
 cdef void _convert_rwparams( sopt_l1_rwparam *c_params, sdmm,
         MeasurementOperator measurements, visibility ) except *:
@@ -48,12 +68,7 @@ cdef void _convert_rwparams( sopt_l1_rwparam *c_params, sdmm,
     c_params.init_sol = 1 if sdmm.rw.warm_start else 0
     if sdmm.rw.sigma is not None: c_params.sigma = sdmm.rw.sigma
     elif c_params.init_sol == 1: c_params.sigma = 0
-    else:
-        scale = sqrt(float(product(measurements.sizes.image)) / float(len(visibility)))
-        nelements = float(product(measurements.sizes.image))
-        nvis = float(len(visibility))
-        c_params.sigma = _default_sigma(visibility) \
-                * nvis / nelements / sqrt(len(float(sdmm)))
+    else: c_params.sigma = guess_sigma(sdmm, visibility, measurements)
 
 cdef void _convert_tvparam( sopt_tv_sdmmparam* c_params, sdmm,
                             MeasurementOperator measurements,
