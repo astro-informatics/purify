45a9132332e3f034c68a234b554f41ae60f9c573
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-10-31 16:21:24 +0000

    Version bump by patch number
    Also PEP-8ed the setup.py file.
    And made casa install not fail if buildtask does not work.

--- setup.py
+++ setup.py
@@ -14,8 +14,10 @@ package_dir = join(source_dir, 'pkg_install')
 long_description = open(join(dirname(__file__), 'README.rst'), 'r').read()
 mkpath(package_dir)
 
+
 def cmake_cache_line(variable, value, type='STRING'):
     return "set(%s \"%s\" CACHE %s \"\")\n" % (variable, value, type)
+
 
 def as_preload_file(name, info):
     """ Python information to cmake commandline """
@@ -23,14 +25,17 @@ def as_preload_file(name, info):
     if len(info.get('libraries', [])):
         libs = ('-l' + '-l'.join(info['libraries'])).rstrip().lstrip()
         if len(info.get('library_dirs', [])):
-            libdirs =  ('-L' + '-L'.join(info['library_dirs'])).rstrip().lstrip()
-        else: libdirs = ""
-        result.append(cmake_cache_line("%s_LIBRARIES" % name,
-            "%s %s" %(libdirs, libs)))
+            libdirs = ('-L' + '-L'.join(info['library_dirs']))\
+                .rstrip().lstrip()
+        else:
+            libdirs = ""
+        result.append(
+            cmake_cache_line("%s_LIBRARIES" % name, "%s %s" % (libdirs, libs)))
     if len(info.get('include_dirs', [])):
         incs = ';'.join(info['include_dirs']).rstrip().lstrip()
         result.append(cmake_cache_line("%s_INCLUDE_DIRS" % name, incs))
     return result
+
 
 def cmake_executable():
     """ Path to cmake executable """
@@ -46,7 +51,7 @@ def cmake_executable():
             join(environ['HOME'], 'bin'),
             join(environ['HOME'], '.local', 'bin'),
             join(environ['HOME'], 'usr', 'bin'),
-            join('/', 'sw', 'bin') # -- default Fink location
+            join('/', 'sw', 'bin')  # -- default Fink location
         ]
         for directory in directories:
             if exists(join(directory, 'cmake')):
@@ -56,14 +61,15 @@ def cmake_executable():
         raise RuntimeError('Could not find cmake executable in path')
     return cmake
 
+
 class Build(dBuild):
     """ Build that runs cmake. """
 
     def configure_cmdl(self, filename):
         """ Creates cmake command-line
 
-            First puts variables into a cache file. This is safer that going through the
-            command-line.
+            First puts variables into a cache file. This is safer that going
+            through the command-line.
         """
         from sys import executable
         # other args
@@ -74,7 +80,8 @@ class Build(dBuild):
             '\n',
         ]
 
-        with open(filename, 'w') as file: file.writelines(other_args)
+        with open(filename, 'w') as file:
+            file.writelines(other_args)
         return ['-C%s' % filename]
 
     def _configure(self, build_dir):
@@ -85,15 +92,16 @@ class Build(dBuild):
         mkpath(build_dir)
         command_line = self.configure_cmdl(join(build_dir, 'Variables.cmake'))
         log.info(
-                "CMake: configuring with variables in %s "
-                % join(build_dir, 'Variables.cmake')
+            "CMake: configuring with variables in %s "
+            % join(build_dir, 'Variables.cmake')
         )
         cmake = cmake_executable()
 
         try:
             chdir(build_dir)
             spawn([cmake] + command_line + [source_dir])
-        finally: chdir(current_dir)
+        finally:
+            chdir(current_dir)
 
     def _build(self, build_dir):
         from distutils import log
@@ -106,7 +114,8 @@ class Build(dBuild):
         try:
             chdir(build_dir)
             spawn([cmake, '--build', '.'])
-        finally: chdir(current_dir)
+        finally:
+            chdir(current_dir)
 
     def run(self):
         from os.path import abspath
@@ -120,7 +129,8 @@ class Build(dBuild):
             self.distribution.running_binary = True
             self.distribution.have_run['egg_info'] = 0
             dBuild.run(self)
-        finally: self.distribution.running_binary = prior
+        finally:
+            self.distribution.running_binary = prior
 
     def _install(self, build_dir, install_dir):
         from distutils import log
@@ -134,13 +144,16 @@ class Build(dBuild):
         log.info("CMake: Installing package to %s" % pkg)
         try:
             chdir(build_dir)
-            self.spawn([cmake,
+            self.spawn([
+                cmake,
                 '-DPYTHON_PKG_DIR=\'%s\'' % pkg,
                 '-DLIBRARY_INSTALL_PATH=\'%s\'' % clib,
                 source_dir
             ])
             self.spawn([cmake, '--build', '.', '--target', 'install'])
-        finally: chdir(current_cwd)
+        finally:
+            chdir(current_cwd)
+
 
 class Install(dInstall):
     def run(self):
@@ -156,46 +169,57 @@ class Install(dInstall):
         log.info("CMake: Installing package to %s" % pkg)
         try:
             chdir(build_dir)
-            self.spawn([cmake,
+            self.spawn([
+                cmake,
                 '-DCMAKE_INSTALL_PREFIX=\'%s\'' % prefix,
                 '-DPYTHON_PKG_DIR=\'%s\'' % pkg,
                 '-DLIBRARY_INSTALL_PATH=\'%s\'' % clib,
                 '..'
             ])
             self.spawn([cmake, '--build', '.', '--target', 'install'])
-        finally: chdir(current_cwd)
+        finally:
+            chdir(current_cwd)
 
         try:
             prior = getattr(self.distribution, 'running_binary', False)
             self.distribution.running_binary = prior
             self.distribution.have_run['egg_info'] = 0
             dInstall.run(self)
-        finally: self.distribution.running_binary = prior
+        finally:
+            self.distribution.running_binary = prior
 
         self.install_casa_task(pkg, build_dir)
 
     def install_casa_task(self, install_dir, build_dir):
         from os import environ
         from subprocess import call
-        if 'CASAPATH' not in environ: return
+        if 'CASAPATH' not in environ:
+            return
 
         pwd = getcwd()
         try:
             chdir(join(build_dir, 'casa_task'))
             task_dir = join(install_dir, "purify", "casa")
             r = call(("buildmytasks -i=%s -o=task.py" % task_dir).split())
-            if r != 0: raise Exception("Could not build and install casa task")
-        finally: chdir(pwd)
+            if r != 0:
+                print("Could not build and install casa task.")
+                print("Purify will only be available as a python package.")
+        finally:
+            chdir(pwd)
 
 
 class BuildExt(dBuildExt):
     def __init__(self, *args, **kwargs):
         dBuildExt.__init__(self, *args, **kwargs)
-    def run(self):pass
+
+    def run(self):
+        pass
+
 
 class BuildDistEgg(dBuildDistEgg):
     def __init__(self, *args, **kwargs):
         dBuildDistEgg.__init__(self, *args, **kwargs)
+
     def run(self):
 
         try:
@@ -203,11 +227,14 @@ class BuildDistEgg(dBuildDistEgg):
             self.distribution.running_binary = True
             self.run_command('build')
             dBuildDistEgg.run(self)
-        finally: self.distribution.running_binary = prior
+        finally:
+            self.distribution.running_binary = prior
+
 
 class EggInfo(dEggInfo):
     def __init__(self, *args, **kwargs):
         dEggInfo.__init__(self, *args, **kwargs)
+
     def run(self):
         from setuptools.command.egg_info import manifest_maker
         from os import listdir
@@ -216,8 +243,9 @@ class EggInfo(dEggInfo):
         dist = self.distribution
         old_values = dist.ext_modules, dist.ext_package, \
             dist.packages, dist.package_dir
-        if len(listdir(package_dir)) != 0  \
-            and getattr(self.distribution, 'running_binary', False):
+        condition = len(listdir(package_dir)) != 0 \
+            and getattr(self.distribution, 'running_binary', False)
+        if condition:
             which_template = 'MANIFEST.binary.in'
         else:
             dist.ext_modules, dist.ext_package = None, None
@@ -232,9 +260,11 @@ class EggInfo(dEggInfo):
             dist.ext_modules, dist.ext_package = old_values[:2]
             dist.packages, dist.package_dir = old_values[2:]
 
+
 class SDist(dSDist):
     def __init__(self, *args, **kwargs):
         dSDist.__init__(self, *args, **kwargs)
+
     def run(self):
         dist = self.distribution
         try:
@@ -252,47 +282,51 @@ try:
     cwd = getcwd()
     chdir(source_dir)
     setup(
-        name = "purify",
-        version = "0.1",
+        name="purify",
+        version="0.1.1",
 
         # NOTE: python-dateutil is required by pandas,
         # but is not installed by it (pandas == 0.13)
-        install_requires = ['cython', 'numpy', 'scipy', 'pandas', 'nose'],
-        platforms = ['GNU/Linux','Unix','Mac OS-X'],
+        setup_requires=['cython', 'numpy', 'scipy', 'pytest'],
+        install_requires=['cython', 'numpy', 'scipy', 'pandas', 'pytest'],
+        platforms=['GNU/Linux', 'Unix', 'Mac OS-X'],
 
-        zip_safe = False,
-        cmdclass = {
+        zip_safe=False,
+        cmdclass={
             'build': Build, 'install': Install,
             'build_ext': BuildExt, 'bdist_egg': BuildDistEgg,
             'egg_info': EggInfo
         },
 
-        author = "Jason McEwen",
-        author_email = "j.mcewen@ucl.ac.uk",
-        description = "Purify does what it does well",
-        license = "GPL-2",
-        url = "https://github.com/basp-group/purify",
-        ext_modules = [Extension('purify.cparams', [])],
-        ext_package = 'purify',
-        packages = ['purify', 'purify.tests'],
-        package_dir = {'': str(basename(package_dir))},
+        author="Jason McEwen",
+        author_email="j.mcewen@ucl.ac.uk",
+        description="Purify does what it does well",
+        license="GPL-2",
+        url="https://github.com/basp-group/purify",
+        ext_modules=[Extension('purify.cparams', [])],
+        ext_package='purify',
+        packages=['purify', 'purify.tests'],
+        package_dir={'': str(basename(package_dir))},
         include_package_data=True,
 
-        keywords= "radio astronomy",
-        classifiers = [
-             'Development Status :: 0 - Beta',
-             'Intended Audience :: Developers',
-             'Intended Audience :: Science/Research',
-             'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
-             'Operating System :: OS Independent',
-             'Programming Language :: Python :: 2.6',
-             'Programming Language :: Python :: 2.7',
-             'Topic :: Scientific/Engineering',
-             'Topic :: Scientific/Engineering :: Astronomy',
-             'Topic :: Scientific/Engineering :: Information Analysis',
-             'Topic :: Software Development :: Libraries :: Python Modules',
-             'Topic :: Software Development :: Libraries :: Application Frameworks',
+        keywords="radio astronomy",
+        classifiers=[
+            'Development Status :: 0 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU Lesser '
+                'General Public License v2 (LGPLv2)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Astronomy',
+            'Topic :: Scientific/Engineering :: Information Analysis',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development '
+                ':: Libraries :: Application Frameworks',
         ],
         long_description=long_description
     )
-finally: chdir(cwd)
+finally:
+    chdir(cwd)
