ab6ffa645f5a7e19a0cd7d1aa91a4792324761f9
Luke Pratley <Luke.Pratley@gmail.com>
2015-10-16 09:46:05 +0100

    changed input variables for generating the interpolation kernel

--- u_init_1d.m
+++ u_init_1d.m
@@ -1,4 +1,4 @@
-function [u, T, r] = u_init_1d(m, imsize, fftsize, J, T, omega_m, km, alpha, beta, L)
+function [u, T, r, D] = u_init_1d(imsize, fftsize, J, T, omega_m, km, alpha, beta, L)
 % A function that calculates a 1D u, the 1D interpolation kernel, for a given
 % frequency omega_m.
 %
@@ -6,7 +6,6 @@ function [u, T, r] = u_init_1d(m, imsize, fftsize, J, T, omega_m, km, alpha, bet
 % 
 
 %|in
-%| m - frequency index to interpolate (scalar)
 %| imsize - imsize of dimension (scalar)
 %| fftsize - fftsize of dimension (scalar)
 %| J - Number of nearest neighbours in interpolation (scalar)
@@ -21,12 +20,11 @@ function [u, T, r] = u_init_1d(m, imsize, fftsize, J, T, omega_m, km, alpha, bet
 %| u - 1D interpolation kernel
 
 r = zeros(J,1);
-%Calculating r vector for given m
 for j = 1:J
-    r(j) = sum(sum(alpha .* diric((omega_m(m) - km(m) - j + beta*L)* 2*pi/fftsize, imsize)));
+    r(j) = sum(sum(alpha .* diric((omega_m - km - j + beta*L)*2*pi/fftsize, imsize)));
 end
 
 %calculating delta matrix
-Delta = exp(1j*(omega_m(m)-(km(m) + (1:J)' )*(imsize-1)/2 * 2*pi/fftsize)); %with complex conjugate applied
+D = exp(1j*(omega_m-(km + (1:J)' )*(imsize-1)/2*2*pi/fftsize)); %with complex conjugate applied
 
-u = Delta .*(T * r);
+u = D .*(T * r);
