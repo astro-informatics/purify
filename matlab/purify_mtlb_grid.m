563d735724cae47d4b466467572de6cb9c120e63
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2015-12-09 11:48:22 +0000

    Fixed the order of zero padding and FFTshift when degridding.

--- matlab/purify_mtlb_grid.m
+++ matlab/purify_mtlb_grid.m
@@ -1,4 +1,4 @@
-function im_est = purify_mtlb_ftgridadj(vis,st)
+function im_est = purify_mtlb_grid(vis,st)
 
 Nx1=st.Nx1;
 Ny1=st.Ny1;
@@ -11,12 +11,13 @@ spec_est=reshape(spec_est1,Ny2,Nx2);
 
 %Quadrant swap and FFT
 %spec_est = fftshift(spec_est1);
-im21 = ifft2(spec_est)*sqrt(Nx2*Ny2);
+%st.deconv=1;
+im21 = ifftshift(ifft2(spec_est))*sqrt(Nx2*Ny2).*st.deconv;
 
 %Cropping
 %Find image first point (centered image)
 xo = floor(Nx2/2) - floor(Nx1/2);
 yo = floor(Ny2/2) - floor(Ny1/2);
 
-im_est = st.deconv(yo+1:yo+Ny1,xo+1:xo+Nx1).*im21(yo+1:yo+Ny1,xo+1:xo+Nx1);
+im_est = im21(yo+1:yo+Ny1,xo+1:xo+Nx1);
 %im_est = im21(1:Ny1,1:Nx1);
