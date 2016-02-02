c56f1a3cd64fc639a65dd8c3be5762a419837da8
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-02-02 16:58:55 +0000

    Test purify_fitsio uses data access functions

--- cpp/tests/purify_fitsio.cc
+++ cpp/tests/purify_fitsio.cc
@@ -1,13 +1,15 @@
 #include "catch.hpp"
 #include "pfitsio.h"
+#include "directories.h"
 using namespace purify;
+using namespace purify::notinstalled;
 
 TEST_CASE("Purify fitsio", "[readwrite]") {
 
-  Image<t_complex> input = pfitsio::read2d("../data/images/M31.fits");
-  pfitsio::write2d(input.real(), "fits_output.fits");
-  Image<t_complex> input2 = pfitsio::read2d("fits_output.fits");
-  std::cout << input2;
+  Image<t_complex> input = pfitsio::read2d(image_filename("M31.fits"));
+  pfitsio::write2d(input.real(), output_filename("fits_output.fits"));
+  Image<t_complex> input2 = pfitsio::read2d(output_filename("fits_output.fits"));
+  CAPTURE(input2);
   CHECK(input(238, 231) == input2(238, 231));
   CHECK(input.isApprox(input2, 1e-6));
 
