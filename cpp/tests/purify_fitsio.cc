da38ed699285cfdaa2fc8048d6db576209152e8b
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-02-02 17:18:02 +0000

    fixed bug in test and altered fitsio test

--- cpp/tests/purify_fitsio.cc
+++ cpp/tests/purify_fitsio.cc
@@ -7,9 +7,7 @@ TEST_CASE("Purify fitsio", "[readwrite]") {
   Image<t_complex> input = pfitsio::read2d("../data/images/M31.fits");
   pfitsio::write2d(input.real(), "fits_output.fits");
   Image<t_complex> input2 = pfitsio::read2d("fits_output.fits");
-  std::cout << input2;
-  CHECK(input(238, 231) == input2(238, 231));
-  CHECK(input.isApprox(input2, 1e-6));
+  CHECK(input.isApprox(input2, 1e-13));
 
 }
 
