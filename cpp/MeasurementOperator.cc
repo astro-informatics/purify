5ba8a98e3f6cca4e8a16eaaa69acd1dea6714107
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-02-03 16:14:07 +0000

    minor formatting change

--- cpp/MeasurementOperator.cc
+++ cpp/MeasurementOperator.cc
@@ -267,8 +267,7 @@ namespace purify {
 
       t_real alpha = 2.34 * J; // value said to be optimal in Fessler et. al. 2003
       t_complex eta = std::sqrt(static_cast<t_complex>((purify_pi * x * J)*(purify_pi * x * J) - alpha * alpha));
-      t_real normalisation = 38828.11016883
-      ; //Factor that keeps it consistent with fessler formula
+      t_real normalisation = 38828.11016883; //Factor that keeps it consistent with fessler formula
 
       return std::real(std::sin(eta) / eta) / normalisation; //simple way of doing the calculation, the boost bessel funtions do not support complex valued arguments
   }
