97610a4519e7ac653c874bd7ec118837674a8b9b
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-02-23 22:57:53 +0000

    Example of using M31 and SDMM through sopt

--- cpp/example/sdmm.cc
+++ cpp/example/sdmm.cc
@@ -1,7 +1,4 @@
-#include "purify_config.h"
-#include "purify_types.h"
 #include <array>
-#include <fftw3.h>
 #include <memory>
 #include <random>
 #include "sopt/relative_variation.h"
@@ -13,6 +10,7 @@
 #include "pfitsio.h"
 #include "regressions/cwrappers.h"
 #include "types.h"
+#include "MeasurementOperator.h"
 
 int main(int, char **) {
   using namespace purify;
@@ -22,28 +20,52 @@ int main(int, char **) {
   std::string const fitsfile = image_filename("M31.fits");
   std::string const visfile = image_filename("Coverages/cont_sim4.vis");
   std::string const outfile = output_filename("M31.tiff");
+  std::string const dirty_image = output_filename("M31_dirty.tiff");
 
+  t_real const over_sample = 1.375;
   auto const M31 = pfitsio::read2d(fitsfile);
-  purify::CMeasurementOperator const measurements(visfile, M31.rows(), M31.cols());
-  sopt::wavelets::SARA const sara{std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u)};
+  auto uv_data = utilities::read_visibilities(visfile, PURIFY_VISIBILITY_FILETYPE_PROFILE_VIS);
+  uv_data = utilities::uv_scale(uv_data, floor(M31.cols() * over_sample), floor(M31.rows() * over_sample)); // scale uv coordinates to units of Fourier grid size
+  uv_data = utilities::uv_symmetry(uv_data); // Enforce condjugate symmetry by reflecting measurements in uv coordinates
+  MeasurementOperator measurements(uv_data.v, uv_data.u, uv_data.weights, 4, 4, "kb_interp", M31.cols(), M31.rows(), over_sample);
+
+ 
+  auto direct = [&measurements, &M31](Vector<t_complex> &out, Vector<t_complex> const &x) {
+        assert(x.size() == M31.size());
+        auto const image = Image<t_complex>::Map(x.data(), M31.rows(), M31.cols());
+        out = measurements.degrid(image);
+  };
+  auto adjoint = [&measurements, &M31](Vector<t_complex> &out, Vector<t_complex> const &x) {
+        auto image = Image<t_complex>::Map(out.data(), M31.rows(), M31.cols());
+        image = measurements.grid(x);
+  };
+  auto measurements_transform = sopt::linear_transform<Vector<t_complex>>(
+    direct, {0, 1, static_cast<t_int>(uv_data.vis.size())},
+    adjoint, {0, 1, static_cast<t_int>(M31.size())}
+  );
+
+  sopt::wavelets::SARA const sara{std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u), std::make_tuple("DB3", 3u), std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u), std::make_tuple("DB6", 3u), std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)};
   auto const Psi = sopt::linear_transform<t_complex>(sara, M31.rows(), M31.cols());
 
   std::mt19937_64 mersenne;
   Vector<t_complex> const y0
-      = measurements.linear_transform() * Vector<t_complex>::Map(M31.data(), M31.size());
+      = (measurements_transform * Vector<t_complex>::Map(M31.data(), M31.size()));
   auto const input = dirty(y0, mersenne, 30e0);
+  Vector<> dimage = (measurements_transform.adjoint() * input).real();
+  sopt::utilities::write_tiff(Image<t_real>::Map(dimage.data(), M31.rows(), M31.cols()), dirty_image);
+
   auto const epsilon
       = std::sqrt(y0.size() + 2 * std::sqrt(y0.size())) * sigma(y0, 30e0)
         / std::sqrt(static_cast<t_real>(y0.size()) / static_cast<t_real>(M31.size()));
-
+  std::cout << "Starting sopt!" << '\n';
   auto const sdmm
       = sopt::algorithm::SDMM<t_complex>()
             .itermax(500)
-            .gamma((measurements.linear_transform().adjoint() * input).real().maxCoeff() * 1e-3)
+            .gamma((measurements_transform.adjoint() * input).real().maxCoeff() * 1e-3)
             .is_converged(sopt::RelativeVariation<t_complex>(1e-3))
-            .conjugate_gradient(100, 1e-6)
+            .conjugate_gradient(100, 1e-3)
             .append(sopt::proximal::translate(sopt::proximal::L2Ball<t_complex>(epsilon), -input),
-                    measurements.linear_transform())
+                    measurements_transform)
             .append(sopt::proximal::l1_norm<t_complex>, Psi.adjoint(), Psi)
             .append(sopt::proximal::positive_quadrant<t_complex>);
   auto const result = sdmm(Vector<t_complex>::Zero(M31.size()));
