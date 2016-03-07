93cbca4db4a95d5d35b9871dca78c8fcfb0c6cc7
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-03-07 22:13:10 +0000

    edited w_projection example to degrid then grid M31 simulation using a simulated uv coverage

--- cpp/example/w_projection_example.cc
+++ cpp/example/w_projection_example.cc
@@ -17,42 +17,35 @@ int main( int nargs, char const** args ){
   t_real over_sample;
   t_real cellsize;
   std::string kernel;
-  std::string vis_file = "../data/vla/at166B.3C129.c0.vis";
-  //std::string vis_file = "../data/ATCA/1637-77.vis";
 
 
   //Gridding example
   cellsize = 5;//
   over_sample = 2;
   t_int J = 4;
-  t_int width = 32;
-  t_int height = width;
-  t_int number_of_vis = 20;
+
+  t_int number_of_vis = 200;
   bool use_w_term = true;
+  std::string const fitsfile = image_filename("M31.fits");
+  auto M31 = pfitsio::read2d(fitsfile);
+  t_int height = M31.rows();
+  t_int width = M31.cols();
   
-  const t_real theta_FoV_L = cellsize * width;
-  const t_real theta_FoV_M = cellsize * height;
+  uv_vis = utilities::random_sample_density(number_of_vis, 0, 4000);
+  
+
+  const t_real theta_FoV_L = cellsize * width * over_sample;
+  const t_real theta_FoV_M = cellsize * height * over_sample;
 
   const t_real L = 2 * std::sin(purify_pi / 180.* theta_FoV_L / (60. * 60.) * 0.5);
   const t_real M = 2 * std::sin(purify_pi / 180.* theta_FoV_M / (60. * 60.) * 0.5);
-
-  uv_vis.u = Vector<t_real>::Random(number_of_vis);
-  uv_vis.v = Vector<t_real>::Random(number_of_vis);
-  uv_vis.w = Vector<t_real>::Random(number_of_vis);
-  t_real w_max = uv_vis.w.maxCoeff();
-  uv_vis.w.setOnes();
-  uv_vis.w = uv_vis.w *  1/(L*L);
-  uv_vis.weights = Vector<t_complex>::Zero(number_of_vis); uv_vis.weights.setOnes();
-  //uv_vis = utilities::set_cell_size(uv_vis, cellsize); // scale uv coordinates to correct pixel size and to units of 2pi
-  //uv_vis = utilities::uv_scale(uv_vis, floor(width * over_sample), floor(height * over_sample)); // scale uv coordinates to units of Fourier grid size
-  //uv_vis = utilities::uv_symmetry(uv_vis); // Enforce condjugate symmetry by reflecting measurements in uv coordinates
-
-  t_real upsample = utilities::upsample_ratio(uv_vis, cellsize, cellsize, width, height);
-
-  std::cout << " upsample: " << upsample << '\n';
-
+  t_real FoV = std::max(L, M);
+  uv_vis.w = Vector<t_real>::Constant(number_of_vis, 1)/FoV * (uv_vis.u.array() * uv_vis.u.array() + uv_vis.v.array() * uv_vis.v.array()).sqrt().maxCoeff();
 
   kernel = "kb";
   MeasurementOperator op(uv_vis, J, J, kernel, width, height, over_sample, "none", 0, use_w_term, cellsize, cellsize); // Create Measurement Operator
-  
+  auto y0 = op.degrid(M31);
+  pfitsio::write2d(op.grid(y0).real(), "w_test.fits");
+  MeasurementOperator op_norm(uv_vis, J, J, kernel, width, height, over_sample, "none", 0, false, cellsize, cellsize); // Create Measurement Operator
+  pfitsio::write2d(op_norm.grid(y0).real(), "w_norm_test.fits");
 }
