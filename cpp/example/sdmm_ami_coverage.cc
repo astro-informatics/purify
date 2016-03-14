ffafd23106acfc1b61ebb26f26721812e65e86df
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-03-14 18:37:31 +0000

    -updated example sdmm with ami coverage example

--- cpp/example/sdmm_ami_coverage.cc
+++ cpp/example/sdmm_ami_coverage.cc
@@ -19,51 +19,57 @@ int main(int, char **) {
   sopt::logging::initialize();
 
   std::string const fitsfile = image_filename("M31.fits");
+  std::string const inputfile = output_filename("M31_input.fits");
   std::string const visfile = image_filename("Coverages/cont_sim4.vis");
   std::string const outfile = output_filename("M31.tiff");
+  std::string const residual_fits = output_filename("M31_residual.fits");
   std::string const outfile_fits = output_filename("M31_solution.fits");
   std::string const dirty_image = output_filename("M31_dirty.tiff");
   std::string const dirty_image_fits = output_filename("M31_dirty.fits");
 
   t_real const over_sample = 1.375;
-  auto const M31 = pfitsio::read2d(fitsfile);
+  auto M31 = pfitsio::read2d(fitsfile);
+  t_real const max = M31.array().abs().maxCoeff();
+  M31 = M31 * 1. / max;
   auto uv_data = utilities::read_visibilities(visfile, PURIFY_VISIBILITY_FILETYPE_PROFILE_VIS);
   uv_data.units = "radians";
   uv_data = utilities::uv_symmetry(uv_data); //reflect uv measurements
   MeasurementOperator measurements(uv_data, 4, 4, "kb_interp", M31.cols(), M31.rows(), over_sample);
 
  
-  auto direct = [&measurements, &M31](Vector<t_complex> &out, Vector<t_complex> const &x) {
-        assert(x.size() == M31.size());
-        auto const image = Image<t_complex>::Map(x.data(), M31.rows(), M31.cols());
+  auto direct = [&measurements](Vector<t_complex> &out, Vector<t_complex> const &x) {
+        assert(x.size() == measurements.imsizex * measurements.imsizey);
+        auto const image = Image<t_complex>::Map(x.data(), measurements.imsizey, measurements.imsizex);
         out = measurements.degrid(image);
   };
-  auto adjoint = [&measurements, &M31](Vector<t_complex> &out, Vector<t_complex> const &x) {
-        auto image = Image<t_complex>::Map(out.data(), M31.rows(), M31.cols());
+  auto adjoint = [&measurements](Vector<t_complex> &out, Vector<t_complex> const &x) {
+        auto image = Image<t_complex>::Map(out.data(), measurements.imsizey, measurements.imsizex);
         image = measurements.grid(x);
   };
   auto measurements_transform = sopt::linear_transform<Vector<t_complex>>(
     direct, {0, 1, static_cast<t_int>(uv_data.vis.size())},
-    adjoint, {0, 1, static_cast<t_int>(M31.size())}
+    adjoint, {0, 1, static_cast<t_int>(measurements.imsizex * measurements.imsizey)}
   );
 
-  sopt::wavelets::SARA const sara{std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u), std::make_tuple("DB3", 3u), std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u), std::make_tuple("DB6", 3u), std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)};
-  auto const Psi = sopt::linear_transform<t_complex>(sara, M31.rows(), M31.cols());
+  sopt::wavelets::SARA const sara{std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u), std::make_tuple("DB3", 3u), 
+          std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u), std::make_tuple("DB6", 3u), std::make_tuple("DB7", 3u), 
+          std::make_tuple("DB8", 3u)};
+  auto const Psi = sopt::linear_transform<t_complex>(sara, measurements.imsizey, measurements.imsizex);
 
   std::mt19937_64 mersenne;
   Vector<t_complex> const y0
       = (measurements_transform * Vector<t_complex>::Map(M31.data(), M31.size()));
   auto const input = dirty(y0, mersenne, 30e0);
   Vector<> dimage = (measurements_transform.adjoint() * input).real();
-  t_real max_val = dimage.array().abs().maxCoeff();
+  t_real const max_val = dimage.array().abs().maxCoeff();
   dimage = dimage / max_val;
   Vector<t_complex> initial_estimate = Vector<t_complex>::Zero(dimage.size());
-  sopt::utilities::write_tiff(Image<t_real>::Map(dimage.data(), M31.rows(), M31.cols()), dirty_image);
-  pfitsio::write2d(Image<t_real>::Map(dimage.data(), M31.rows(), M31.cols()), dirty_image_fits);
+  sopt::utilities::write_tiff(Image<t_real>::Map(dimage.data(), measurements.imsizey, measurements.imsizex), dirty_image);
+  pfitsio::write2d(Image<t_real>::Map(dimage.data(), measurements.imsizey, measurements.imsizex), dirty_image_fits);
 
   auto const epsilon
-      = std::sqrt(y0.size() + 2 * std::sqrt(y0.size())) * sigma(y0, 30e0)
-        / std::sqrt(static_cast<t_real>(y0.size()) / static_cast<t_real>(M31.size()));
+      = utilities::calculate_l2_radius(y0.size(), sigma(y0, 30e0), measurements.imsizex * measurements.imsizey);
+
   std::cout << "Starting sopt!" << '\n';
   auto const sdmm
       = sopt::algorithm::SDMM<t_complex>()
@@ -77,10 +83,11 @@ int main(int, char **) {
             .append(sopt::proximal::positive_quadrant<t_complex>);
   auto const result = sdmm(initial_estimate);
   assert(result.out.size() == M31.size());
-  Image<t_real> image = Image<t_complex>::Map(result.out.data(), M31.rows(), M31.cols()).real();
-  t_real max_val_final = image.array().abs().maxCoeff();
+  Image<t_real> image = Image<t_complex>::Map(result.out.data(), measurements.imsizey, measurements.imsizex).real();
+  t_real const max_val_final = image.array().abs().maxCoeff();
   image = image / max_val_final;
   sopt::utilities::write_tiff(image, outfile);
   pfitsio::write2d(image, outfile_fits);
+  pfitsio::write2d(M31.real() - image, residual_fits);
   return 0;
 }
