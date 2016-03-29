164af1930419568f7b2debe642d6fc5737f34da9
Luke Pratley <Luke.Pratley.15@ucl.ac.uk>
2016-03-29 17:14:10 +0100

    Fixed plotting of errors in simulation.

--- scripts/plot_m31_SNR.py
+++ scripts/plot_m31_SNR.py
@@ -17,34 +17,46 @@ def calculate_average_SNR(kernel):
 		SNRs[i] = 20 * np.log10(s / r)
 		os.system("rm " + residuals[i])
 		os.system("rm " + solutions[i])
-	return SNRs.mean()
+	return SNRs.mean(), SNRs.std()
 
 meankbSNR = []
 meankb_interpSNR = []
 meangaussSNR = []
 meanpswfSNR = []
+errorkbSNR = []
+errorkb_interpSNR = []
+errorgaussSNR = []
+errorpswfSNR = []
 M_over_N = np.arange(1, 11) * 0.2
 for i in M_over_N:
 	process = subprocess.Popen(["python", "run_m31_test.py", "kb", str(i)])
 	process.wait()
-	meankbSNR.append(calculate_average_SNR("kb"))
+	snr, error = calculate_average_SNR("kb")
+	meankbSNR.append(snr)
+	errorkbSNR.append(error)
 for i in M_over_N:
 	process = subprocess.Popen(["python", "run_m31_test.py", "kb_interp", str(i)])
 	process.wait()
-	meankb_interpSNR.append(calculate_average_SNR("kb_interp"))
+	snr, error = calculate_average_SNR("kb_interp")
+	meankb_interpSNR.append(snr)
+	errorkb_interpSNR.append(error)
 for i in M_over_N:
 	process = subprocess.Popen(["python", "run_m31_test.py", "gauss", str(i)])
 	process.wait()
-	meangaussSNR.append(calculate_average_SNR("gauss"))
+	snr, error = calculate_average_SNR("gauss")
+	meangaussSNR.append(snr)
+	errorgaussSNR.append(error)
 for i in M_over_N:
 	process = subprocess.Popen(["python", "run_m31_test.py", "pswf", str(i)])
 	process.wait()
-	meanpswfSNR.append(calculate_average_SNR("pswf"))
-
-plt.scatter(M_over_N, meankbSNR)
-plt.scatter(M_over_N, meankb_interpSNR, c = "red")
-plt.scatter(M_over_N, meangaussSNR, c = "green")
-plt.scatter(M_over_N, meanpswfSNR, c = "black")
+	snr, error = calculate_average_SNR("pswf")
+	meanpswfSNR.append(snr)
+	errorpswfSNR.append(error)
+print len(meankbSNR), len(errorkbSNR)
+plt.errorbar(M_over_N, meankbSNR, errorkbSNR, fmt='')
+plt.errorbar(M_over_N, meankb_interpSNR, errorkb_interpSNR, c = "red", fmt='')
+plt.errorbar(M_over_N, meangaussSNR, errorgaussSNR, c = "green", fmt='')
+plt.errorbar(M_over_N, meanpswfSNR, errorpswfSNR, c = "black", fmt='')
 plt.legend(["kb", "kb_interp", "gauss", "pswf"])
 plt.xlabel("M/N")
 plt.ylabel("SNR, db")
