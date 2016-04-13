ad13f4089b43b5c84a70a51d67fee3f3139cbd5f
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-04-13 16:38:52 +0100

    -added dirac wavelet to random coverage example

--- cpp/example/sdmm_random_coverage.cc
+++ cpp/example/sdmm_random_coverage.cc
@@ -63,7 +63,7 @@ int main(int, char **) {
     adjoint, {0, 1, static_cast<t_int>(measurements.imsizex * measurements.imsizey)}
   );
 
-  sopt::wavelets::SARA const sara{std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u), std::make_tuple("DB3", 3u), 
+  sopt::wavelets::SARA const sara{std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u), std::make_tuple("DB3", 3u), 
           std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u), std::make_tuple("DB6", 3u), std::make_tuple("DB7", 3u), 
           std::make_tuple("DB8", 3u)};
 
