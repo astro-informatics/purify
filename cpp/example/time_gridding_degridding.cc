5b7d2f285c1ba3032e1811a872ba0d4411af385d
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-04-24 13:04:44 +0100

    -Added time_gridding_degridding.cc as a simple test of gridding and degridding times. -Also added plot_grid_degrid_times.py to plot the times.

---
+++ cpp/example/time_gridding_degridding.cc
@@ -0,0 +1,85 @@
+
+#include "MeasurementOperator.h"
+#include "utilities.h"
+#include "pfitsio.h"
+#include "directories.h"
+
+#include <ctime>
+
+using namespace purify;  
+using namespace purify::notinstalled;
+
+int main( int nargs, char const** args ){
+  if (nargs != 6 )
+  {
+    std::cout << " Wrong number of arguments! " << '\n';
+    return 1;
+  }
+
+  std::string const kernel = args[1];
+  t_real const over_sample = std::stod(static_cast<std::string>(args[2]));
+  t_int const J = static_cast<t_int>(std::stod(static_cast<std::string>(args[3])));
+  t_int const sample = static_cast<t_int>(std::stod(args[4]));
+  t_int const number_of_tests = static_cast<t_int>(std::stod(args[5]));  
+
+  std::string const results = output_filename("Grid_Degrid_timing_" + kernel + "_" + std::to_string(sample)+".txt");
+
+  //Gridding example
+  t_int const width = 256;
+  t_int const height = 256;
+  t_int const number_of_vis = 2 * std::floor(width * height * sample / 10.);
+  t_real const sigma_m = purify_pi/3;
+
+  Vector<t_real> grid_times = Vector<t_real>::Zero(number_of_tests);
+  Vector<t_real> degrid_times = Vector<t_real>::Zero(number_of_tests);
+
+  Image<t_complex> im = Matrix<t_complex>::Random(width, height);
+  Vector<t_complex> vis = Vector<t_complex>::Random(number_of_vis);
+  t_real inner_loop = 100;
+  t_complex const I(0, 1);
+  std::cout.precision(20);
+  for (t_int i = 0; i < number_of_tests; ++i)
+  {
+    auto uv_data = utilities::random_sample_density(number_of_vis, 0, sigma_m);
+    uv_data.units = "radians";
+    MeasurementOperator op(uv_data, J, J, kernel, width, height, over_sample); // Generating gridding matrix
+    
+    uv_data.vis = Vector<t_complex>::Random(number_of_vis) + I*Vector<t_complex>::Random(number_of_vis);
+    std::clock_t c_start = std::clock();
+    for (t_int j = 0; j < inner_loop; ++j)
+    {
+      op.grid(uv_data.vis);
+    }
+    std::clock_t c_end = std::clock();
+    grid_times(i) = static_cast<t_real>((c_end - c_start) / CLOCKS_PER_SEC)/inner_loop;
+    std::cout << (c_end - c_start) / CLOCKS_PER_SEC/inner_loop << "\n";
+  }
+  
+  for (t_int i = 0; i < number_of_tests; ++i)
+  {
+    auto uv_data = utilities::random_sample_density(number_of_vis, 0, sigma_m);
+    uv_data.units = "radians";
+    MeasurementOperator op(uv_data, J, J, kernel, width, height, over_sample); // Generating gridding matrix
+    Image<t_complex> im = Matrix<t_complex>::Random(width, height) + I*Matrix<t_complex>::Random(width, height);
+    std::clock_t c_start = std::clock();
+    for (t_int j = 0; j < inner_loop; ++j)
+    {
+      op.degrid(im);
+    }
+    std::clock_t c_end = std::clock();
+    degrid_times(i) = static_cast<t_real>((c_end - c_start) / CLOCKS_PER_SEC)/inner_loop;
+    std::cout << (c_end - c_start) / CLOCKS_PER_SEC/inner_loop << "\n";
+  }  
+  std::printf("Done! \n");
+  t_real const mean_grid_time = grid_times.array().mean();
+  t_real const rms_grid_time = utilities::standard_deviation(grid_times);
+
+  t_real const mean_degrid_time = degrid_times.array().mean();
+  t_real const rms_degrid_time = utilities::standard_deviation(degrid_times);
+  //writing snr and time to a file
+  std::ofstream out(results);
+  out.precision(20);
+  out << mean_grid_time << " " << rms_grid_time << " " << mean_degrid_time << " " << rms_degrid_time << "\n";
+  std::cout << mean_grid_time << " " << rms_grid_time << " " << mean_degrid_time << " " << rms_degrid_time << "\n";
+  out.close();
+}
---
+++ scripts/plot_grid_degrid_times.py
@@ -0,0 +1,115 @@
+import os 
+import multiprocessing
+import time
+import numpy as np 
+import pyfits
+import glob
+import subprocess
+import matplotlib.pyplot as plt
+
+def run_test((i, kernel, number_of_samples, start_time)):
+	time.sleep(start_time)
+	J = 4
+	oversample = 2
+	if kernel == "pswf":
+  		J = 6
+	if kernel == "kb_interp":
+  		oversample = 1.375
+
+  	
+  	os.system("../build/cpp/example/time_gridding_degridding " + kernel + " " 
+      + str(oversample) + " " +str(J) + " " +str(i) + " " + str(number_of_samples))
+  	
+  	results_file = "../build/outputs/Grid_Degrid_timing_" + kernel + "_" + str(i) + ".txt"
+  	results = np.loadtxt(results_file, dtype = str)
+  	grid_time = results[0]
+  	grid_time_error = results[1]
+  	degrid_time = results[2]
+  	degrid_time_error = results[3]
+ 
+	os.system("rm " + results_file)
+  	return [float(grid_time), float(grid_time_error), float(degrid_time), float(degrid_time_error)]
+
+
+if __name__ == '__main__':
+	M_N_ratios = np.arange(1, 11)
+	args = []
+	test_num = 0
+	kernels = ["kb", "kb_interp", "pswf", "gauss"]
+	total_tests = len(kernels) * len(M_N_ratios)
+	for k in kernels:
+		for m in M_N_ratios:
+			test_num = test_num + 1
+			args.append((test_num, k, m, test_num * 1./ total_tests * 30.))
+			print test_num
+	n_processors = multiprocessing.cpu_count() + 1
+	p = multiprocessing.Pool(min(n_processors, 41)) # Limiting the number of processes used to 40, otherwise it will cause problems with the user limit
+	results = p.map(run_test, args)
+
+	gridkbtime = []
+	gridkbtime_error = []
+	degridkbtime = []
+	degridkbtime_error = []
+
+	gridkb_interptime = []
+	gridkb_interptime_error = []
+	degridkb_interptime = []
+	degridkb_interptime_error = []
+
+	gridpswftime = []
+	gridpswftime_error = []
+	degridpswftime = []
+	degridpswftime_error = []
+
+	gridgausstime = []
+	gridgausstime_error = []
+	degridgausstime = []
+	degridgausstime_error = []	
+	for m in M_N_ratios:
+		for i in range(len(args)):
+			if m == args[i][2]:
+				if args[i][1] == "kb":
+					gridkbtime.append(results[i][0])
+					gridkbtime_error.append(results[i][1])
+					degridkbtime.append(results[i][2])
+					degridkbtime_error.append(results[i][3])
+				if args[i][1] == "kb_interp":
+					gridkb_interptime.append(results[i][0])
+					gridkb_interptime_error.append(results[i][1])
+					degridkb_interptime.append(results[i][2])
+					degridkb_interptime_error.append(results[i][3])
+				if args[i][1] == "gauss":
+					gridpswftime.append(results[i][0])
+					gridpswftime_error.append(results[i][1])
+					degridpswftime.append(results[i][2])
+					degridpswftime_error.append(results[i][3])
+				if args[i][1] == "pswf":
+					gridgausstime.append(results[i][0])
+					gridgausstime_error.append(results[i][1])
+					degridgausstime.append(results[i][2])
+					degridgausstime_error.append(results[i][3])
+	
+
+
+	plt.errorbar(M_N_ratios/10. * 2, gridkbtime, gridkbtime_error, fmt='')
+	plt.errorbar(M_N_ratios/10. * 2, gridkb_interptime, gridkb_interptime_error, c = "red", fmt='')
+	plt.errorbar(M_N_ratios/10. * 2, gridpswftime, gridpswftime_error, c = "green", fmt='')
+	plt.errorbar(M_N_ratios/10. * 2, gridgausstime, gridgausstime_error, c = "black", fmt='')
+	plt.legend(["Kaiser-Bessel (KB)", "KB Linear Interpolation", "Gaussian", "PSWF"])
+	plt.xlabel("M/N")
+	plt.ylabel("Time (seconds)")
+	plt.xlim(0, 2.2)
+	plt.savefig("Grid_Time_plot.pdf")
+
+	plt.clf()
+
+	plt.errorbar(M_N_ratios/10. * 2, degridkbtime, degridkbtime_error, fmt='')
+	plt.errorbar(M_N_ratios/10. * 2, degridkb_interptime, degridkb_interptime_error, c = "red", fmt='')
+	plt.errorbar(M_N_ratios/10. * 2, degridpswftime, degridpswftime_error, c = "green", fmt='')
+	plt.errorbar(M_N_ratios/10. * 2, degridgausstime, degridgausstime_error, c = "black", fmt='')
+	#plt.legend(["Kaiser-Bessel (KB)", "KB Linear Interpolation", "Gaussian", "PSWF"])
+	plt.xlabel("M/N")
+	plt.ylabel("Time (seconds)")
+	plt.xlim(0, 2.2)
+	plt.savefig("Degrid_Time_plot.pdf")
+	plt.clf()
