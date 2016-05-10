1d4b5b5dcac7e150ab314d862eca7eb53e99402f
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-05-10 12:23:49 +0100

    Replaced old script for converting .ms files to new script. This script writes the .vis file in units of wavelength rather than meters.

--- scripts/extract_vis.py
+++ scripts/extract_vis.py
@@ -3,6 +3,7 @@
 #    Assumes CASA Version 4.1.0
 # -------------------------------------------------
 # [130902] - a.scaife@soton.ac.uk
+# [151012] - luke.pratley.15@ucl.ac.uk
 # -------------------------------------------------
 # to run:
 # my-laptop> casapy --nologger -c extract_vis.py
@@ -17,12 +18,12 @@ const_c = 299792458. # m/s
 
 # -----------------------------------------------------
 # define input data files:
-infile='at166B.3C129.c0'
+infile='CygA-X'
 
 
 # -----------------------------------------------------
 # open MS SPECTRAL table:
-tb.open(infile+'.ms/SPECTRAL_WINDOW',nomodify=True)
+tb.open(infile+'.MS/SPECTRAL_WINDOW',nomodify=True)
 # extract channel frequencies (in Hz):
 freq=tb.getcol("CHAN_FREQ")
 refreq=tb.getcol("REF_FREQUENCY")
@@ -32,7 +33,7 @@ tb.close
 
 # -----------------------------------------------------
 # open MS MAIN table:
-tb.open(infile+'.ms',nomodify=True)
+tb.open(infile+'.MS',nomodify=True)
 # extract UVW (note these are in metres):
 uvw=tb.getcol("UVW")
 units=tb.getcolkeyword("UVW", "QuantumUnits")
@@ -43,26 +44,33 @@ data=tb.getcol("DATA")
 # extract weight for each visibility:
 sigma=tb.getcol("SIGMA")
 
-dataI = 0.5*(data[0,0,:]+data[3,0,:])
-sigmaI= 0.5*np.sqrt(sigma[0,:]**2+sigma[3,:]**2)
-
 # -----------------------------------------------------
 # open output vis-file:
 os.system('rm -rf '+infile+'.vis \n')
 visfile=open(infile+'.vis','w')
+print 'Total Channels: ', len(data[0,:,0])
+# Loop over frequency channels, and add uv data to .vis file
+for j in range(0,len(data[0,:,0])):
+	print 'Adding Channel:', j, 'Frequency (Hz):', freq[j, 0]
+	flag = tb.getcol('FLAG')[0, j, :]
 
+	dataI = 0.5*(data[0,j,:]+data[3,j,:])
+	sigmaI= 0.5*np.sqrt(sigma[0,:]**2+sigma[3,:]**2)
 
-# -----------------------------------------------------
-print ">> Columns are:"
-print " U(lambda)  V(lambda)  Real(Jy)  Imag(Jy)  Sigma(Jy) "
+	# -----------------------------------------------------
+	print ">> Columns are:"
+	print " U(lambda)  V(lambda)  Real(Jy)  Imag(Jy)  Sigma(Jy) "
 
-uvw_c = uvw*freq[0,0]/const_c # convert from m -> lambda
-# loop over visibility:
-for i in range(0,uvw.shape[1]):
-    if (sigma[0,i]!=-1.0)and(sigma[3,i]!=-1.0):
-        visfile.write(str(uvw_c[0,i])+' '+str(uvw_c[1,i])+' '\
-                    +str(dataI[i].real)+' '+str(dataI[i].imag)\
-                    +' '+str(sigmaI[i])+'\n')
+	uvw_c = uvw*np.mean(freq)/const_c # convert from m -> lambda, not wanted for MFS!
+	#scale = 1./(60. * 60. * 5)
+	#uvw_c = uvw_c * scale
+	# loop over visibility:
+	for i in range(0,uvw.shape[1]):
+	    if (sigma[0,i]!=-1.0)and(sigma[3,i]!=-1.0)and(flag[i]==0):
+	        visfile.write(str(uvw_c[0,i])+' '+str(uvw_c[1,i])+' '\
+	                    +str(dataI[i].real)+' '+str(dataI[i].imag)\
+	                    +' '+str(sigmaI[i])+'\n')
+	break
 
 
 visfile.close()
