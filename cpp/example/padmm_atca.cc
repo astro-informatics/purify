8659fbc325679ca2fccf0753717711b9d5ea5924
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-05-31 19:41:24 +0100

    updated atca padmm example to write header information to fits file

--- cpp/example/padmm_atca.cc
+++ cpp/example/padmm_atca.cc
@@ -25,16 +25,28 @@ int main(int, char **) {
 
   std::string const residual_fits = output_filename("atca_residual.fits");
 
-  t_int const niters = 1e5;
-  t_real const beta = 1e-1;
+  std::string const restore_fits = output_filename("atca_restore.fits");
+
+  t_int const niters = 500;
+  t_real const beta = 1e-2;
   t_real const over_sample = 2;
   auto uv_data = utilities::read_visibility(visfile);
   uv_data.units = "lambda";
-  t_real cellsize = 1;
-  t_int width = 512;
-  t_int height = 512;
-  //uv_data = utilities::uv_symmetry(uv_data);
-  MeasurementOperator measurements(uv_data, 4, 4, "kb_min", width, height, over_sample, cellsize, cellsize, "none");
+  t_real cellsize = 2;
+  t_int width = 1024;
+  t_int height = 1024;
+  uv_data = utilities::uv_symmetry(uv_data);
+  //header information
+  pfitsio::header_params header;
+  header.mean_frequency = 1381.67703151703;
+  header.ra = 251.083333359168;
+  header.dec = -77.2583333178662;
+  header.cell_x = cellsize;
+  header.cell_y = cellsize;
+
+
+
+  MeasurementOperator measurements(uv_data, 4, 4, "kb", width, height, over_sample, cellsize, cellsize, "none");
 
   auto direct = [&measurements, &width, &height](Vector<t_complex> &out, Vector<t_complex> const &x) {
         assert(x.size() == width * height);
@@ -68,32 +80,32 @@ int main(int, char **) {
     //initial_estimate = pfitsio::read2d(outfile_fits);
   
 
-  pfitsio::write2d(Image<t_real>::Map(dimage.data(), height, width), dirty_image_fits);
+  pfitsio::write2d_header(Image<t_real>::Map(dimage.data(), height, width), header);
 
- 
-  t_real const noise_rms = 0.5630 * 0.5;
+
+  t_real const noise_rms = 0.5630 * 0.8/std::sqrt(2);
   std::cout << "Calculated RMS noise of " << noise_rms * 1e3 << " mJy" << '\n';
   t_real epsilon = utilities::calculate_l2_radius(input, noise_rms); //Calculation of l_2 bound following SARA paper
   t_real epsilon_alt = std::sqrt(uv_data.vis.size()) * noise_rms;
-  auto purify_gamma = (Psi.adjoint() * (measurements_transform.adjoint() * (uv_data.weights.array().real() * input.array()).matrix())).real().maxCoeff() * beta;
+  auto purify_gamma = (Psi.adjoint() * (measurements_transform.adjoint() * (uv_data.weights.array().real() * input.array()).matrix())).real().cwiseAbs().maxCoeff() * beta;
 
   std::cout << "Starting sopt!" << '\n';
-  std::cout << "Epsilon = " << epsilon - epsilon_alt << '\n';
+  std::cout << "Epsilon = " << epsilon << '\n';
   std::cout << "Gamma = " << purify_gamma << '\n';
   auto const padmm = sopt::algorithm::L1ProximalADMM<t_complex>(input)
-    .itermax(500)
+    .itermax(niters)
     .gamma(purify_gamma)
     .relative_variation(1e-3)
     .l2ball_proximal_epsilon(epsilon)
     .l2ball_proximal_weights(uv_data.weights.array().real())
     .tight_frame(false)
-    .l1_proximal_tolerance(1e-3)
+    .l1_proximal_tolerance(1e-4)
     .l1_proximal_nu(1)
-    .l1_proximal_itermax(50)
-    .l1_proximal_positivity_constraint(false)
+    .l1_proximal_itermax(100)
+    .l1_proximal_positivity_constraint(true)
     .l1_proximal_real_constraint(true)
     .residual_convergence(epsilon)
-    .lagrange_update_scale(0.9)
+    .lagrange_update_scale(0.5)
     .nu(1e0)
     .Psi(Psi)
     .Phi(measurements_transform);
@@ -101,14 +113,17 @@ int main(int, char **) {
   assert(result.x.size() == width * height);
   Image<t_complex> image = Image<t_complex>::Map(result.x.data(), measurements.imsizey, measurements.imsizex);
   t_real const max_val_final = image.array().abs().maxCoeff();
-  image = image / max_val_final;
-  sopt::utilities::write_tiff(image.real(), outfile);
+  image = image;
+  // header information
+  header.pix_units = "JY/PIXEL";
+  header.fits_name = outfile_fits;
 
-  pfitsio::write2d(image.real(), outfile_fits);
+
+  pfitsio::write2d_header(image.real(), header);
   image = Image<t_complex>::Map(result.x.data(), measurements.imsizey, measurements.imsizex);
   Image<t_complex> residual = measurements.grid(uv_data.weights.array().real().sqrt() * (input - measurements.degrid(image)).array());
-  pfitsio::write2d(residual.real(), residual_fits);
-  auto restore = image = image + residual;
-  pfitsio::write2d(restore.real(), restore_fits);
+  header.pix_units = "JY/BEAM";
+  header.fits_name = residual_fits;
+  pfitsio::write2d_header(residual.real(), header);
   return 0;
 }
