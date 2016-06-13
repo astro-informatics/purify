a3b179f0df018ce819bce55003fde5770c64ad10
Luke Pratley <Luke.Pratley.15@ucl.ac.uk>
2016-06-13 12:02:16 +0100

    removed old scripts, and replaced with master script

--- scripts/padmm_plot_m31_SNR_and_time.py
+++ scripts/padmm_plot_m31_SNR_and_time.py
@@ -7,9 +7,9 @@ import glob
 import subprocess
 import matplotlib.pyplot as plt
 
-
+image = "30dor_256"
 n_tests = 10
-input_SNR = 100
+input_SNR = 60
 
 def kernel_settings(kernel):
         J = 4
@@ -24,42 +24,51 @@ def kernel_settings(kernel):
         if kernel == "kb_min":
                 oversample = 1.375
                 J = 5
-	return J, oversample
-def run_test_padmm((i, kernel, M_N_ratio, start_time, input_SNR)):
+	if kernel == "kb_min4":
+		oversample = 1.375
+		J = 4
+		kernel = "kb_min"
+	if kernel == "kb_interp4":
+		oversample = 1.375
+		J = 4
+		kernel = "kb_interp"
+	
+	return J, oversample, kernel
+def run_test_padmm((i, kernel, M_N_ratio, start_time, input_SNR, image)):
 	time.sleep(start_time)
 
-	J, oversample = kernel_settings(kernel)
+	J, oversample, kernel = kernel_settings(kernel)
 
   	os.system("../build/cpp/example/padmm_m31_simulation " + kernel + " " 
-      + str(oversample) + " " +str(J) + " " +str(M_N_ratio) + " " + str(i) + " "+str(input_SNR))
-  	results_file = "../build/outputs/M31_results_" + kernel + "_" + str(i) + ".txt"
+      + str(oversample) + " " +str(J) + " " +str(M_N_ratio) + " " + str(i) + " "+str(input_SNR)+ " " + str(image))
+  	results_file = "../build/outputs/"+image+"_results_" + kernel + "_" + str(i) + ".txt"
   	results = np.loadtxt(results_file, dtype = str)
   	SNR = results[0]
   	total_time = results[1]
 	os.system("rm " + results_file)
   	return [float(SNR), float(total_time)]
-def run_test_ms_clean((i, kernel, M_N_ratio, start_time, input_SNR)):
+def run_test_ms_clean((i, kernel, M_N_ratio, start_time, input_SNR, image)):
 	time.sleep(start_time)
 	
-	J, oversample = kernel_settings(kernel)
+	J, oversample, kernel = kernel_settings(kernel)
 	
   	os.system("../build/cpp/example/padmm_ms_clean_m31_simulation " + kernel + " " 
-      + str(oversample) + " " +str(J) + " " +str(M_N_ratio) + " " + str(i) + " " + str(input_SNR))
-  	results_file = "../build/outputs/M31_results_" + kernel + "_" + str(i) + ".txt"
+      + str(oversample) + " " +str(J) + " " +str(M_N_ratio) + " " + str(i) + " " + str(input_SNR)+ " " +str(image))
+  	results_file = "../build/outputs/"+image+"_results_" + kernel + "_" + str(i) + ".txt"
   	results = np.loadtxt(results_file, dtype = str)
   	SNR = results[0]
   	total_time = results[1]
 	os.system("rm " + results_file)
   	return [float(SNR), float(total_time)]
 
-def run_test_clean((i, kernel, M_N_ratio, start_time, input_SNR)):
+def run_test_clean((i, kernel, M_N_ratio, start_time, input_SNR, image)):
 	time.sleep(start_time)
 
-	J, oversample = kernel_settings(kernel)
+	J, oversample, kernel = kernel_settings(kernel)
 
   	os.system("../build/cpp/example/padmm_clean_m31_simulation " + kernel + " " 
-      + str(oversample) + " " +str(J) + " " +str(M_N_ratio) + " " + str(i) + " " + str(input_SNR))
-  	results_file = "../build/outputs/M31_results_" + kernel + "_" + str(i) + ".txt"
+      + str(oversample) + " " +str(J) + " " +str(M_N_ratio) + " " + str(i) + " " + str(input_SNR) + " " + str(image))
+  	results_file = "../build/outputs/"+image+"results_" + kernel + "_" + str(i) + ".txt"
   	results = np.loadtxt(results_file, dtype = str)
   	SNR = results[0]
   	total_time = results[1]
@@ -100,6 +109,7 @@ def create_plots(args, results, M_N_ratios, name, kernels, colours,legend = []):
 	plt.xlabel("M/N")
 	plt.ylabel("SNR, db")
 	plt.xlim(0, 2.2)
+	plt.ylim(5, 60)
 	plt.savefig(name + "_SNR_plot.pdf")
 	plt.clf()
 
@@ -119,22 +129,28 @@ if __name__ == '__main__':
 	args = []
 
 	test_num = 0
-	kernels = ["kb", "kb_interp", "pswf", "gauss", "box", "gauss_alt", "kb_min"]
+	#kernels = ["kb", "kb_interp", "pswf", "gauss", "box", "gauss_alt", "kb_min", "kb_min4"]#, "kb_interp4"]
+	kernels = ["kb", "pswf", "gauss", "gauss_alt"]
 	total_tests = n_tests * len(kernels) * len(M_N_ratios)
 	for i in range(1, n_tests + 1):
 		for k in kernels:
 			for m in M_N_ratios:
 				test_num = test_num + 1
-				args.append((test_num, k, m, test_num * 1./ total_tests * 30., input_SNR))
+				args.append((test_num, k, m, test_num * 1./ total_tests * 30., input_SNR, image))
 				print test_num
 	n_processors = multiprocessing.cpu_count() + 1
 	p = multiprocessing.Pool(min(n_processors, 41)) # Limiting the number of processes used to 40, otherwise it will cause problems with the user limit
 	
-	legend = ["Kaiser Bessel (KB)", "KB (Linear-interp, Min-oversample)", "PSWF", "Gaussian (Optimal)", "Box", "Gaussian (non-Optimal)", "KB (Min-oversample)"]
-	colours = ['blue', 'red', 'black', 'green', 'magenta', 'cyan', 'yellow']
+	#legend = ["Kaiser Bessel (KB)", "KB (Linear-interp, Min-oversample)", "PSWF", "Gaussian (Optimal)", "Box", "Gaussian (non-Optimal)", "KB (Min-oversample)", "KB4 (Min-oversample)"]#, "KB4 (Linear-interp, Min-oversample)"]
+	#colours = ['blue', 'red', 'black', 'green', 'magenta', 'cyan', 'yellow', "#800000"]#, "#808000"]
+	legend = ["Kaiser Bessel (KB)", "PSWF", "Gaussian (Optimal)", "Gaussian (non-Optimal)"]
+	colours = ['blue', 'black', 'green', 'cyan']
 	results = p.map(run_test_padmm, args)
 	create_plots(args, results, M_N_ratios, "padmm", kernels, colours, legend)
+	print "PADMM Done!"
 	results = p.map(run_test_ms_clean, args)
 	create_plots(args, results, M_N_ratios, "ms_clean", kernels, colours)
+	print "MS CLEAN Done!"
 	results = p.map(run_test_clean, args)
 	create_plots(args, results, M_N_ratios, "clean", kernels, colours)
+	print "All Done!"
