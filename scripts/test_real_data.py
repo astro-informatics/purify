c34f540aa93bc8882e0ad68d28f74f5a614f719d
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2016-07-25 11:40:15 +0100

    Added dataset names to real data script

--- scripts/test_real_data.py
+++ scripts/test_real_data.py
@@ -7,6 +7,8 @@ import glob
 import subprocess
 import matplotlib.pyplot as plt
 
+nice_value = 0
+
 home = os.path.expanduser("~")
 
 outpath = home + "/Purify_RealData/"
@@ -14,10 +16,9 @@ data = home + "/dev/purify/data/"
 
 atca = "atca/"
 vla = "vla/"
-names = [atca + "0332-391"]
-ra_values = []
-dec_values = []
-params = []
+names = [atca + "0332-391", atca + "0114-476", vla + "CygA-X", vla + "3C129BC"]
+
+
 
 def make_path(name, outpath, epsilon_factor):
 	return outpath + name + "/" + str(epsilon_factor) + "/"
@@ -26,25 +27,33 @@ def make_directory(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 
-def run_test((name, inpath, size, epsilon_factor, ra, dec)):
+def run_test((name, inpath, size, epsilon_factor)):
 	path = make_path(name, outpath, epsilon_factor)
-	print path
+	
 	make_directory(path)
-	os.system("screen -S "+ name.split("/")[-1] + "_" + str(epsilon_factor) + " -d -m ~/dev/purify/build/cpp/example/purify_main" +
+	os.system("nice -" + str(nice_value) + " screen -S "+ name.split("/")[-1] + "_" + str(epsilon_factor) + " -d -m ~/dev/purify/build/cpp/example/purify_main" +
 		" --name " + path + name.split("/")[-1] +
 		" --vis " + inpath + "I.vis --n_mean " + str(epsilon_factor) + 
-		" --size " + str(size) + " --ra " + str(ra) + " --dec " + str(dec) +
+		" --size " + str(size) +
 		" --update --diagnostic"
 		)
 	return
-
+	
+params = []
 for n in names:
-	image_size = 512
-	params.append((n, data + n, image_size, 1))
-	params.append((n, data + n, image_size, 1.2))
-	params.append((n, data + n, image_size, 1.3))
-	params.append((n, data + n, image_size, 1.4))
-	params.append((n, data + n, image_size, 1.5))
+	print "Looking for " + data + n
+	if os.path.exists(data + n + "I.vis"):
+		print n + " found!"
+		image_size = 512
+		if n == atca + "0114-476":
+			image_size = 1024
+		params.append((n, data + n, image_size, 1))
+		params.append((n, data + n, image_size, 1.2))
+		params.append((n, data + n, image_size, 1.3))
+		params.append((n, data + n, image_size, 1.4))
+		params.append((n, data + n, image_size, 1.5))
+	else:
+		print n + " not found!"
 
 n_processors = multiprocessing.cpu_count() + 1
 p = multiprocessing.Pool(min(n_processors, 5))
