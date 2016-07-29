08e668dea8a6e15f40651c53535b90724d97c9e9
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2016-07-29 19:47:54 +0100

    Add config template file

---
+++ cpp/config.in.h
@@ -0,0 +1,31 @@
+#ifndef PURIFY_CPP_CONFIG_H
+#define PURIFY_CPP_CONFIG_H
+
+//! Problems with using and constructors
+#cmakedefine PURIFY_HAS_USING
+#ifndef PURIFY_HAS_USING
+#define PURIFY_HAS_NOT_USING
+#endif
+
+//! Macro to start logging or not
+#cmakedefine PURIFY_DO_LOGGING
+
+#include <string>
+#include <tuple>
+
+namespace purify {
+  //! Returns library version
+  inline std::string version() { return "@Purify_VERSION@"; }
+  //! Returns library version
+  inline std::tuple<uint8_t, uint8_t, uint8_t> version_tuple() {
+    return std::tuple<uint8_t, uint8_t, uint8_t>(
+                                                 @Purify_VERSION_MAJOR@, @Purify_VERSION_MINOR@, @Purify_VERSION_PATCH@);
+  }
+  //! Returns library git reference, if known
+  inline std::string gitref() { return "@Purify_GITREF@"; }
+  //! Default logging level
+  inline std::string default_logging_level() { return "@PURIFY_TEST_LOG_LEVEL@"; }
+}
+
+#endif
+
