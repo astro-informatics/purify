df9e21d8e0162c5d1635736467baefec884b583a
luke <Luke.Pratley.15@ucl.ac.uk>
2016-09-09 09:50:49 +0100

    updated test real data script

--- scripts/test_real_data.py
+++ scripts/test_real_data.py
@@ -20,7 +20,7 @@ outpath = home + "/purify_real_data/"
 if stokes_v_calculation:
 	outpath = home + "/purify_real_data_v/"
 
-data = home + "/dev/purify/data/"
+data = home + "/Dropbox/Visibility_Files/"
 
 atca = "atca/"
 vla = "vla/"
@@ -48,7 +48,13 @@ def run_test((name, inpath, cellsize, size, epsilon_factor)):
 		noise +
 		" --cellsize " + cellsize +
 		" --size " + str(size) +
-		" --update --diagnostic power_iterations 200"], shell=True)
+		" --update --diagnostic power_iterations 200" +
+                " --residual_convergence 0. " +
+                " --relative_variation 0.0001" +
+                " --adapt_iter 100" +
+                " --niters 200" +
+                " --relative_gamma_adapt 0.01"
+        ], shell=True)
 	print "Waiting for " + path
 	while not os.path.exists(path + name.split("/")[-1] + "_solution_whiten_final.fits"):
 		time.sleep(10)
@@ -56,7 +62,7 @@ def run_test((name, inpath, cellsize, size, epsilon_factor)):
 
 params = []
 for i in range(len(names)):
-	i = 3
+	i = 2
 	n = names[i]
 	print "Looking for " + data + n
 	if os.path.exists(data + n + ".ms"):
@@ -64,13 +70,13 @@ for i in range(len(names)):
 		image_size = imsize[i] 
 		pix_size = cellsizes[i]
 		#params.append((n, data + n, pix_size, image_size, 1))
-		params.append((n, data + n, pix_size, image_size, 1.25))
-		params.append((n, data + n, pix_size, image_size, 1.3))
-		params.append((n, data + n, pix_size, image_size, 1.4))
+		params.append((n, data + n, pix_size, image_size, 2))
+		#params.append((n, data + n, pix_size, image_size, 1.3))
+		#params.append((n, data + n, pix_size, image_size, 1.4))
 		#params.append((n, data + n, pix_size, image_size, 2.5))
 	else:
 		print n + " not found!"
 	break
 n_processors = multiprocessing.cpu_count() + 1
-p = multiprocessing.Pool(min(n_processors, 20))
+p = multiprocessing.Pool(min(n_processors, 1))
 p.map(run_test, params)
