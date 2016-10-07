45a5c1be1475f0613281246960b2a77729e87002
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-10-07 17:15:55 +0100

    Correcting tags for downloading sopt
    It's v2.0.0 by default.

--- cmake_files/dependencies.cmake
+++ cmake_files/dependencies.cmake
@@ -51,17 +51,7 @@ endif()
 # Unless otherwise specified, if purify is not on master, then sopt will be
 # downloaded from development branch.
 if(NOT sopt_tag)
-    execute_process(COMMAND git branch
-        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-        RESULT_VARIABLE got_branch
-        OUTPUT_VARIABLE current_branch)
-    if(NOT got_branch)
-        set(sopt_tag master)
-    endif()
-    if(NOT current_branch STREQUAL master)
-        set(sopt_tag v2.0)
-    endif()
-    set(sopt_tag ${sopt_tag} CACHE STRING "Branch/tag when downloading sopt")
+    set(sopt_tag v2.0.0 CACHE STRING "Branch/tag when downloading sopt")
 endif()
 if(NOT Sopt_FOUND)
   message(STATUS "If downloading Sopt locally, then it will be branch ${sopt_tag}")
