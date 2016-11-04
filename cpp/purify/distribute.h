3378694383220499f98c36b9a89df6dbc6f0678b
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-11-04 17:33:35 +0200

    Test for serial vs parallel does not quite work
    But inpainting test does, so probably some parameter is not equivalent
    in serial and parallel

--- cpp/purify/distribute.h
+++ cpp/purify/distribute.h
@@ -4,8 +4,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <sopt/mpi/communicator.h>
 #include "purify/logging.h"
 #include "purify/types.h"
+#include "purify/utilities.h"
 
 namespace purify {
 namespace distribute {
@@ -15,6 +17,14 @@ distribute_measurements(Vector<t_real> const &u, Vector<t_real> const &v, Vector
                         t_int const number_of_nodes,
                         std::string const &distribution_plan = "equal_distribution",
                         t_int const &grid_size = 128);
+
+inline std::vector<t_int>
+distribute_measurements(utilities::vis_params const &params, sopt::mpi::Communicator const &comm,
+                        std::string const &distribution_plan = "equal_distribution",
+                        t_int const &grid_size = 128) {
+  return distribute_measurements(params.u, params.v, params.w, comm.size(), distribution_plan,
+                                 grid_size);
+}
 //! Distribute visiblities into nodes in order of distance from the centre
 Vector<t_int> distance_distribution(Vector<t_real> const &u, Vector<t_real> const &v);
 //! Distribute the visiblities into nodes in order of density
