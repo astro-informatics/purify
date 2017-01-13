c84110dc0f6d86732308863894db326b62de8ba7
luke <Luke.Pratley.15@ucl.ac.uk>
2017-01-13 10:35:26 +0000

    some reformatting

--- cpp/purify/distribute.h
+++ cpp/purify/distribute.h
@@ -10,26 +10,26 @@
 #include "purify/utilities.h"
 
 namespace purify {
-namespace distribute {
-//! Distribute visiblities into groups
-std::vector<t_int>
-distribute_measurements(Vector<t_real> const &u, Vector<t_real> const &v, Vector<t_real> const &w,
-                        t_int const number_of_nodes,
-                        std::string const &distribution_plan = "equal_distribution",
-                        t_int const &grid_size = 128);
+  namespace distribute {
+    //! Distribute visiblities into groups
+    std::vector<t_int>
+      distribute_measurements(Vector<t_real> const &u, Vector<t_real> const &v, Vector<t_real> const &w,
+          t_int const number_of_nodes,
+          std::string const &distribution_plan = "equal_distribution",
+          t_int const &grid_size = 128);
 
-inline std::vector<t_int>
-distribute_measurements(utilities::vis_params const &params, sopt::mpi::Communicator const &comm,
-                        std::string const &distribution_plan = "equal_distribution",
-                        t_int const &grid_size = 128) {
-  return distribute_measurements(params.u, params.v, params.w, comm.size(), distribution_plan,
-                                 grid_size);
-}
-//! Distribute visiblities into nodes in order of distance from the centre
-Vector<t_int> distance_distribution(Vector<t_real> const &u, Vector<t_real> const &v);
-//! Distribute the visiblities into nodes in order of density
-Vector<t_int>
-equal_distribution(Vector<t_real> const &u, Vector<t_real> const &v, t_int const &grid_size);
-}
+    inline std::vector<t_int>
+      distribute_measurements(utilities::vis_params const &params, sopt::mpi::Communicator const &comm,
+          std::string const &distribution_plan = "equal_distribution",
+          t_int const &grid_size = 128) {
+        return distribute_measurements(params.u, params.v, params.w, comm.size(), distribution_plan,
+            grid_size);
+      }
+    //! Distribute visiblities into nodes in order of distance from the centre
+    Vector<t_int> distance_distribution(Vector<t_real> const &u, Vector<t_real> const &v);
+    //! Distribute the visiblities into nodes in order of density
+    Vector<t_int>
+      equal_distribution(Vector<t_real> const &u, Vector<t_real> const &v, t_int const &grid_size);
+  }
 }
 #endif
