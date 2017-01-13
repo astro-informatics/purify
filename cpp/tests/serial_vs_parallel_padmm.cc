6bf64f1ea9bbc480fe7b6d4290ec180c7f7f88c9
luke <Luke.Pratley.15@ucl.ac.uk>
2017-01-13 10:48:51 +0000

    mostly reformatting of mph padmm test

--- cpp/tests/serial_vs_parallel_padmm.cc
+++ cpp/tests/serial_vs_parallel_padmm.cc
@@ -18,7 +18,7 @@
 #include "purify/utilities.h"
 
 
-TEST_CASE("Parallel vs serial inpainting") {
+TEST_CASE("Serial vs. Parallel PADMM with random coverage.") {
   extern std::unique_ptr<std::mt19937_64> mersenne;
   using namespace sopt;
   auto const world = mpi::Communicator::World();
@@ -40,9 +40,9 @@ TEST_CASE("Parallel vs serial inpainting") {
   std::string const fitsfile = image_filename("30dor_256.fits");
 
   PURIFY_MEDIUM_LOG("Starting Purify");
-  
+
   auto sky_model = world.is_root() ? world.broadcast(pfitsio::read2d(fitsfile)) :
-                                     world.broadcast<sopt::Image<t_complex>>();
+    world.broadcast<sopt::Image<t_complex>>();
   auto sky_model_max = sky_model.array().abs().maxCoeff();
   sky_model = sky_model / sky_model_max;
   t_int const number_of_vis = std::floor(m_over_n * sky_model.size());
@@ -53,7 +53,7 @@ TEST_CASE("Parallel vs serial inpainting") {
   PURIFY_MEDIUM_LOG("Number of measurements: {}", uv_data.u.size());
 
   MeasurementOperator sky_measurements(uv_data, 4, 4, "kb", sky_model.cols(), sky_model.rows(), 100,
-                                       over_sample);
+      over_sample);
   // working out value of sigma given SNR of 30
   auto const sigma = world.broadcast(utilities::SNR_to_standard_deviation(uv_data.vis, ISNR));
   // adding noise to visibilities
@@ -65,19 +65,19 @@ TEST_CASE("Parallel vs serial inpainting") {
   uv_data.weights = world.broadcast(uv_data.weights);
   uv_data.vis = world.broadcast(uv_data.vis);
   if(split_comm.size() > 1 and split_comm.is_root()) {
-    auto const order = distribute::distribute_measurements(uv_data, split_comm);
+    auto const order = distribute::distribute_measurements(uv_data, split_comm, "distance_distribution");
     uv_data = utilities::regroup_and_scatter(uv_data, order, split_comm);
   } else if(split_comm.size() > 1)
     uv_data = utilities::scatter_visibilities(split_comm);
 
   MeasurementOperator measurements(uv_data, J, J, kernel, sky_model.cols(), sky_model.rows(), 100,
-                                   over_sample);
+      over_sample);
   auto measurements_transform = linear_transform(measurements, uv_data.vis.size());
 
   auto const wavelets = {std::make_tuple("DB4", 3u)};
   sopt::wavelets::SARA const sara(wavelets.begin(), wavelets.end());
   auto const Psi
-      = sopt::linear_transform<t_complex>(sara, measurements.imsizey(), measurements.imsizex());
+    = sopt::linear_transform<t_complex>(sara, measurements.imsizey(), measurements.imsizex());
 
   sopt::Vector<> dimage = world.broadcast((measurements_transform.adjoint() * uv_data.vis).real());
   t_real const max_val = dimage.array().abs().maxCoeff();
@@ -93,58 +93,58 @@ TEST_CASE("Parallel vs serial inpainting") {
   PURIFY_MEDIUM_LOG("Epsilon {}", epsilon);
   PURIFY_MEDIUM_LOG("Gamma {}", purify_gamma);
   auto padmm
-      = sopt::algorithm::ImagingProximalADMM<t_complex>(uv_data.vis)
-            .gamma(purify_gamma)
-            .relative_variation(1e-3)
-            .l2ball_proximal(
-                sopt::proximal::WeightedL2Ball<sopt::t_complex>(epsilon).communicator(split_comm))
-            .tight_frame(false)
-            .l1_proximal_tolerance(1e-2)
-            .l1_proximal_nu(1)
-            .l1_proximal_itermax(50)
-            .l1_proximal_positivity_constraint(true)
-            .l1_proximal_real_constraint(true)
-            .lagrange_update_scale(0.9)
-            .nu(1e0)
-            .Psi(Psi)
-            .itermax(5);
+    = sopt::algorithm::ImagingProximalADMM<t_complex>(uv_data.vis)
+    .gamma(purify_gamma)
+    .relative_variation(1e-3)
+    .l2ball_proximal(
+        sopt::proximal::WeightedL2Ball<sopt::t_complex>(epsilon).communicator(split_comm))
+    .tight_frame(false)
+    .l1_proximal_tolerance(1e-2)
+    .l1_proximal_nu(1)
+    .l1_proximal_itermax(50)
+    .l1_proximal_positivity_constraint(true)
+    .l1_proximal_real_constraint(true)
+    .lagrange_update_scale(0.9)
+    .nu(1e0)
+    .Psi(Psi)
+    .itermax(5);
   LinearTransform<sopt::Vector<t_complex>> const parallel_phi(
       [&measurements_transform](sopt::Vector<t_complex> &out,
-                                sopt::Vector<t_complex> const &input) {
-        out = measurements_transform * input;
+        sopt::Vector<t_complex> const &input) {
+      out = measurements_transform * input;
       },
       measurements_transform.sizes(),
       [&measurements_transform, split_comm](sopt::Vector<t_complex> &out,
-                                            sopt::Vector<t_complex> const &input) {
-        out = measurements_transform.adjoint() * input;
-        split_comm.all_sum_all(out);
+        sopt::Vector<t_complex> const &input) {
+      out = measurements_transform.adjoint() * input;
+      split_comm.all_sum_all(out);
       },
       measurements_transform.adjoint().sizes());
 
   padmm.Phi(parallel_phi);
   padmm.residual_convergence(
       [&padmm, epsilon, split_comm, world](sopt::Vector<t_complex> const &residual) {
-        auto const tolerance = epsilon * 1.001;
-        auto const residual_norm
-            = sopt::mpi::l2_norm(residual, padmm.l2ball_proximal_weights(), split_comm);
-        SOPT_LOW_LOG("    - Residuals: epsilon = {}, residual norm = {}", tolerance, residual_norm);
-        auto const result = residual_norm < tolerance;
-        CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
-        return result;
+      auto const tolerance = epsilon * 1.001;
+      auto const residual_norm
+      = sopt::mpi::l2_norm(residual, padmm.l2ball_proximal_weights(), split_comm);
+      SOPT_LOW_LOG("    - Residuals: epsilon = {}, residual norm = {}", tolerance, residual_norm);
+      auto const result = residual_norm < tolerance;
+      CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
+      return result;
       });
   sopt::algorithm::ImagingProximalADMM<t_complex>::ObjectiveConvergence conv;
   padmm.objective_convergence([&padmm, conv, split_comm,
-                               world](sopt::Vector<t_complex> const &residual) mutable -> bool {
-    auto const result = conv(padmm, sopt::mpi::l1_norm(residual + padmm.target(),
-                                                       padmm.l1_proximal_weights(), split_comm));
-    CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
-    return result;
-  });
+      world](sopt::Vector<t_complex> const &residual) mutable -> bool {
+      auto const result = conv(padmm, sopt::mpi::l1_norm(residual + padmm.target(),
+            padmm.l1_proximal_weights(), split_comm));
+      CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
+      return result;
+      });
   padmm.is_converged([world](sopt::Vector<t_complex> const &image) -> bool {
-    auto const from_root = world.broadcast(image);
-    CHECK(from_root.isApprox(image, 1e-12));
-    return true;
-  });
+      auto const from_root = world.broadcast(image);
+      CHECK(from_root.isApprox(image, 1e-12));
+      return true;
+      });
 
   auto const diagnostic = padmm();
   CHECK(diagnostic.good == (world.broadcast<int>(diagnostic.good, world.root_id()) != 0));
