6e89046d4f37d2c521ef68091c8509439076b78f
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-02-22 10:31:09 +0000

    Add logger initialization to purify tests

---
+++ cpp/tests/common_catch_main.cc
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_RUNNER
+
+#include "purify/config.h"
+#include <sopt/config.h>
+#include <catch.hpp>
+#include <memory>
+#include <random>
+#include <sopt/logging.h>
+#include "purify/logging.h"
+
+std::unique_ptr<std::mt19937_64> mersenne(new std::mt19937_64(0));
+
+int main(int argc, char const **argv) {
+  Catch::Session session; // There must be exactly once instance
+
+  int returnCode = session.applyCommandLine(argc, argv);
+  if(returnCode != 0) // Indicates a command line error
+    return returnCode;
+  mersenne.reset(new std::mt19937_64(session.configData().rngSeed));
+
+  sopt::logging::initialize();
+  purify::logging::initialize();
+
+  return session.run();
+}
