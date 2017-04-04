008a9904573b6513fced626bf799efda102acd17
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-04-04 13:34:01 +0100

    Adds function to get non-empty outer vectors of sparse matrix

--- cpp/purify/IndexMapping.h
+++ cpp/purify/IndexMapping.h
@@ -2,40 +2,52 @@
 #define PURIFY_MAPPING_OPERATOR_H
 
 #include "purify/config.h"
-#include "purify/types.h"
 #include <set>
+#include "purify/types.h"
 
 namespace purify {
-  class IndexMapping {
 
-    public:
-    IndexMapping(const std::set<t_int> & indices, const t_int size) : indices(indices), size(size) {};
+class IndexMapping {
+public:
+  IndexMapping(const std::set<t_int> &indices, const t_int size) : indices(indices), size(size){};
 
-    //! Creates a vector of elements of input with given indices
-    template<class T0, class T1> void operator()(Eigen::MatrixBase<T0> const &input, Eigen::MatrixBase<T1> const &output) const {
-      const_cast<Eigen::MatrixBase<T1>&>(output).derived().resize(indices.size());
-      typename T0::Index i(0);
-      for(auto const &index: indices) {
-        assert(index >= 0 and index <= input.size());
-        const_cast<Eigen::MatrixBase<T1>&>(output)(i++) = input(index);
-      }
+  //! Creates a vector of elements equal to re-indexed input
+  template <class T0, class T1>
+  void operator()(Eigen::MatrixBase<T0> const &input, Eigen::MatrixBase<T1> const &output) const {
+    const_cast<Eigen::MatrixBase<T1> &>(output).derived().resize(indices.size());
+    typename T0::Index i(0);
+    for(auto const &index : indices) {
+      assert(index >= 0 and index <= input.size());
+      const_cast<Eigen::MatrixBase<T1> &>(output)(i++) = input(index);
     }
+  }
 
-    //! Creates a vector of elements of input with given indices
-    template<class T0, class T1> void adjoint(Eigen::MatrixBase<T0> const &input, Eigen::MatrixBase<T1> const &output) const {
-      assert(indices.size() <= size);
-      assert(input.size() == indices.size());
-      const_cast<Eigen::MatrixBase<T1>&>(output) = T1::Zero(size);
-      typename T0::Index i(0);
-      for(auto const &index: indices) {
-        assert(index >= 0 and index <= output.size());
-        const_cast<Eigen::MatrixBase<T1>&>(output)(index) = input(i++);
-      }
+  //! Vector of size `size` where non-zero elements are chosen from input at given indices
+  template <class T0, class T1>
+  void adjoint(Eigen::MatrixBase<T0> const &input, Eigen::MatrixBase<T1> const &output) const {
+    assert(indices.size() <= size);
+    assert(input.size() == indices.size());
+    auto &derived = output.const_cast_derived();
+    derived = T1::Zero(size);
+    typename T0::Index i(0);
+    for(auto const &index : indices) {
+      assert(index >= 0 and index <= output.size());
+      derived(index) = input(i++);
     }
+  }
 
-    private:
-    std::set<t_int> indices;
-    t_int size;
-  };
+private:
+  std::set<t_int> indices;
+  t_int size;
+};
+
+//! Indices of non empty outer indices
+template <class T0> std::set<t_int> non_empty_outers(Eigen::SparseMatrixBase<T0> const &matrix) {
+  std::set<t_int> result;
+  for(typename T0::StorageIndex k = 0; k < matrix.outerSize(); ++k)
+    for(typename T0::InnerIterator it(matrix, k); it; ++it)
+      result.insert(it.col());
+  return result;
+}
 }
 #endif
--- cpp/tests/index_mapping.cc
+++ cpp/tests/index_mapping.cc
@@ -1,14 +1,13 @@
-
+#include <iostream>
 #include "purify/config.h"
-#include "catch.hpp"
-#include "purify/types.h"
-#include "purify/IndexMapping.h"
 #include <numeric>
+#include "catch.hpp"
+#include "purify/IndexMapping.h"
+#include "purify/types.h"
 
 using namespace purify;
 
-TEST_CASE("") {
-
+TEST_CASE("Index mapping operator") {
   std::set<t_int> indices{1, 3, 5, 2};
   auto mapper = IndexMapping(indices, 10);
   Vector<t_int> input(10);
@@ -16,12 +15,11 @@ TEST_CASE("") {
 
   std::iota(input.data(), input.data() + input.size(), 0);
 
-
   mapper(input, output);
   SECTION("Direct application") {
     CHECK(output.size() == 4);
     decltype(output)::Index i(0);
-    for(auto const &index: indices)
+    for(auto const &index : indices)
       CHECK(output(i++) == index);
   }
 
@@ -29,10 +27,21 @@ TEST_CASE("") {
   mapper.adjoint(output, adjoint);
   SECTION("Adjoint application") {
     CHECK(adjoint.size() == 10);
-    for(auto const &index: indices) {
+    for(auto const &index : indices) {
       CHECK(adjoint(index) == index);
       adjoint(index) = 0;
     }
     CHECK(adjoint == adjoint.Zero(adjoint.size()));
   }
+}
+
+TEST_CASE("Non empty outer vectors") {
+  Sparse<t_int> matrix(4, 4);
+  std::vector<Eigen::Triplet<t_int>> const triplets = {{0, 0, 1}, {0, 3, 1}, {2, 3, 1}};
+  matrix.setFromTriplets(triplets.begin(), triplets.end());
+
+  auto const indices = non_empty_outers(matrix);
+  CHECK(indices.size() == 2);
+  CHECK(indices.count(0) == 1);
+  CHECK(indices.count(3) == 1);
 }
