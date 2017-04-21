87c9e6704fb19a31e7d88fe8effa441c43a77a76
luke <Luke.Pratley.15@ucl.ac.uk>
2017-04-22 00:34:07 +0100

    updated mpi measurement operator test

--- cpp/tests/mpi_measurement_operator.cc
+++ cpp/tests/mpi_measurement_operator.cc
@@ -132,11 +132,74 @@ TEST_CASE("Serial vs Distributed Fourier Grid") {
 }
 
 TEST_CASE("Serial vs Distributed Fourier Grid Operator") {
-  sopt::logging::set_level("debug");
-  purify::logging::set_level("debug");
   auto const world = sopt::mpi::Communicator::World();
 
-  auto const N = 1000;
+  auto const N = 5;
+  auto uv_serial = utilities::random_sample_density(N, 0, constant::pi / 3);
+  uv_serial.u = world.broadcast(uv_serial.u);
+  uv_serial.v = world.broadcast(uv_serial.v);
+  uv_serial.w = world.broadcast(uv_serial.w);
+  uv_serial.units = "radians";
+  uv_serial.vis = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(uv_serial.u.size()));
+  uv_serial.weights
+      = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(uv_serial.u.size()));
+
+  utilities::vis_params uv_mpi;
+  if(world.is_root()) {
+    auto const order
+        = distribute::distribute_measurements(uv_serial, world, "distance_distribution");
+    uv_mpi = utilities::regroup_and_scatter(uv_serial, order, world);
+  } else
+    uv_mpi = utilities::scatter_visibilities(world);
+
+  auto const over_sample = 2;
+  auto const J = 4;
+  auto const kernel = "kb";
+  auto const width = 128;
+  auto const height = 128;
+  const sopt::LinearTransform<Vector<t_complex>> op_serial
+      = purify::measurementoperator::init_degrid_operator_2d<Vector<t_complex>>(
+          uv_serial.u, uv_serial.v, height, width, over_sample, 100);
+
+  const sopt::LinearTransform<Vector<t_complex>> op
+      = purify::measurementoperator::init_degrid_operator_2d_mpi<Vector<t_complex>>(
+          world, uv_mpi.u, uv_mpi.v, height, width, over_sample, 100);
+
+  if(uv_serial.u.size() == uv_mpi.u.size()) {
+    REQUIRE(uv_serial.u.isApprox(uv_mpi.u));
+    CHECK(uv_serial.v.isApprox(uv_mpi.v));
+    CHECK(uv_serial.weights.isApprox(uv_mpi.weights));
+  }
+  SECTION("Degridding") {
+    Vector<t_complex> const image
+        = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(width * height));
+
+    auto uv_degrid = uv_serial;
+    if(world.is_root()) {
+      uv_degrid.vis = op_serial * image;
+      auto const order
+          = distribute::distribute_measurements(uv_degrid, world, "distance_distribution");
+      uv_degrid = utilities::regroup_and_scatter(uv_degrid, order, world);
+    } else
+      uv_degrid = utilities::scatter_visibilities(world);
+    Vector<t_complex> const degridded = op * image;
+    REQUIRE(degridded.size() == uv_degrid.vis.size());
+    REQUIRE(degridded.isApprox(uv_degrid.vis, 1e-4));
+  }
+  SECTION("Gridding") {
+    Vector<t_complex> const gridded = op.adjoint() * uv_mpi.vis;
+    Vector<t_complex> const gridded_serial = op_serial.adjoint() * uv_serial.vis;
+    REQUIRE(gridded.size() == gridded_serial.size());
+    REQUIRE(gridded.isApprox(gridded_serial, 1e-4));
+  }
+}
+
+TEST_CASE("Serial vs Distributed Fourier Grid Operator weighted") {
+  // sopt::logging::set_level("debug");
+  // purify::logging::set_level("debug");
+  auto const world = sopt::mpi::Communicator::World();
+
+  auto const N = 5;
   auto uv_serial = utilities::random_sample_density(N, 0, constant::pi / 3);
   uv_serial.u = world.broadcast(uv_serial.u);
   uv_serial.v = world.broadcast(uv_serial.v);
@@ -161,20 +224,19 @@ TEST_CASE("Serial vs Distributed Fourier Grid Operator") {
   auto const height = 128;
   const sopt::LinearTransform<Vector<t_complex>> op_serial
       = purify::measurementoperator::init_degrid_weighted_operator_2d<Vector<t_complex>>(
-          uv_serial.u, uv_serial.v, uv_serial.weights, height, width, over_sample);
+          uv_serial.u, uv_serial.v, uv_serial.weights, height, width, over_sample, 100);
 
   const sopt::LinearTransform<Vector<t_complex>> op
       = purify::measurementoperator::init_degrid_weighted_operator_2d_mpi<Vector<t_complex>>(
-          world, uv_mpi.u, uv_mpi.v, uv_mpi.weights, height, width, over_sample);
-  /*
-  SECTION("Gridding") {
-    auto const gridded = op.adjoint() * uv_mpi.vis;
-    auto const gridded_serial = op_serial.adjoint() * uv_serial.vis;
-    CHECK(gridded.isApprox(gridded_serial));
-  }
+          world, uv_mpi.u, uv_mpi.v, uv_mpi.weights, height, width, over_sample, 100);
 
+  if(world.size() == 1) {
+    REQUIRE(uv_serial.u.isApprox(uv_mpi.u));
+    CHECK(uv_serial.v.isApprox(uv_mpi.v));
+    CHECK(uv_serial.weights.isApprox(uv_mpi.weights));
+  }
   SECTION("Degridding") {
-    auto const image
+    Vector<t_complex> const image
         = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(width * height));
 
     auto uv_degrid = uv_serial;
@@ -185,10 +247,14 @@ TEST_CASE("Serial vs Distributed Fourier Grid Operator") {
       uv_degrid = utilities::regroup_and_scatter(uv_degrid, order, world);
     } else
       uv_degrid = utilities::scatter_visibilities(world);
-
     Vector<t_complex> const degridded = op * image;
     REQUIRE(degridded.size() == uv_degrid.vis.size());
-    CHECK(degridded.isApprox(uv_degrid.vis));
+    REQUIRE(degridded.isApprox(uv_degrid.vis, 1e-4));
   }
-*/
+  SECTION("Gridding") {
+    Vector<t_complex> const gridded = op.adjoint() * uv_mpi.vis;
+    Vector<t_complex> const gridded_serial = op_serial.adjoint() * uv_serial.vis;
+    REQUIRE(gridded.size() == gridded_serial.size());
+    REQUIRE(gridded.isApprox(gridded_serial, 1e-4));
+  }
 }
