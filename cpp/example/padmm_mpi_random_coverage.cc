8c6a099abc864b0e78e326d6364c0ffccaea04ab
luke <Luke.Pratley.15@ucl.ac.uk>
2017-04-24 14:53:37 +0100

    fixed compile error on old compilers

--- cpp/example/padmm_mpi_random_coverage.cc
+++ cpp/example/padmm_mpi_random_coverage.cc
@@ -31,33 +31,33 @@ dirty_visibilities(Image<t_complex> const &ground_truth_image, t_uint number_of_
   auto uv_data = utilities::random_sample_density(number_of_vis, 0, constant::pi / 3);
   uv_data.units = "radians";
   PURIFY_HIGH_LOG("Number of measurements / number of pixels: {}",
-                  uv_data.u.size() / ground_truth_image.size());
+      uv_data.u.size() / ground_truth_image.size());
   // creating operator to generate measurements
   auto const sky_measurements = std::make_shared<sopt::LinearTransform<Vector<t_complex>> const>(
       measurementoperator::init_degrid_operator_2d<Vector<t_complex>>(
-          uv_data, ground_truth_image.rows(), ground_truth_image.cols(), 1., 1., 2, 100, 1e-4, "kb",
-          8, 8));
+        uv_data, ground_truth_image.rows(), ground_truth_image.cols(), 1., 1., 2, 100, 1e-4, "kb",
+        8, 8));
   // Generates measurements from image
   uv_data.vis = (*sky_measurements)
-                * Image<t_complex>::Map(ground_truth_image.data(), ground_truth_image.size(), 1);
+    * Image<t_complex>::Map(ground_truth_image.data(), ground_truth_image.size(), 1);
 
   // working out value of signal given SNR of 30
   auto const sigma = utilities::SNR_to_standard_deviation(uv_data.vis, snr);
   // adding noise to visibilities
   uv_data.vis = utilities::add_noise(uv_data.vis, 0., sigma);
-  return {uv_data, sigma};
+  return std::make_tuple(uv_data, sigma);
 }
 
 std::tuple<utilities::vis_params, t_real>
 dirty_visibilities(Image<t_complex> const &ground_truth_image, t_uint number_of_vis, t_real snr,
-                   sopt::mpi::Communicator const &comm) {
+    sopt::mpi::Communicator const &comm) {
   if(comm.size() == 1)
     return dirty_visibilities(ground_truth_image, number_of_vis, snr);
   if(comm.is_root()) {
     auto result = dirty_visibilities(ground_truth_image, number_of_vis, snr);
     comm.broadcast(std::get<1>(result));
     auto const order
-        = distribute::distribute_measurements(std::get<0>(result), comm, "distance_distribution");
+      = distribute::distribute_measurements(std::get<0>(result), comm, "distance_distribution");
     std::get<0>(result) = utilities::regroup_and_scatter(std::get<0>(result), order, comm);
     return result;
   }
@@ -67,12 +67,12 @@ dirty_visibilities(Image<t_complex> const &ground_truth_image, t_uint number_of_
 
 std::shared_ptr<sopt::algorithm::ImagingProximalADMM<t_complex>>
 padmm_factory(std::shared_ptr<sopt::LinearTransform<Vector<t_complex>> const> const &measurements,
-              const sopt::wavelets::SARA &sara, const Image<t_complex> &ground_truth_image,
-              const utilities::vis_params &uv_data, const t_real sigma,
-              const sopt::mpi::Communicator &comm) {
+    const sopt::wavelets::SARA &sara, const Image<t_complex> &ground_truth_image,
+    const utilities::vis_params &uv_data, const t_real sigma,
+    const sopt::mpi::Communicator &comm) {
 
   auto const Psi = sopt::linear_transform<t_complex>(sara, ground_truth_image.rows(),
-                                                     ground_truth_image.cols(), comm);
+      ground_truth_image.cols(), comm);
 
 #if PURIFY_PADMM_ALGORITHM == 2
   auto const epsilon = std::sqrt(
@@ -81,7 +81,7 @@ padmm_factory(std::shared_ptr<sopt::LinearTransform<Vector<t_complex>> const> co
   auto const epsilon = utilities::calculate_l2_radius(uv_data.vis, sigma);
 #endif
   auto const gamma
-      = (Psi.adjoint() * (measurements->adjoint() * uv_data.vis)).cwiseAbs().maxCoeff() * 1e-3;
+    = (Psi.adjoint() * (measurements->adjoint() * uv_data.vis)).cwiseAbs().maxCoeff() * 1e-3;
   PURIFY_MEDIUM_LOG("Epsilon {}", epsilon);
   PURIFY_MEDIUM_LOG("Gamma {}", gamma);
 
@@ -89,55 +89,55 @@ padmm_factory(std::shared_ptr<sopt::LinearTransform<Vector<t_complex>> const> co
   // not reproduce. E.g. padmm definition is self-referential.
   auto padmm = std::make_shared<sopt::algorithm::ImagingProximalADMM<t_complex>>(uv_data.vis);
   padmm->itermax(50)
-      .gamma(comm.all_reduce(gamma, MPI_MAX))
-      .relative_variation(1e-3)
-      .l2ball_proximal_epsilon(epsilon)
+    .gamma(comm.all_reduce(gamma, MPI_MAX))
+    .relative_variation(1e-3)
+    .l2ball_proximal_epsilon(epsilon)
 #if PURIFY_PADMM_ALGORITHM == 2
-      // communicator ensuring l2 norm in l2ball proximal is global
-      .l2ball_proximal_communicator(comm)
+    // communicator ensuring l2 norm in l2ball proximal is global
+    .l2ball_proximal_communicator(comm)
 #endif
-      // communicator ensuring l1 norm in l1 proximal is global
-      .l1_proximal_adjoint_space_comm(comm)
-      .tight_frame(false)
-      .l1_proximal_tolerance(1e-2)
-      .l1_proximal_nu(1)
-      .l1_proximal_itermax(50)
-      .l1_proximal_positivity_constraint(true)
-      .l1_proximal_real_constraint(true)
-      .residual_tolerance(epsilon)
-      .lagrange_update_scale(0.9)
-      .nu(1e0)
-      .Psi(Psi)
-      .Phi(*measurements);
+    // communicator ensuring l1 norm in l1 proximal is global
+    .l1_proximal_adjoint_space_comm(comm)
+    .tight_frame(false)
+    .l1_proximal_tolerance(1e-2)
+    .l1_proximal_nu(1)
+    .l1_proximal_itermax(50)
+    .l1_proximal_positivity_constraint(true)
+    .l1_proximal_real_constraint(true)
+    .residual_tolerance(epsilon)
+    .lagrange_update_scale(0.9)
+    .nu(1e0)
+    .Psi(Psi)
+    .Phi(*measurements);
   sopt::ScalarRelativeVariation<t_complex> conv(padmm->relative_variation(),
-                                                padmm->relative_variation(), "Objective function");
+      padmm->relative_variation(), "Objective function");
   std::weak_ptr<decltype(padmm)::element_type> const padmm_weak(padmm);
   padmm->residual_convergence([padmm_weak, conv,
-                               comm](Vector<t_complex> const &x,
-                                     Vector<t_complex> const &residual) mutable -> bool {
-    auto const padmm = padmm_weak.lock();
+      comm](Vector<t_complex> const &x,
+        Vector<t_complex> const &residual) mutable -> bool {
+      auto const padmm = padmm_weak.lock();
 #if PURIFY_PADMM_ALGORITHM == 2
-    auto const residual_norm = sopt::mpi::l2_norm(residual, padmm->l2ball_proximal_weights(), comm);
-    auto const result = residual_norm < padmm->residual_tolerance();
+      auto const residual_norm = sopt::mpi::l2_norm(residual, padmm->l2ball_proximal_weights(), comm);
+      auto const result = residual_norm < padmm->residual_tolerance();
 #elif PURIFY_PADMM_ALGORITHM == 3 || PURIFY_PADMM_ALGORITHM == 1
-    auto const residual_norm = sopt::l2_norm(residual, padmm->l2ball_proximal_weights());
-    auto const result
-        = comm.all_reduce<int8_t>(residual_norm < padmm->residual_tolerance(), MPI_LAND);
+      auto const residual_norm = sopt::l2_norm(residual, padmm->l2ball_proximal_weights());
+      auto const result
+      = comm.all_reduce<int8_t>(residual_norm < padmm->residual_tolerance(), MPI_LAND);
 #endif
-    SOPT_LOW_LOG("    - [PADMM] Residuals: {} <? {}", residual_norm, padmm->residual_tolerance());
-    return result;
-  });
+      SOPT_LOW_LOG("    - [PADMM] Residuals: {} <? {}", residual_norm, padmm->residual_tolerance());
+      return result;
+      });
 
   padmm->objective_convergence([padmm_weak, conv, comm](Vector<t_complex> const &x,
-                                                        Vector<t_complex> const &) mutable -> bool {
-    auto const padmm = padmm_weak.lock();
+        Vector<t_complex> const &) mutable -> bool {
+      auto const padmm = padmm_weak.lock();
 #if PURIFY_PADMM_ALGORITHM == 2
-    return conv(sopt::mpi::l1_norm(padmm->Psi().adjoint() * x, padmm->l1_proximal_weights(), comm));
+      return conv(sopt::mpi::l1_norm(padmm->Psi().adjoint() * x, padmm->l1_proximal_weights(), comm));
 #elif PURIFY_PADMM_ALGORITHM == 3 || PURIFY_PADMM_ALGORITHM == 1
-    return comm.all_reduce<uint8_t>(
-        conv(sopt::l1_norm(padmm->Psi().adjoint() * x, padmm->l1_proximal_weights())), MPI_LAND);
+      return comm.all_reduce<uint8_t>(
+          conv(sopt::l1_norm(padmm->Psi().adjoint() * x, padmm->l1_proximal_weights())), MPI_LAND);
 #endif
-  });
+      });
 
   return padmm;
 }
@@ -167,22 +167,22 @@ int main(int nargs, char const **args) {
 #if PURIFY_PADMM_ALGORITHM == 2 || PURIFY_PADMM_ALGORITHM == 3
   auto const measurements = std::make_shared<sopt::LinearTransform<Vector<t_complex>> const>(
       measurementoperator::init_degrid_operator_2d<Vector<t_complex>>(
-          world, std::get<0>(data), ground_truth_image.rows(), ground_truth_image.cols(), 1., 1.));
+        world, std::get<0>(data), ground_truth_image.rows(), ground_truth_image.cols(), 1., 1.));
 #elif PURIFY_PADMM_ALGORITHM == 1
   auto const measurements = std::make_shared<sopt::LinearTransform<Vector<t_complex>> const>(
       measurementoperator::init_degrid_operator_2d_mpi<Vector<t_complex>>(
-          world, std::get<0>(data), ground_truth_image.rows(), ground_truth_image.cols(), 1., 1.));
+        world, std::get<0>(data), ground_truth_image.rows(), ground_truth_image.cols(), 1., 1.));
 #endif
   auto const sara = sopt::wavelets::distribute_sara(
       sopt::wavelets::SARA{
-          std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u),
-          std::make_tuple("DB3", 3u), std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u),
-          std::make_tuple("DB6", 3u), std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)},
+      std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u),
+      std::make_tuple("DB3", 3u), std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u),
+      std::make_tuple("DB6", 3u), std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)},
       world);
 
   // Create the padmm solver
   auto const padmm = padmm_factory(measurements, sara, ground_truth_image, std::get<0>(data),
-                                   std::get<1>(data), world);
+      std::get<1>(data), world);
   // calls padmm
   auto const diagnostic = (*padmm)();
 
@@ -200,7 +200,7 @@ int main(int nargs, char const **args) {
 #elif PURIFY_PADMM_ALGORITHM == 2
     auto const pb_path = path / kernel / "global_epsilon_replicated_grids";
 #elif PURIFY_PADMM_ALGORITHM == 1
-    auto const pb_path = path / kernel / "global_epsilon_distributed_grids";
+    auto const pb_path = path / kernel / "local_epsilon_distributed_grids";
 #else
 #error Unknown or unimplemented algorithm
 #endif
@@ -208,11 +208,11 @@ int main(int nargs, char const **args) {
 
     pfitsio::write2d(ground_truth_image.real(), (path / "input.fits").native());
     pfitsio::write2d(dirty_image, ground_truth_image.rows(), ground_truth_image.cols(),
-                     (pb_path / "dirty.fits").native());
+        (pb_path / "dirty.fits").native());
     pfitsio::write2d(diagnostic.x.real(), ground_truth_image.rows(), ground_truth_image.cols(),
-                     (pb_path / "solution.fits").native());
+        (pb_path / "solution.fits").native());
     pfitsio::write2d(residual_image, ground_truth_image.rows(), ground_truth_image.cols(),
-                     (pb_path / "residual.fits").native());
+        (pb_path / "residual.fits").native());
   }
   return 0;
 }
