013abe685d51f2e08779a918b2d4381009ba7548
luke <Luke.Pratley.15@ucl.ac.uk>
2017-06-09 13:52:35 +0100

    added functions and tests to read and write 3D fits cubes

--- cpp/purify/pfitsio.cc
+++ cpp/purify/pfitsio.cc
@@ -1,7 +1,6 @@
-#include "purify/config.h"
 #include "purify/pfitsio.h"
+#include "purify/config.h"
 #include "purify/logging.h"
-
 namespace purify {
 namespace pfitsio {
 //! Write image to fits file
@@ -112,5 +111,124 @@ Image<t_complex> read2d(const std::string &fits_name) {
   std::copy(&contents[0], &contents[0] + eigen_image.size(), eigen_image.data());
   return eigen_image;
 }
+void write3d_header(const std::vector<Image<t_real>> &eigen_images,
+                    const pfitsio::header_params &header, const bool &overwrite) {
+  /*
+     Writes an image to a fits file.
+     image:: image data, a 2d Image.
+     header:: structure containing header information
+     overwrite:: if true, overwrites old fits file with same name
+*/
+  if(overwrite == true) {
+    remove(header.fits_name.c_str());
+  };
+  long naxes[4]
+      = {static_cast<long>(eigen_images[0].rows()), static_cast<long>(eigen_images[0].cols()),
+         static_cast<long>(eigen_images.size()), 1};
+  std::unique_ptr<CCfits::FITS> pFits(new CCfits::FITS(header.fits_name, FLOAT_IMG, 4, naxes));
+  long fpixel(1);
+  std::vector<long> extAx = {eigen_images[0].rows(), eigen_images[0].cols()};
+
+  std::valarray<double> image(naxes[0] * naxes[1] * eigen_images.size());
+  for(int i = 0; i < eigen_images.size(); i++)
+    std::copy(eigen_images[i].data(), eigen_images[i].data() + eigen_images[i].size(),
+              &image[naxes[0] * naxes[1] * i]);
+  // Writing to fits header
+  pFits->pHDU().addKey("AUTHOR", "Purify", "");
+  pFits->pHDU().addKey("BUNIT", header.pix_units, ""); // units
+  pFits->pHDU().addKey("NAXIS", 4, "");                // number of axes
+  pFits->pHDU().addKey("NAXIS1", static_cast<t_int>(eigen_images[0].cols()), "");
+  pFits->pHDU().addKey("NAXIS2", static_cast<t_int>(eigen_images[0].rows()), "");
+  pFits->pHDU().addKey("NAXIS3", eigen_images.size(), "");
+  pFits->pHDU().addKey("NAXIS4", 1, "");
+  pFits->pHDU().addKey("CRPIX1", static_cast<t_int>(std::floor(eigen_images[0].cols() / 2)) + 1,
+                       "");
+  pFits->pHDU().addKey("CRPIX2", static_cast<t_int>(std::floor(eigen_images[0].rows() / 2)) + 1,
+                       "");
+  pFits->pHDU().addKey("CRPIX3", 1, "");
+  pFits->pHDU().addKey("CRPIX4", header.polarisation, "");
+  pFits->pHDU().addKey("CRVAL1", header.ra * 180. / purify::constant::pi, "");
+  pFits->pHDU().addKey("CRVAL2", header.dec * 180. / purify::constant::pi, "");
+  pFits->pHDU().addKey("CRVAL3", header.mean_frequency * std::pow(10, 6) * 1., "");
+  pFits->pHDU().addKey("CRVAL4", 1, "");
+  pFits->pHDU().addKey("CTYPE1", "RA---SIN", "");
+  pFits->pHDU().addKey("CTYPE2", "DEC---SIN", "");
+  pFits->pHDU().addKey("CTYPE3", "FREQ-OBS", "");
+  pFits->pHDU().addKey("CTYPE4", "STOKES", "");
+  pFits->pHDU().addKey("CDELT1", -header.cell_x / 3600., "");
+  pFits->pHDU().addKey("CDELT2", header.cell_y / 3600., "");
+  pFits->pHDU().addKey("CDELT3", header.channel_width * std::pow(10, 6) * 1., "");
+  pFits->pHDU().addKey("CDELT4", 1, "");
+  pFits->pHDU().addKey("BTYPE", "intensity", "");
+  pFits->pHDU().addKey("EQUINOX", 2000, "");
+  pFits->pHDU().addKey("PURIFY-NITERS", header.niters, "");
+  if(header.hasconverged) {
+    pFits->pHDU().addKey("PURIFY-CONVERGED", "T", "");
+  } else {
+    pFits->pHDU().addKey("PURIFY-CONVERGED", "F", "");
+  }
+  pFits->pHDU().addKey("PURIFY-RELATIVEVARIATION", header.relative_variation, "");
+  pFits->pHDU().addKey("PURIFY-RESIDUALCONVERGENCE", header.residual_convergence, "");
+  pFits->pHDU().addKey("PURIFY-EPSILON", header.epsilon, "");
+
+  // Writing cube to fits file
+  pFits->pHDU().write(fpixel, naxes[0] * naxes[1] * eigen_images.size(), image);
 }
+
+void write3d(const std::vector<Image<t_real>> &eigen_images, const std::string &fits_name,
+             const std::string &pix_units, const bool &overwrite) {
+  /*
+     Writes a vector of images to a fits file.
+     image:: image data, a 3d Image.
+     fits_name:: string containing the file name of the fits file.
+     pix_units:: units of flux
+     ra:: centre pixel coordinate in ra
+     dec:: centre pixel coordinate in dec
+*/
+  if(overwrite == true) {
+    remove(fits_name.c_str());
+  };
+  long naxes[3]
+      = {static_cast<long>(eigen_images[0].rows()), static_cast<long>(eigen_images[0].cols()),
+         static_cast<long>(eigen_images.size())};
+  std::unique_ptr<CCfits::FITS> pFits(new CCfits::FITS(fits_name, FLOAT_IMG, 3, naxes));
+  long fpixel(1);
+  std::vector<long> extAx = {eigen_images[0].rows(), eigen_images[0].cols()};
+
+  std::valarray<double> image(naxes[0] * naxes[1] * eigen_images.size());
+  for(int i = 0; i < eigen_images.size(); i++)
+    std::copy(eigen_images[i].data(), eigen_images[i].data() + eigen_images[i].size(),
+              &image[naxes[0] * naxes[1] * i]);
+  pFits->pHDU().addKey("AUTHOR", "Purify", "");
+  pFits->pHDU().addKey("BUNIT", pix_units, "");
+  pFits->pHDU().addKey("NAXIS", 3, "");
+  pFits->pHDU().addKey("NAXIS1", naxes[0], "");
+  pFits->pHDU().addKey("NAXIS2", naxes[1], "");
+  pFits->pHDU().addKey("NAXIS3", eigen_images.size(), "");
+  pFits->pHDU().write(fpixel, naxes[0] * naxes[1] * eigen_images.size(), image);
 }
+std::vector<Image<t_complex>> read3d(const std::string &fits_name) {
+  /*
+     Reads in a cube from a fits file and returns the vector of images.
+     fits_name:: name of fits file
+     */
+
+  std::auto_ptr<CCfits::FITS> pInfile(new CCfits::FITS(fits_name, CCfits::Read, true));
+  std::valarray<t_real> contents;
+  CCfits::PHDU &image = pInfile->pHDU();
+  image.read(contents);
+  t_int ax1(image.axis(0));
+  t_int ax2(image.axis(1));
+  t_int channels_total;
+  image.readKey("NAXIS3", channels_total);
+  std::vector<Image<t_complex>> eigen_images;
+  for(int i = 0; i < channels_total; i++) {
+    Image<t_complex> eigen_image(ax1, ax2);
+    std::copy(&contents[ax1 * ax2 * i], &contents[ax1 * ax2 * i] + eigen_image.size(),
+              eigen_image.data());
+    eigen_images.push_back(eigen_image);
+  }
+  return eigen_images;
+}
+} // namespace pfitsio
+} // namespace purify
--- cpp/tests/purify_fitsio.cc
+++ cpp/tests/purify_fitsio.cc
@@ -12,3 +12,77 @@ TEST_CASE("Purify fitsio", "[readwrite]") {
   CAPTURE(input2);
   CHECK(input.isApprox(input2, 1e-6));
 }
+
+TEST_CASE("readwrite2dheader", "purify fitsio") {
+  Image<t_complex> input = pfitsio::read2d(image_filename("M31.fits"));
+  pfitsio::header_params header_example;
+  header_example.fits_name = output_filename("fits_output.fits");
+  pfitsio::write2d_header(input.real(), header_example);
+  Image<t_complex> input2 = pfitsio::read2d(output_filename("fits_output.fits"));
+  CHECK(input.isApprox(input2, 1e-6));
+}
+
+TEST_CASE("readwrite3d", "purify fitsio") {
+
+  std::vector<Image<t_complex>> input = pfitsio::read3d(image_filename("cube_example.fits"));
+  CHECK(input.size() == 4);
+  CHECK(input[0].size() == 200 * 200);
+  std::vector<Image<t_real>> input_real;
+  for(int i = 0; i < input.size(); i++) {
+    input_real.push_back(input[i].real());
+  }
+  pfitsio::write3d(input_real, output_filename("cube_output.fits"));
+  std::vector<Image<t_complex>> input2 = pfitsio::read3d(output_filename("cube_output.fits"));
+  for(int i = 0; i < input.size(); i++) {
+    CHECK(input[i].isApprox(input2[i], 1e-6));
+  }
+}
+TEST_CASE("readwrite3dheader", "purify fitsio") {
+
+  std::vector<Image<t_complex>> input = pfitsio::read3d(image_filename("cube_example.fits"));
+  CHECK(input.size() == 4);
+  CHECK(input[0].size() == 200 * 200);
+  std::vector<Image<t_real>> input_real;
+  for(int i = 0; i < input.size(); i++) {
+    input_real.push_back(input[i].real());
+  }
+  pfitsio::header_params header_example;
+  header_example.fits_name = output_filename("cube_output.fits");
+  pfitsio::write3d_header(input_real, header_example);
+  std::vector<Image<t_complex>> input2 = pfitsio::read3d(output_filename("cube_output.fits"));
+  for(int i = 0; i < input.size(); i++) {
+    CHECK(input[i].isApprox(input2[i], 1e-6));
+  }
+}
+TEST_CASE("readwrite3dwith2d", "purify fitsio") {
+
+  std::vector<Image<t_complex>> input = pfitsio::read3d(image_filename("M31.fits"));
+  CHECK(input.size() == 1);
+  CHECK(input[0].size() == 256 * 256);
+  std::vector<Image<t_real>> input_real;
+  for(int i = 0; i < input.size(); i++) {
+    input_real.push_back(input[i].real());
+  }
+  pfitsio::write3d(input_real, output_filename("2dcube_output.fits"));
+  std::vector<Image<t_complex>> input2 = pfitsio::read3d(output_filename("2dcube_output.fits"));
+  for(int i = 0; i < input.size(); i++) {
+    CHECK(input[i].isApprox(input2[i], 1e-6));
+  }
+}
+TEST_CASE("readwrite3dheaderwith2d", "purify fitsio") {
+
+  std::vector<Image<t_complex>> input = pfitsio::read3d(image_filename("M31.fits"));
+  CHECK(input.size() == 1);
+  CHECK(input[0].size() == 256 * 256);
+  std::vector<Image<t_real>> input_real;
+  for(int i = 0; i < input.size(); i++) {
+    input_real.push_back(input[i].real());
+  }
+  pfitsio::header_params header_example;
+  header_example.fits_name = output_filename("2dcube_output.fits");
+  pfitsio::write3d_header(input_real, header_example);
+  std::vector<Image<t_complex>> input2 = pfitsio::read3d(output_filename("2dcube_output.fits"));
+  for(int i = 0; i < input.size(); i++) {
+    CHECK(input[i].isApprox(input2[i], 1e-6));
+  }
+}
