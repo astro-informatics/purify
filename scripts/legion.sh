99b9f8d3c847da0d6004136435672c7d8e2937e7
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2017-07-20 09:30:56 +0100

    Attempt at parallel submission script...

--- scripts/legion.sh
+++ scripts/legion.sh
@@ -1,21 +1,40 @@
 #!/bin/bash -l
 #$ -S /bin/bash
-#$ -P RCSoftDev
-#$ -l paid=1
 #$ -l h_rt=0:10:0
 #$ -l mem=1G
 
-# Request TMPDIR space (default is 10 GB)
-#$ -l tmpfs=10G
+# Run jobs on XYZ nodes reserved only for this job, so that we can compare benchmark results.
+#$ -ac allow=XYZ
+##$ -ac exclusive
 
-# Set the name of the job.
-#$ -N serialBenchmark
+# I think this is to make it run now?
+#$ -now n
+
+# Request TMPDIR space (default is 10 GB)
+#$ -l tmpfs=1G
 
 # Set the working directory to somewhere in your scratch space.  This is
 # a necessary step as compute nodes cannot write to $HOME.
 #$ -wd /home/cceaiac/Scratch/purify
 
-# Your work *must* be done in $TMPDIR 
-cd $TMPDIR
-
-$HOME/purify/build/cpp/benchmarks/measurement_operator > $HOME/Scratch/purify/results_$JOB_ID.out
+OPTIND=1
+getopts ":p" opt
+case $opt in
+    p)
+# Set the name of the job.
+#$ -N purifyPBenchmark
+# Select the MPI parallel environment and number of cores.
+#$ -pe mpi 2
+#Automatically set threads to processes per node: if on X nodes = 12 OMP threads
+export OMP_NUM_THREADS=$(ppn)
+#Run our MPI job with the default modules. Gerun is a wrapper script for mpirun. 
+gerun $HOME/purify/build/cpp/benchmarks/mpi_benchmarks > $HOME/Scratch/purify/purifyPBenchmark.$JOB_ID.out
+	;;
+    *)
+	# Set the name of the job.
+	#$ -N purifySBenchmark
+	# Your work *must* be done in $TMPDIR 
+	cd $TMPDIR
+	$HOME/purify/build/cpp/benchmarks/measurement_operator > $HOME/Scratch/purify/purifySBenchmark.$JOB_ID.out
+	;;
+esac
