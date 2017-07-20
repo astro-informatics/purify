ffc785dd8a1211e6ffd8f22acbb2bed427fc0e7d
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2017-07-20 13:20:06 +0100

    Two different legion submission scripts for serial and parallel benchmarks.

--- scripts/legion_parallel.sh
+++ scripts/legion_parallel.sh
@@ -5,10 +5,7 @@
 
 # Run jobs on XYZ nodes reserved only for this job, so that we can compare benchmark results.
 #$ -ac allow=XYZ
-##$ -ac exclusive
-
-# I think this is to make it run now?
-#$ -now n
+#$ -ac exclusive
 
 # Request TMPDIR space (default is 10 GB)
 #$ -l tmpfs=1G
@@ -17,24 +14,14 @@
 # a necessary step as compute nodes cannot write to $HOME.
 #$ -wd /home/cceaiac/Scratch/purify
 
-OPTIND=1
-getopts ":p" opt
-case $opt in
-    p)
 # Set the name of the job.
 #$ -N purifyPBenchmark
+
 # Select the MPI parallel environment and number of cores.
-#$ -pe mpi 2
-#Automatically set threads to processes per node: if on X nodes = 12 OMP threads
+#$ -pe mpi 1
+
+#Automatically set threads to processes per node: eg. if on X nodes = 12 OMP threads
 export OMP_NUM_THREADS=$(ppn)
+
 #Run our MPI job with the default modules. Gerun is a wrapper script for mpirun. 
-gerun $HOME/purify/build/cpp/benchmarks/mpi_benchmarks > $HOME/Scratch/purify/purifyPBenchmark.$JOB_ID.out
-	;;
-    *)
-	# Set the name of the job.
-	#$ -N purifySBenchmark
-	# Your work *must* be done in $TMPDIR 
-	cd $TMPDIR
-	$HOME/purify/build/cpp/benchmarks/measurement_operator > $HOME/Scratch/purify/purifySBenchmark.$JOB_ID.out
-	;;
-esac
+gerun $HOME/purify/build/cpp/benchmarks/mpi_benchmarks > $HOME/Scratch/purify/parallelBenchmark.$JOB_ID.out
---
+++ scripts/legion_serial.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#$ -S /bin/bash
+#$ -l h_rt=0:10:0
+#$ -l mem=1G
+
+# Run jobs on XYZ nodes reserved only for this job, so that we can compare benchmark results.
+#$ -ac allow=XYZ
+#$ -ac exclusive
+
+# Request TMPDIR space (default is 10 GB)
+#$ -l tmpfs=1G
+
+# Set the working directory to somewhere in your scratch space.  This is
+# a necessary step as compute nodes cannot write to $HOME.
+#$ -wd /home/cceaiac/Scratch/purify
+
+# Set the name of the job.
+#$ -N purifySBenchmark
+
+# Your work *must* be done in $TMPDIR 
+cd $TMPDIR
+
+$HOME/purify/build/cpp/benchmarks/measurement_operator > $HOME/Scratch/purify/serialBenchmark.$JOB_ID.out
