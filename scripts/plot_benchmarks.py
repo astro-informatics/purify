88ece1292adecd41ec36b29c47c26c9d18200c56
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2017-07-24 12:13:35 +0100

    Plot distributed and MPI in different plots on same page.

--- scripts/plot_benchmarks.py
+++ scripts/plot_benchmarks.py
@@ -10,14 +10,13 @@ def parse_file(filename):
             linelist = line.split()
             if len(linelist)>4:
                 timeslist.append(int(linelist[2]))
-    print(np.array(timeslist))
+    #print(np.array(timeslist))
     return np.array(timeslist)
 
-def prep_arrays(serial_times, parallel_times, index, N):
-    #times = [serial_times[index], parallel_times[index], parallel_times[index+N]]
-    times = [x for x in parallel_times[index::N]]
-    times.insert(0, serial_times[index])
-    print(times)
+def prep_arrays(serial_times, parallel_times, index_s, index_p, N):
+    times = [x for x in parallel_times[index_p::N]]
+    times.insert(0, serial_times[index_s])
+    #print(times)
     return np.array(times)
 
 if __name__ == "__main__":
@@ -31,25 +30,23 @@ if __name__ == "__main__":
      npar = n1*n2*n3
      # number of benchmarked functions
      nb = int(sys.argv[6])
-     N = n1*n2*n3*nb
-     # number of benchmarked ncore configurations: serial not included
-     nconfP = int(sys.argv[7])
-     # total number of configurations = 1 (for serial) + 2*nconf (for parallel)
-     # The *2 is for the degrid operator benchmarks: distr+MPI
-     nconf = 1 + nconfP*2
+     N = npar*nb
+     # number of benchmarked ncore configurations, serial included
+     nconf = int(sys.argv[7])
+     # For the degrid operator benchmarks, there are 2 sets of plots distr+MPI
      x = range(nconf)
      x_name = ['serial',
-               'distr/1', 'MPI/1',
-               'distr/2', 'MPI/2',
-               'distr/8', 'MPI/8',
-               'distr/12', 'MPI/12',
-               'distr/16', 'MPI/16',
-               'distr/24', 'MPI/24',
-               'distr/48', 'MPI/48'
+               '1',
+               '2',
+               '8',
+               '12',
+               '16',
+               '24',
+               '48'
      ]
      colors = ['black', 'red', 'green', 'blue']
-     markers = ['o', '^', 's']
-     bm_name = ['ctor ', 'dir ', 'adj ']
+     markers = ['o', '^', 's', 'x']
+     bm_name = ['Constructor', 'Apply Direct', 'Apply Adjoint']
      par_name = ['im=128, vis=1000, k=2',
                  'im=128, vis=1000, k=4',
                  'im=256, vis=1000, k=2',
@@ -57,15 +54,27 @@ if __name__ == "__main__":
      ]
      for bm in range(0,nb):
          fig = plt.figure()
-         ax = fig.add_subplot(1,1,1)
+         fig.suptitle(bm_name[bm], fontsize=16)
+         ax = fig.add_subplot(1,2,1)
          for par in range(0,npar):
              index = bm*npar + par
-             y = prep_arrays(serial_times, parallel_times, index, N)
-             group = bm_name[bm] + par_name[par]
-             ax.scatter(x, y, c=colors[par], marker=markers[bm], label=group)
+             y = prep_arrays(serial_times, parallel_times, index, index, 2*N)
+             group = par_name[par]
+             ax.scatter(x, y, c=colors[par], s=200, marker=markers[par], label=group)
+             plt.title('Distributed')
          plt.setp(ax.get_xticklabels(), visible=True)
          plt.xticks(range(nconf), x_name)
-         plt.xlabel("parallelization scheme/number of cores")
+         plt.xlabel("number of cores")
          plt.ylabel("time (ms or us)")
+         ax = fig.add_subplot(1,2,2)
+         for par in range(0,npar):
+             index = bm*npar + N + par
+             y = prep_arrays(serial_times, parallel_times, index-N, index, 2*N)
+             group = par_name[par]
+             ax.scatter(x, y, c=colors[par], s=200, marker=markers[par], label=group)
+             plt.title('MPI')
+         plt.setp(ax.get_xticklabels(), visible=True)
+         plt.xticks(range(nconf), x_name)
+         plt.xlabel("number of cores")
          plt.legend()
          plt.show()
