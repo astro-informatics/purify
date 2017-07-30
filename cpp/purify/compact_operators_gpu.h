1770746964e4a156af11c65405725eb4a7d2923d
luke <Luke.Pratley.15@ucl.ac.uk>
2017-07-31 00:53:12 +0100

    Added gpu version of compact measurement operator. Also added unit tests to show it is consistent with phiTphi.

---
+++ cpp/purify/compact_operators_gpu.h
@@ -0,0 +1,294 @@
+#ifndef PURIFY_COMPACT_OPERATORS_GPU
+#define PURIFY_COMPACT_OPERATORS_GPU
+
+#include "purify/config.h"
+#include <iostream>
+#include <tuple>
+#include "sopt/chained_operators.h"
+#include "sopt/linear_transform.h"
+#include "purify/logging.h"
+#include "purify/operators.h"
+#include "purify/types.h"
+#ifdef purify_arrayfire
+#include <arrayfire.h>
+#endif
+
+#ifdef PURIFY_ARRAYFIRE
+namespace purify {
+  namespace gpu {
+    namespace details {
+      sopt::OperatorFunction<af::array>
+        init_af_gridding_degridding_matrix_2d(const Vector<t_real> &u, const Vector<t_real> &v,
+            const Vector<t_real> &w, const Vector<t_complex> weights,
+            const t_uint &imsizey_, const t_uint &imsizex_,
+            const t_real oversample_ratio,
+            const std::function<t_real(t_real)> kernelu,
+            const std::function<t_real(t_real)> kernelv, const t_uint Ju = 4,
+            const t_uint Jv = 4, const bool w_term = false, const t_real &cellx = 1,
+            const t_real &celly = 1, const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          const Sparse<t_complex> interpolation_matrix = purify::details::init_gridding_matrix_2d(
+              u, v, w, weights, imsizey_, imsizex_, oversample_ratio, kernelu, kernelv, Ju, Jv, w_term,
+              cellx, celly, energy_chirp_fraction, energy_kernel_fraction);
+          const Sparse<t_complex> gtg = interpolation_matrix.adjoint() * interpolation_matrix;
+          const af::array GTG = gpu::init_af_G_2d(gtg);
+          return [GTG](af::array &output, const af::array &input) { output = af::matmul(GTG, input); };
+        }
+#ifdef PURIFY_MPI
+      //! Constructs degridding operator with mpi and arrayfire
+      sopt::OperatorFunction<Vector<t_complex>>
+        init_af_gridding_degridding_matrix_2d(const sopt::mpi::Communicator &comm, const Vector<t_real> &u,
+            const Vector<t_real> &v, const Vector<t_real> &w,
+            const Vector<t_complex> &weights, const t_uint &imsizey_,
+            const t_uint &imsizex_, const t_real oversample_ratio,
+            const std::function<t_real(t_real)> kernelu,
+            const std::function<t_real(t_real)> kernelv, const t_uint Ju = 4,
+            const t_uint Jv = 4, const bool w_term = false, const t_real cellx = 1,
+            const t_real &celly = 1, const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          PURIFY_MEDIUM_LOG("Number of visibilities: {}", u.size());
+          PURIFY_LOW_LOG("Constructing MPI Gridding Operator: G");
+          Sparse<t_complex> interpolation_matrix = purify::details::init_gridding_matrix_2d(
+              u, v, w, weights, imsizey_, imsizex_, oversample_ratio, kernelu, kernelv, Ju, Jv, w_term,
+              cellx, celly, energy_chirp_fraction, energy_kernel_fraction);
+          const DistributeSparseVector distributor(interpolation_matrix, comm);
+          interpolation_matrix = purify::compress_outer(interpolation_matrix);
+          const Sparse<t_complex> gTg = interpolation_matrix.adjoint() * interpolation_matrix;
+          const af::array GTG = gpu::init_af_G_2d(gTg);
+          const sopt::OperatorFunction<af::array> directGgpu
+            = [GTG](af::array &output, const af::array &input) { output = af::matmul(GTG, input); };
+          const sopt::OperatorFunction<Vector<t_complex>> directGhost
+            = gpu::host_wrapper(directGgpu, gTg.cols(), gTg.rows());
+          return [=](Vector<t_complex> &output, const Vector<t_complex> &input) {
+            Vector<t_complex> dist_swap;
+            if(comm.is_root()) {
+              assert(input.size() > 0);
+              distributor.scatter(input, dist_swap);
+            } else {
+              distributor.scatter(dist_swap);
+            }
+            Vector<t_complex> dist_out;
+            directGhost(dist_out, dist_swap);
+            if(not comm.is_root()) {
+              distributor.gather(dist_out);
+            } else {
+              distributor.gather(dist_out, output);
+              assert(output.size() > 0);
+            }
+          };
+        }
+#endif
+    }
+
+    namespace operators {
+      sopt::OperatorFunction<Vector<t_complex>>
+        base_grid_degrid_operator_2d(const Vector<t_real> &u, const Vector<t_real> &v, const Vector<t_real> &w,
+            const Vector<t_complex> &weights, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real oversample_ratio = 2,
+            const std::string &kernel = "kb", const t_uint Ju = 4, const t_uint Jv = 4,
+            const bool w_term = false, const t_real cellx = 1, const t_real &celly = 1,
+            const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          std::function<t_real(t_real)> kernelu, kernelv, ftkernelu, ftkernelv;
+          std::tie(kernelu, kernelv, ftkernelu, ftkernelv)
+            = purify::create_kernels(kernel, Ju, Jv, imsizey, imsizex, oversample_ratio);
+          sopt::OperatorFunction<af::array> directFZ, indirectFZ;
+          std::tie(directFZ, indirectFZ)
+            = gpu::operators::af_base_padding_and_FFT_2d(ftkernelu, ftkernelv, imsizey, imsizex, oversample_ratio);
+          PURIFY_MEDIUM_LOG("FoV (width, height): {} deg x {} deg", imsizex * cellx / (60. * 60.),
+              imsizey * celly / (60. * 60.));
+          PURIFY_LOW_LOG("Constructing Weighting and Gridding Operators: WG");
+          PURIFY_MEDIUM_LOG("Number of visibilities: {}", u.size());
+          PURIFY_MEDIUM_LOG("Kernel Support: {} x {}", Ju, Jv);
+          const auto GTG = details::init_af_gridding_degridding_matrix_2d(u, v, w, weights, imsizey, imsizex, oversample_ratio, kernelv, kernelu, Ju, Jv, w_term, cellx,
+              celly, energy_chirp_fraction, energy_kernel_fraction);
+          return gpu::host_wrapper(sopt::chained_operators<af::array>(indirectFZ, GTG, directFZ),
+              imsizey * imsizex, imsizey * imsizex);
+        }
+
+#ifdef PURIFY_MPI
+      sopt::OperatorFunction<Vector<t_complex>>
+        base_mpi_grid_degrid_operator_2d(const sopt::mpi::Communicator &comm, const Vector<t_real> &u,
+            const Vector<t_real> &v, const Vector<t_real> &w,
+            const Vector<t_complex> &weights, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real &oversample_ratio = 2,
+            const std::string &kernel = "kb", const t_uint Ju = 4,
+            const t_uint Jv = 4, const bool w_term = false, const t_real cellx = 1,
+            const t_real &celly = 1, const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          std::function<t_real(t_real)> kernelu, kernelv, ftkernelu, ftkernelv;
+          std::tie(kernelu, kernelv, ftkernelu, ftkernelv)
+            = purify::create_kernels(kernel, Ju, Jv, imsizey, imsizex, oversample_ratio);
+          sopt::OperatorFunction<af::array> directFZ, indirectFZ;
+          std::tie(directFZ, indirectFZ)
+            = gpu::operators::af_base_padding_and_FFT_2d(ftkernelu, ftkernelv, imsizey, imsizex, oversample_ratio);
+          PURIFY_MEDIUM_LOG("FoV (width, height): {} deg x {} deg", imsizex * cellx / (60. * 60.),
+              imsizey * celly / (60. * 60.));
+          PURIFY_LOW_LOG("Constructing Weighting and MPI Gridding Operators: WG");
+          PURIFY_MEDIUM_LOG("Number of visibilities: {}", u.size());
+          PURIFY_MEDIUM_LOG("Kernel Support: {} x {}", Ju, Jv);
+          sopt::OperatorFunction<Vector<t_complex>> gTg = details::init_af_gridding_degridding_matrix_2d(
+              comm, u, v, w, weights, imsizey, imsizex, oversample_ratio, kernelv, kernelu, Ju, Jv, w_term,
+              energy_chirp_fraction, energy_kernel_fraction);
+          sopt::OperatorFunction<Vector<t_complex>> direct
+            = gpu::host_wrapper(directFZ, imsizey * imsizex,
+                std::floor(imsizex * imsizey * oversample_ratio * oversample_ratio));
+          sopt::OperatorFunction<Vector<t_complex>> indirect = gpu::host_wrapper(
+              indirectFZ, std::floor(imsizex * imsizey * oversample_ratio * oversample_ratio),
+              imsizex * imsizey);
+          if(comm.is_root())
+            return sopt::chained_operators<Vector<t_complex>>(indirect, gTg, direct);
+          else
+            return gTg;
+        }
+#endif
+      std::shared_ptr<sopt::OperatorFunction<Vector<t_complex>> const>
+        init_grid_degrid_operator_2d(const Vector<t_real> &u, const Vector<t_real> &v, const Vector<t_real> &w,
+            const Vector<t_complex> &weights, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real &oversample_ratio = 2,
+            const t_uint &power_iters = 100, const t_real &power_tol = 1e-4,
+            const std::string &kernel = "kb", const t_uint Ju = 4, const t_uint Jv = 4,
+            const bool w_term = false, const t_real cellx = 1, const t_real &celly = 1,
+            const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          /*
+           *  Returns linear transform that is the standard degridding operator
+           */
+
+          std::array<t_int, 3> N = {0, 1, static_cast<t_int>(imsizey * imsizex)};
+          std::array<t_int, 3> M = {0, 1, static_cast<t_int>(u.size())};
+          sopt::OperatorFunction<Vector<t_complex>> direct = purify::gpu::operators::base_grid_degrid_operator_2d(
+              u, v, w, weights, imsizey, imsizex, oversample_ratio, kernel, Ju, Jv, w_term,
+              energy_chirp_fraction, energy_kernel_fraction);
+          const t_real op_norm
+            = purify::details::power_method<Vector<t_complex>>({direct, N, [](Vector<t_complex> & out, const Vector<t_complex> & in){out = in;}, N}, power_iters, power_tol, Vector<t_complex>::Random(imsizex * imsizey));
+          auto operator_norm = purify::operators::init_normalise<Vector<t_complex>>(op_norm * op_norm);
+          direct = sopt::chained_operators<Vector<t_complex>>(direct, operator_norm);
+          return std::make_shared<sopt::OperatorFunction<Vector<t_complex>> const>(direct);
+        }
+      std::shared_ptr<sopt::OperatorFunction<Vector<t_complex>> const>
+        init_grid_degrid_operator_2d(const utilities::vis_params &uv_vis_input, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real &cell_x, const t_real &cell_y,
+            const t_real &oversample_ratio = 2, const t_uint &power_iters = 100,
+            const t_real &power_tol = 1e-4, const std::string &kernel = "kb",
+            const t_uint Ju = 4, const t_uint Jv = 4, const bool w_term = false,
+            const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          auto uv_vis = uv_vis_input;
+          if(uv_vis.units == "lambda")
+            uv_vis = utilities::set_cell_size(uv_vis, cell_x, cell_y);
+          if(uv_vis.units == "radians")
+            uv_vis = utilities::uv_scale(uv_vis, std::floor(oversample_ratio * imsizex),
+                std::floor(oversample_ratio * imsizey));
+          return gpu::operators::init_grid_degrid_operator_2d(
+              uv_vis.u, uv_vis.v, uv_vis.w, uv_vis.weights, imsizey, imsizex, oversample_ratio, power_iters,
+              power_tol, kernel, Ju, Jv, w_term, cell_x, cell_y, energy_chirp_fraction,
+              energy_kernel_fraction);
+        }
+
+#ifdef PURIFY_MPI
+      std::shared_ptr<sopt::OperatorFunction<Vector<t_complex>> const>
+        init_grid_degrid_operator_2d_mpi(
+            const sopt::mpi::Communicator &comm, const Vector<t_real> &u, const Vector<t_real> &v,
+            const Vector<t_real> &w, const Vector<t_complex> &weights, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real &oversample_ratio = 2, const t_uint &power_iters = 100,
+            const t_real &power_tol = 1e-4, const std::string &kernel = "kb", const t_uint Ju = 4,
+            const t_uint Jv = 4, const bool w_term = false, const t_real cellx = 1, const t_real &celly = 1,
+            const t_real &energy_chirp_fraction = 1, const t_real &energy_kernel_fraction = 1) {
+
+          /*
+           *  Returns linear transform that is the standard degridding operator
+           */
+
+          std::array<t_int, 3> N = {0, 1, static_cast<t_int>(imsizey * imsizex)};
+          std::array<t_int, 3> M = {0, 1, static_cast<t_int>(u.size())};
+          sopt::OperatorFunction<Vector<t_complex>> direct = purify::gpu::operators::base_mpi_grid_degrid_operator_2d(
+              comm, u, v, w, weights, imsizey, imsizex, oversample_ratio, kernel, Ju, Jv, w_term,
+              energy_chirp_fraction, energy_kernel_fraction);
+          auto Broadcast = purify::operators::init_broadcaster<Vector<t_complex>>(comm);
+          direct = sopt::chained_operators<Vector<t_complex>>(Broadcast, direct);
+          const t_real op_norm
+            = purify::details::power_method<Vector<t_complex>>({direct, N, [](Vector<t_complex> & out, const Vector<t_complex> & in){out = in;}, N}, power_iters, power_tol, Vector<t_complex>::Random(imsizex * imsizey));
+          auto operator_norm = purify::operators::init_normalise<Vector<t_complex>>(op_norm * op_norm);
+          return std::make_shared<sopt::OperatorFunction<Vector<t_complex>> const>(sopt::chained_operators<Vector<t_complex>>(direct, operator_norm));
+        }
+      std::shared_ptr<sopt::OperatorFunction<Vector<t_complex>> const>
+        init_grid_degrid_operator_2d_mpi(const sopt::mpi::Communicator &comm,
+            const utilities::vis_params &uv_vis_input, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real &cell_x, const t_real &cell_y,
+            const t_real &oversample_ratio = 2, const t_uint &power_iters = 100,
+            const t_real &power_tol = 1e-4, const std::string &kernel = "kb",
+            const t_uint Ju = 4, const t_uint Jv = 4, const bool w_term = false,
+            const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          auto uv_vis = uv_vis_input;
+          if(uv_vis.units == "lambda")
+            uv_vis = utilities::set_cell_size(comm, uv_vis, cell_x, cell_y);
+          if(uv_vis.units == "radians")
+            uv_vis = utilities::uv_scale(uv_vis, std::floor(oversample_ratio * imsizex),
+                std::floor(oversample_ratio * imsizey));
+          return gpu::operators::init_grid_degrid_operator_2d_mpi(
+              comm, uv_vis.u, uv_vis.v, uv_vis.w, uv_vis.weights, imsizey, imsizex, oversample_ratio,
+              power_iters, power_tol, kernel, Ju, Jv, w_term, cell_x, cell_y, energy_chirp_fraction,
+              energy_kernel_fraction);
+        }
+      std::shared_ptr<sopt::OperatorFunction<Vector<t_complex>> const>
+        init_grid_degrid_operator_2d(
+            const sopt::mpi::Communicator &comm, const Vector<t_real> &u, const Vector<t_real> &v,
+            const Vector<t_real> &w, const Vector<t_complex> &weights, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real &oversample_ratio = 2, const t_uint &power_iters = 100,
+            const t_real &power_tol = 1e-4, const std::string &kernel = "kb", const t_uint Ju = 4,
+            const t_uint Jv = 4, const bool w_term = false, const t_real cellx = 1, const t_real &celly = 1,
+            const t_real &energy_chirp_fraction = 1, const t_real &energy_kernel_fraction = 1) {
+
+          /*
+           *  Returns linear transform that is the standard degridding operator
+           */
+
+          std::array<t_int, 3> N = {0, 1, static_cast<t_int>(imsizey * imsizex)};
+          std::array<t_int, 3> M = {0, 1, static_cast<t_int>(u.size())};
+          sopt::OperatorFunction<Vector<t_complex>> direct = purify::gpu::operators::base_grid_degrid_operator_2d(
+              u, v, w, weights, imsizey, imsizex, oversample_ratio, kernel, Ju, Jv, w_term, cellx, celly,
+              energy_chirp_fraction, energy_kernel_fraction);
+          const auto allsumall = purify::operators::init_all_sum_all<Vector<t_complex>>(comm);
+          direct = sopt::chained_operators<Vector<t_complex>>(allsumall, direct);
+          const t_real op_norm
+            = purify::details::power_method<Vector<t_complex>>({direct, N, [](Vector<t_complex> & out, const Vector<t_complex> & in){out = in;}, N}, power_iters, power_tol, Vector<t_complex>::Random(imsizex * imsizey));
+          auto operator_norm = purify::operators::init_normalise<Vector<t_complex>>(op_norm * op_norm);
+          direct = sopt::chained_operators<Vector<t_complex>>(direct, operator_norm);
+          return std::make_shared<sopt::OperatorFunction<Vector<t_complex>> const>(direct);
+        }
+      std::shared_ptr<sopt::OperatorFunction<Vector<t_complex>> const>
+        init_grid_degrid_operator_2d(const sopt::mpi::Communicator &comm,
+            const utilities::vis_params &uv_vis_input, const t_uint &imsizey,
+            const t_uint &imsizex, const t_real &cell_x, const t_real &cell_y,
+            const t_real &oversample_ratio = 2, const t_uint &power_iters = 100,
+            const t_real &power_tol = 1e-4, const std::string &kernel = "kb",
+            const t_uint Ju = 4, const t_uint Jv = 4, const bool w_term = false,
+            const t_real &energy_chirp_fraction = 1,
+            const t_real &energy_kernel_fraction = 1) {
+
+          auto uv_vis = uv_vis_input;
+          if(uv_vis.units == "lambda")
+            uv_vis = utilities::set_cell_size(comm, uv_vis, cell_x, cell_y);
+          if(uv_vis.units == "radians")
+            uv_vis = utilities::uv_scale(uv_vis, std::floor(oversample_ratio * imsizex),
+                std::floor(oversample_ratio * imsizey));
+          return gpu::operators::init_grid_degrid_operator_2d(
+              comm, uv_vis.u, uv_vis.v, uv_vis.w, uv_vis.weights, imsizey, imsizex, oversample_ratio,
+              power_iters, power_tol, kernel, Ju, Jv, w_term, energy_chirp_fraction,
+              energy_kernel_fraction);
+        }
+#endif
+    }
+  }
+} // namespace purify
+#endif
+#endif
--- cpp/tests/mpi_measurement_operator.cc
+++ cpp/tests/mpi_measurement_operator.cc
@@ -10,6 +10,7 @@
 #include "purify/utilities.h"
 #ifdef PURIFY_ARRAYFIRE
 #include "purify/operators_gpu.h"
+#include "purify/compact_operators_gpu.h"
 #endif
 using namespace purify;
 
@@ -454,6 +455,104 @@ TEST_CASE("Serial vs Distributed GPU Operator weighted") {
     Vector<t_complex> const gridded_serial = op_serial->adjoint() * uv_serial.vis;
     REQUIRE(gridded.size() == gridded_serial.size());
     REQUIRE(gridded.isApprox(gridded_serial, 1e-4));
+  }
+}
+TEST_CASE("Serial vs Distributed GPU Compact Operator") {
+  auto const world = sopt::mpi::Communicator::World();
+
+  auto const N = 5;
+  auto uv_serial = utilities::random_sample_density(N, 0, constant::pi / 3);
+  uv_serial.u = world.broadcast(uv_serial.u);
+  uv_serial.v = world.broadcast(uv_serial.v);
+  uv_serial.w = world.broadcast(uv_serial.w);
+  uv_serial.units = "radians";
+  uv_serial.vis = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(uv_serial.u.size()));
+  uv_serial.weights
+    = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(uv_serial.u.size()));
+
+  utilities::vis_params uv_mpi;
+  if(world.is_root()) {
+    auto const order
+      = distribute::distribute_measurements(uv_serial, world, "distance_distribution");
+    uv_mpi = utilities::regroup_and_scatter(uv_serial, order, world);
+  } else
+    uv_mpi = utilities::scatter_visibilities(world);
+
+  auto const over_sample = 2;
+  auto const J = 4;
+  auto const kernel = "kb";
+  auto const width = 128;
+  auto const height = 128;
+  const auto op_serial = purify::measurementoperator::init_degrid_operator_2d<Vector<t_complex>>(
+      uv_serial.u, uv_serial.v, uv_serial.w, uv_serial.weights, height, width, over_sample, 100);
+
+  const auto phiTphi = purify::gpu::operators::init_grid_degrid_operator_2d(
+      world, uv_mpi.u, uv_mpi.v, uv_mpi.w, uv_mpi.weights, height, width, over_sample, 100);
+  const auto op = sopt::LinearTransform<Vector<t_complex>>({*phiTphi, [](Vector<t_complex> &out, const Vector<t_complex> & in){out=in;}});
+
+  if(uv_serial.u.size() == uv_mpi.u.size()) {
+    REQUIRE(uv_serial.u.isApprox(uv_mpi.u));
+    CHECK(uv_serial.v.isApprox(uv_mpi.v));
+    CHECK(uv_serial.weights.isApprox(uv_mpi.weights));
+  }
+  SECTION("operation") {
+    Vector<t_complex> const image
+      = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(width * height));
+    auto uv_degrid = uv_serial;
+    const Vector<t_complex> expected_outimage = op_serial->adjoint() (*op_serial * image);
+    const Vector<t_complex> outimage = op * image;
+    REQUIRE(outimage.size() == expected_outimage.size());
+    REQUIRE(outimage.isApprox(expected_outimage, 1e-4));
+  }
+}
+
+TEST_CASE("Serial vs GPU Compact Distributed Fourier Grid Operator weighted") {
+  // sopt::logging::set_level("debug");
+  // purify::logging::set_level("debug");
+  auto const world = sopt::mpi::Communicator::World();
+
+  auto const N = 20;
+  auto uv_serial = utilities::random_sample_density(N, 0, constant::pi / 3);
+  uv_serial.u = world.broadcast(uv_serial.u);
+  uv_serial.v = world.broadcast(uv_serial.v);
+  uv_serial.w = world.broadcast(uv_serial.w);
+  uv_serial.units = "radians";
+  uv_serial.vis = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(uv_serial.u.size()));
+  uv_serial.weights
+    = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(uv_serial.u.size()));
+
+  utilities::vis_params uv_mpi;
+  if(world.is_root()) {
+    auto const order
+      = distribute::distribute_measurements(uv_serial, world, "distance_distribution");
+    uv_mpi = utilities::regroup_and_scatter(uv_serial, order, world);
+  } else
+    uv_mpi = utilities::scatter_visibilities(world);
+
+  auto const over_sample = 2;
+  auto const J = 4;
+  auto const kernel = "kb";
+  auto const width = 128;
+  auto const height = 128;
+  const auto op_serial = purify::measurementoperator::init_degrid_operator_2d<Vector<t_complex>>(
+      uv_serial.u, uv_serial.v, uv_serial.w, uv_serial.weights, height, width, over_sample, 100);
+  const auto phiTphi = purify::gpu::operators::init_grid_degrid_operator_2d_mpi(
+      world, uv_mpi.u, uv_mpi.v, uv_mpi.w, uv_mpi.weights, height, width, over_sample, 100);
+  const auto op = sopt::LinearTransform<Vector<t_complex>>({*phiTphi, [](Vector<t_complex> &out, const Vector<t_complex> & in){out=in;}});
+
+  if(world.size() == 1) {
+    REQUIRE(uv_serial.u.isApprox(uv_mpi.u));
+    CHECK(uv_serial.v.isApprox(uv_mpi.v));
+    CHECK(uv_serial.weights.isApprox(uv_mpi.weights));
+  }
+  SECTION("operation") {
+    Vector<t_complex> const image
+      = world.broadcast<Vector<t_complex>>(Vector<t_complex>::Random(width * height));
+    auto uv_degrid = uv_serial;
+    const Vector<t_complex> expected_outimage = op_serial->adjoint() (*op_serial * image);
+    const Vector<t_complex> outimage = op * image;
+    REQUIRE(outimage.size() == expected_outimage.size());
+    REQUIRE(outimage.isApprox(expected_outimage, 1e-4));
   }
 }
 #endif
