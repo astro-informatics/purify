cdc8587619bbebba5c5199745aec6b5c1ede836d
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2017-08-17 14:20:05 +0100

    Correct naming of im and uv variables in benchmark/utilities.

--- cpp/benchmarks/utilities.cc
+++ cpp/benchmarks/utilities.cc
@@ -10,11 +10,11 @@ using namespace purify;
 namespace b_utilities {
 
   void Arguments(benchmark::internal::Benchmark* b) {
-    int uv_size_max = 4096; // 4096
-    int im_size_max = 10000000; // 1M, 10M, 100M
+    int im_size_max = 4096; // 4096
+    int uv_size_max = 10000000; // 1M, 10M, 100M
     int kernel_max = 16; // 16
-    for (int i=128; i<=uv_size_max; i*=2)
-      for (int j=1000000; j<=im_size_max; j*=10)
+    for (int i=128; i<=im_size_max; i*=2)
+      for (int j=1000000; j<=uv_size_max; j*=10)
         for (int k=2; k<=kernel_max; k*=2)
           if (k*k<i)
             b->Args({i,j,k});
