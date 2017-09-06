de0cb494d23c5ddc04f193849ae593eb3ed1bc69
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2017-09-06 15:28:37 +0100

    Some generalizations in the benchmark plotting script, to work for variable number of benchmarked parameters.

--- scripts/plot_benchmarks.py
+++ scripts/plot_benchmarks.py
@@ -25,68 +25,79 @@ def prep_arrays(serial_times, parallel_times, index_s, index_p, N):
 
 if __name__ == "__main__":
      import sys
+     # Input arguments:
      # Plot walltime or data processing rate
      what = sys.argv[8]
+     # File names
+     serial_times = parse_file(sys.argv[1], what)
+     parallel_times = parse_file(sys.argv[2], what)
+     # number of benchmarking parameters
+     n1 = int(sys.argv[3]) # image sizes
+     n2 = int(sys.argv[4]) # visibilities sizes
+     n3 = int(sys.argv[5]) # kernel sizes
+     # number of benchmarked functions (eg. [ctor, direct, adjoint]=3)
+     nb = int(sys.argv[6])
+     # number of benchmarked ncore configurations, serial included
+     nconf = int(sys.argv[7])
+
      ylabel = ""
      if what=="time":
          ylabel = "Time (ms or us)"
      else:
          ylabel = "Data rate (MB/s)"
-     # File names
-     serial_times = parse_file(sys.argv[1], what)
-     parallel_times = parse_file(sys.argv[2], what)
-     # number of benchmarking parameters
-     n1 = int(sys.argv[3])
-     n2 = int(sys.argv[4])
-     n3 = int(sys.argv[5])
-     npar = n1*n2*n3
-     # number of benchmarked functions
-     nb = int(sys.argv[6])
-     N = npar*nb
-     # number of benchmarked ncore configurations, serial included
-     nconf = int(sys.argv[7])
-     # For the degrid operator benchmarks, there are 2 sets of plots distr+MPI
+
+     # Parameter lists etc
+     im_sizes = [128, 256, 512, 1024, 2048, 4096]
+     vis_sizes = ['1M', '10M']
+     k_sizes = [2, 4, 8, 16]
      x = range(nconf)
      x_name = ['serial',
-               '1',
-               '2',
-               '8',
-               '12',
-               '16',
-               '24',
-               '48'
+               '1/1',
+               '1/2',
+               '1/8',
+               '1/12',
+               '2/8',
+               '2/12',
+               '4/12'
      ]
      colors = ['black', 'red', 'green', 'blue']
      markers = ['o', '^', 's', 'x']
      bm_name = ['Constructor', 'Apply Direct', 'Apply Adjoint']
-     par_name = ['im=128, vis=1000, k=2',
-                 'im=128, vis=1000, k=4',
-                 'im=256, vis=1000, k=2',
-                 'im=256, vis=1000, k=4'
-     ]
+     npar = 0
+     par_name = []
+     for im in range(n1):
+         for vis in range(n2):
+             for k in range(n3):
+                 if k_sizes[k]*k_sizes[k]<im_sizes[im]:
+                     npar = npar+1
+                     par_name.append('im='+str(im_sizes[im])+', vis='+vis_sizes[vis]+', k='+str(k_sizes[k]))
+     N = npar*nb
+                     
      for bm in range(0,nb):
          fig = plt.figure()
          fig.suptitle(bm_name[bm], fontsize=16)
          ax = fig.add_subplot(1,2,1)
          for par in range(0,npar):
              index = bm*npar + par
+             # For the degrid operator parallel benchmarks, there are 2 sets of plots distr+MPI
              y = prep_arrays(serial_times, parallel_times, index, index, 2*N)
              group = par_name[par]
-             ax.scatter(x, y, c=colors[par], s=200, marker=markers[par], label=group)
+             ax.scatter(x, y, c=colors[par%4], s=200, marker=markers[par%4], label=group)
              plt.title('Distributed')
          plt.setp(ax.get_xticklabels(), visible=True)
          plt.xticks(range(nconf), x_name)
-         plt.xlabel("number of cores")
+         plt.xlabel("# nodes/# cores per node")
          plt.ylabel(ylabel)
          ax = fig.add_subplot(1,2,2)
          for par in range(0,npar):
              index = bm*npar + N + par
+             # For the degrid operator parallel benchmarks, there are 2 sets of plots distr+MPI
              y = prep_arrays(serial_times, parallel_times, index-N, index, 2*N)
              group = par_name[par]
-             ax.scatter(x, y, c=colors[par], s=200, marker=markers[par], label=group)
+             ax.scatter(x, y, c=colors[par%4], s=200, marker=markers[par%4], label=group)
              plt.title('MPI')
          plt.setp(ax.get_xticklabels(), visible=True)
          plt.xticks(range(nconf), x_name)
-         plt.xlabel("number of cores")
+         plt.xlabel("# nodes/# cores per node")
          plt.legend()
          plt.show()
