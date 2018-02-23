a1c32e953d434e9ece2f017c1894d87c03cca8a8
luke <Luke.Pratley.15@ucl.ac.uk>
2018-02-23 17:37:38 +0000

    Tidied up w grid code and fixed w_delta calculation. Also added function to calculate w expansion for single w term.

--- cpp/example/w_gridding.cc
+++ cpp/example/w_gridding.cc
@@ -1,69 +1,52 @@
 
 #include "purify/directories.h"
+#include "purify/logging.h"
 #include "purify/pfitsio.h"
-#include "purify/wproj_grid.h"
 #include "purify/wproj_utilities.h"
 
 using namespace purify;
 using namespace purify::notinstalled;
 
 int main(int nargs, char const **args) {
-  std::srand((unsigned int)std::time(0));
   purify::logging::initialize();
   purify::logging::set_level("debug");
-  const t_real fov = 10; // degrees
+  std::srand((unsigned int)std::time(0));
+  const t_real fov = 15; // degrees
   const t_int imsizex = 512;
   const t_int imsizey = imsizex;
   const t_real cell_x = fov / imsizex * 60 * 60;
   const t_real cell_y = fov / imsizey * 60 * 60;
-  const t_uint J = 4;
-  const t_uint M = 1;
+  const t_uint p = 1;
+  const t_uint M = 1e3;
   auto const LM = wproj_utilities::fov_cosines(cell_x, cell_y, imsizex, imsizey);
 
   const t_real cL = std::get<0>(LM) * 0.5;
   const t_real cM = std::get<1>(LM) * 0.5;
   const t_real n_max = (1 - std::sqrt(1 - cL * cL - cM * cM));
-  //  const t_real w_cell = 1.88 / (constant::pi * n_max) / 2;
-  const t_real w_cell = 2 * constant::pi / n_max;
-  const t_real w_max = w_cell * imsizex * 0.25;
+  const t_real xi = 1e-2;
+  const t_real w_cell = xi / (2 * constant::pi * n_max);
+  const t_real w_max = w_cell * 20;
   const Vector<t_real> w_grid_coords = wproj_utilities::w_range(w_cell, w_max);
-  const Vector<t_real> w_components = Vector<t_real>::Random(M) * w_max * 0;
-  /*
-    const Sparse<t_complex> w_degrider
-        = wproj_utilities::w_plane_degrid_matrix(w_grid_coords, w_components, J);
+  // const Vector<t_real> w_components = Vector<t_real>::Random(M) * w_max;
+  const Vector<t_real> w_components = wproj_utilities::w_range(0.005, w_max);
+  const std::vector<t_uint> w_rows = wproj_utilities::w_rows(w_components, w_grid_coords);
+  const auto series = wproj_utilities::expansions::taylor(p, cell_x, cell_y, imsizex, imsizey);
+  const std::vector<Sparse<t_complex>> w_expan = wproj_utilities::w_expansion(
+      imsizex, imsizey, cell_x, cell_y, 1., w_rows, w_cell, w_max, std::get<0>(series));
+  const Sparse<t_complex> W = wproj_utilities::w_projection_expansion(
+      w_expan, w_components, w_grid_coords, w_rows, std::get<1>(series));
+  const auto ft_plan = operators::fftw_plan::measure;
+  const auto fftop_ = operators::init_FFT_2d<Vector<t_complex>>(imsizex, imsizey, 1., ft_plan);
 
-    const std::vector<t_uint> w_rows = wproj_utilities::w_rows(w_degrider);
+  Vector<t_complex> interpchirp;
+  const Vector<t_complex> kernel = Vector<t_complex>(W.row(0).transpose());
+  std::get<1>(fftop_)(interpchirp, kernel);
+  std::string const interp_image_filename = output_filename("w_interp_image.fits");
+  pfitsio::write2d(Matrix<t_complex>::Map(interpchirp.data(), imsizey, imsizex).real().transpose(),
+                   interp_image_filename);
 
-    const Sparse<t_complex> w_grid = wproj_utilities::w_plane_grid(imsizex, imsizey, cell_x, cell_y,
-                                                                   1., J, w_rows, w_cell, w_max);
-    //  const Image<t_complex> grid_image = Matrix<t_complex>(w_grid);
-    //  std::string const grid_filename = output_filename("w_grid.fits");
-    //  pfitsio::write2d(grid_image.cwiseAbs().transpose(), grid_filename);
-    const Sparse<t_complex> W = w_degrider * w_grid;
-    const auto ft_plan = operators::fftw_plan::measure;
-    const auto fftop_ = operators::init_FFT_2d<Vector<t_complex>>(imsizex, imsizey, 1., ft_plan);
-
-    Vector<t_complex> interpchirp;
-    const Vector<t_complex> kernel = Vector<t_complex>(W.row(0).transpose());
-    std::get<1>(fftop_)(interpchirp, kernel);
-    std::string const interp_image_filename = output_filename("w_interp_image.fits");
-    pfitsio::write2d(Matrix<t_complex>::Map(interpchirp.data(), imsizey,
-    imsizex).real().transpose(), interp_image_filename);
-
-    const auto grid_correction = [=](const t_real &y, const t_real &x) -> t_complex {
-      return 1. / wproj_utilities::ft_kernel(y, x, n_max, J);
-    };
-    const Matrix<t_complex> correction
-        = wproj_utilities::generate_dde(grid_correction, cell_x, cell_y, imsizex, imsizey);
-    std::string const correction_filename = output_filename("w_grid_correction.fits");
-    pfitsio::write2d(correction.cwiseAbs().transpose(), correction_filename);
-    const Matrix<t_complex> chirp_corrected = wproj_utilities::generate_chirp(
-        grid_correction, 0. * w_max, cell_x, cell_y, imsizex, imsizey);
-    std::string const chirp_correction_filename = output_filename("w_grid_chrip_corrected.fits");
-    pfitsio::write2d(chirp_corrected.real().transpose(), chirp_correction_filename);
-    const Matrix<t_complex> chirp
-        = wproj_utilities::generate_chirp(0. * w_max, cell_x, cell_y, imsizex, imsizey);
-    std::string const chirp_filename = output_filename("w_grid_chrip.fits");
-    pfitsio::write2d(chirp.real().transpose(), chirp_filename);
-  */
+  const Matrix<t_complex> chirp
+      = wproj_utilities::generate_chirp(w_components(0), cell_x, cell_y, imsizex, imsizey);
+  std::string const chirp_filename = output_filename("w_grid_chrip.fits");
+  pfitsio::write2d(chirp.real().transpose(), chirp_filename);
 }
