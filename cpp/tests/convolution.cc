434c7fd9352ea80b71afd9bffaaf5bde5ca144bd
luke <Luke.Pratley.15@ucl.ac.uk>
2018-03-19 07:11:39 +0000

    fixed reflection in convolution operation

--- cpp/tests/convolution.cc
+++ cpp/tests/convolution.cc
@@ -30,7 +30,9 @@ TEST_CASE("1d_convolution") {
   const Vector<t_int> output = convol::linear_convol_1d(tri, signal);
   CHECK(output.size() == signal.size() - tri.size() + 1);
   for(int i = 0; i < signal.size() - tri.size() - 1; i++)
-    CHECK(output(i) == tri(0) * signal(i) + tri(1) * signal(i + 1) + tri(2) * signal(i + 2));
+    CHECK(output(i)
+          == tri(0) * signal(output.size() - i - 1) + tri(1) * signal(output.size() - i - 1 + 1)
+                 + tri(2) * signal(output.size() - i - 1 + 2));
 }
 TEST_CASE("2d_convolution") {
   const Vector<t_real> kernelu = Vector<t_real>::Random(3);
@@ -47,23 +49,27 @@ TEST_CASE("2d_convolution") {
         != output(output.rows() - 1, output.cols() - 2));
   for(int i = 0; i < output.cols(); i++) {
     for(int j = 0; j < output.rows(); j++) {
-      CHECK(
-          std::abs(output(j, i)
-                   - kernelv(0)
-                         * (kernelu(0) * test_buff(j, i) + kernelu(1) * test_buff(j, i + 1)
-                            + kernelu(2) * test_buff(j, i + 2))
-                   - kernelv(1)
-                         * (kernelu(0) * test_buff(j + 1, i) + kernelu(1) * test_buff(j + 1, i + 1)
-                            + kernelu(2) * test_buff(j + 1, i + 2)))
-          < 1e-7);
+      CHECK(std::abs(
+                output(j, i)
+                - kernelv(0)
+                      * (kernelu(0) * test_buff(output.rows() - j - 1, output.cols() - i - 1)
+                         + kernelu(1) * test_buff(output.rows() - j - 1, output.cols() - i + 1 - 1)
+                         + kernelu(2) * test_buff(output.rows() - j - 1, output.cols() - i - 1 + 2))
+                - kernelv(1)
+                      * (kernelu(0) * test_buff(output.rows() - 1 - j + 1, output.cols() - i - 1)
+                         + kernelu(1)
+                               * test_buff(output.rows() - 1 - j + 1, output.cols() - i - 1 + 1)
+                         + kernelu(2)
+                               * test_buff(output.rows() - 1 - j + 1, output.cols() - i - 1 + 2)))
+            < 1e-7);
     }
   }
 }
 TEST_CASE("2d_convolution_functions") {
   const t_int Ju = 3;
   const t_int Jv = 4;
-  const t_int Jw = 5;
   SECTION("box and delta") {
+    const t_int Jw = 5;
     auto convol_kernelu = [=](const t_int ju) -> t_complex { return 1.; };
     auto convol_kernelv = [=](const t_int jv) -> t_complex { return 1.; };
     auto convol_kernelw = [=](const t_int jv, const t_int ju) -> t_complex {
@@ -81,6 +87,7 @@ TEST_CASE("2d_convolution_functions") {
               .isApprox(Matrix<t_complex>::Constant(Jv, Ju, 1.), 1e-12));
   }
   SECTION("delta and delta") {
+    const t_int Jw = 5;
     auto convol_kernelu = [=](const t_int ju) -> t_complex {
       return (std::floor(ju - (Ju - 1) * 0.5) == 0) ? 1. : 0.;
     };
@@ -98,6 +105,48 @@ TEST_CASE("2d_convolution_functions") {
     CAPTURE(projection_kernel);
     CHECK(projection_kernel.cols() == (Ju + Jw - 1));
     CHECK(projection_kernel.rows() == (Jv + Jw - 1));
-    CHECK(projection_kernel(3, 3) == 1.);
+    CHECK(projection_kernel(4, 3) == 1.);
+  }
+  SECTION("delta_size_1") {
+    const t_int Jw = 1;
+    auto convol_kernelu = [=](const t_int ju) -> t_complex {
+      return (std::floor(ju - (Ju - 1) * 0.5) == 0) ? 1. : 0.;
+    };
+    auto convol_kernelv = [=](const t_int jv) -> t_complex {
+      return (std::floor(jv - (Jv - 1) * 0.5) == 0) ? 1. : 0.;
+    };
+    auto convol_kernelw = [=](const t_int jv, const t_int ju) -> t_complex {
+      return ((std::floor(ju - (Jw - 1) * 0.5) == 0) and (std::floor(jv - (Jw - 1) * 0.5) == 0)) ?
+                 1. :
+                 0.;
+    };
+    const Matrix<t_complex> projection_kernel = convol::linear_convol_2d<t_complex>(
+        convol_kernelu, convol_kernelv, convol_kernelw, static_cast<t_int>(Ju),
+        static_cast<t_int>(Jv), static_cast<t_int>(Jw), static_cast<t_int>(Jw));
+    CAPTURE(projection_kernel);
+    CHECK(projection_kernel.cols() == (Ju + Jw - 1));
+    CHECK(projection_kernel.rows() == (Jv + Jw - 1));
+    CHECK(projection_kernel(2, 1) == 1.);
+  }
+  SECTION("box_and_delta_size_1") {
+    const t_int Jw = 1;
+    auto convol_kernelu = [=](const t_int ju) -> t_complex { return ju * 1.; };
+    auto convol_kernelv = [=](const t_int jv) -> t_complex { return jv * 1.; };
+    auto convol_kernelw = [=](const t_int jv, const t_int ju) -> t_complex {
+      return ((std::floor(ju - (Jw - 1) * 0.5) == 0) and (std::floor(jv - (Jw - 1) * 0.5) == 0)) ?
+                 1. :
+                 0.;
+    };
+    const Matrix<t_complex> projection_kernel = convol::linear_convol_2d<t_complex>(
+        convol_kernelu, convol_kernelv, convol_kernelw, static_cast<t_int>(Ju),
+        static_cast<t_int>(Jv), static_cast<t_int>(Jw), static_cast<t_int>(Jw));
+    CAPTURE(projection_kernel);
+    CHECK(projection_kernel.cols() == (Ju + Jw - 1));
+    CHECK(projection_kernel.rows() == (Jv + Jw - 1));
+    for(int i = 0; i < projection_kernel.cols(); i++) {
+      for(int j = 0; j < projection_kernel.rows(); j++) {
+        CHECK(std::abs(static_cast<t_real>(i * j) - projection_kernel(j, i)) < 1e-12);
+      }
+    }
   }
 }
