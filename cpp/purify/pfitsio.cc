36b839c662281ad22a2a1a8ecc5e0f066687b972
luke <Luke.Pratley.15@ucl.ac.uk>
2018-04-15 21:52:37 +0100

    replaced code writing a 3d fits cube to use cfitsio

--- cpp/purify/pfitsio.cc
+++ cpp/purify/pfitsio.cc
@@ -147,60 +147,75 @@ void write3d_header(const std::vector<Image<t_real>> &eigen_images,
      header:: structure containing header information
      overwrite:: if true, overwrites old fits file with same name
 */
+
+  PURIFY_LOW_LOG("Writing fits file {}", header.fits_name);
   if(overwrite == true) {
     remove(header.fits_name.c_str());
-  };
-  long naxes[4]
-      = {static_cast<long>(eigen_images[0].rows()), static_cast<long>(eigen_images[0].cols()),
-         static_cast<long>(eigen_images.size()), 1};
-  std::unique_ptr<CCfits::FITS> pFits(new CCfits::FITS(header.fits_name, FLOAT_IMG, 4, naxes));
-  long fpixel(1);
-  std::vector<long> extAx = {eigen_images[0].rows(), eigen_images[0].cols()};
-
-  std::valarray<double> image(naxes[0] * naxes[1] * eigen_images.size());
-  for(int i = 0; i < eigen_images.size(); i++)
-    std::copy(eigen_images[i].data(), eigen_images[i].data() + eigen_images[i].size(),
-              &image[naxes[0] * naxes[1] * i]);
-  // Writing to fits header
-  pFits->pHDU().addKey("AUTHOR", "Purify", "");
-  pFits->pHDU().addKey("BUNIT", header.pix_units, ""); // units
-  pFits->pHDU().addKey("NAXIS", 4, "");                // number of axes
-  pFits->pHDU().addKey("NAXIS1", static_cast<t_int>(eigen_images[0].cols()), "");
-  pFits->pHDU().addKey("NAXIS2", static_cast<t_int>(eigen_images[0].rows()), "");
-  pFits->pHDU().addKey("NAXIS3", eigen_images.size(), "");
-  pFits->pHDU().addKey("NAXIS4", 1, "");
-  pFits->pHDU().addKey("CRPIX1", static_cast<t_int>(std::floor(eigen_images[0].cols() / 2)) + 1,
-                       "");
-  pFits->pHDU().addKey("CRPIX2", static_cast<t_int>(std::floor(eigen_images[0].rows() / 2)) + 1,
-                       "");
-  pFits->pHDU().addKey("CRPIX3", 1, "");
-  pFits->pHDU().addKey("CRPIX4", header.polarisation, "");
-  pFits->pHDU().addKey("CRVAL1", header.ra * 180. / purify::constant::pi, "");
-  pFits->pHDU().addKey("CRVAL2", header.dec * 180. / purify::constant::pi, "");
-  pFits->pHDU().addKey("CRVAL3", header.mean_frequency * std::pow(10, 6) * 1., "");
-  pFits->pHDU().addKey("CRVAL4", 1, "");
-  pFits->pHDU().addKey("CTYPE1", "RA---SIN", "");
-  pFits->pHDU().addKey("CTYPE2", "DEC---SIN", "");
-  pFits->pHDU().addKey("CTYPE3", "FREQ-OBS", "");
-  pFits->pHDU().addKey("CTYPE4", "STOKES", "");
-  pFits->pHDU().addKey("CDELT1", -header.cell_x / 3600., "");
-  pFits->pHDU().addKey("CDELT2", header.cell_y / 3600., "");
-  pFits->pHDU().addKey("CDELT3", header.channel_width * std::pow(10, 6) * 1., "");
-  pFits->pHDU().addKey("CDELT4", 1, "");
-  pFits->pHDU().addKey("BTYPE", "intensity", "");
-  pFits->pHDU().addKey("EQUINOX", 2000, "");
-  pFits->pHDU().addKey("PURIFY-NITERS", header.niters, "");
-  if(header.hasconverged) {
-    pFits->pHDU().addKey("PURIFY-CONVERGED", "T", "");
-  } else {
-    pFits->pHDU().addKey("PURIFY-CONVERGED", "F", "");
   }
-  pFits->pHDU().addKey("PURIFY-RELATIVEVARIATION", header.relative_variation, "");
-  pFits->pHDU().addKey("PURIFY-RESIDUALCONVERGENCE", header.residual_convergence, "");
-  pFits->pHDU().addKey("PURIFY-EPSILON", header.epsilon, "");
+  fitsfile *fptr;
+  t_int status = 0;
+  std::vector<long> naxes
+      = {static_cast<long>(eigen_images.at(0).rows()), static_cast<long>(eigen_images.at(0).cols()),
+         static_cast<long>(eigen_images.size()), 1};
+  std::vector<long> fpixel = {1, 1, 1, 1};
 
-  // Writing cube to fits file
-  pFits->pHDU().write(fpixel, naxes[0] * naxes[1] * eigen_images.size(), image);
+  Vector<t_real> image = Vector<t_real>::Zero(naxes.at(0) * naxes.at(1) * naxes.at(2));
+  for(int i = 0; i < eigen_images.size(); i++)
+    image.segment(i * naxes.at(0) * naxes.at(1), naxes.at(0) * naxes.at(1))
+        = Vector<t_real>::Map(eigen_images.at(i).data(), naxes.at(0) * naxes.at(1));
+
+  if(fits_create_file(&fptr, header.fits_name.c_str(), &status))
+    throw std::runtime_error("Problem creating fits file:" + header.fits_name);
+  if(fits_create_img(fptr, DOUBLE_IMG, static_cast<t_int>(naxes.size()), naxes.data(), &status))
+    throw std::runtime_error("Problem creating HDU for image in fits file:" + header.fits_name);
+  if(fits_write_pix(fptr, TDOUBLE, fpixel.data(), static_cast<long>(image.size()),
+                    const_cast<t_real *>(image.data()), &status))
+    throw std::runtime_error("Problem writing image in fits file:" + header.fits_name);
+
+#define PURIFY_MACRO(KEY, VALUE, COMMENT) write_key(fptr, KEY, VALUE, COMMENT, &status);
+
+  // Writing to fits header
+  PURIFY_MACRO("BUNIT", header.pix_units, "");                 // units
+  PURIFY_MACRO("NAXIS", static_cast<t_int>(naxes.size()), ""); // number of axes
+  PURIFY_MACRO("NAXIS1", static_cast<t_int>(naxes.at(0)), "");
+  PURIFY_MACRO("NAXIS2", static_cast<t_int>(naxes.at(1)), "");
+  PURIFY_MACRO("NAXIS3", static_cast<t_int>(naxes.at(2)), "");
+  PURIFY_MACRO("NAXIS4", static_cast<t_int>(naxes.at(3)), "");
+  PURIFY_MACRO("CRPIX1", static_cast<t_int>(std::floor(naxes.at(0) / 2)) + 1, "");
+  PURIFY_MACRO("CRPIX2", static_cast<t_int>(std::floor(naxes.at(1) / 2)) + 1, "");
+  PURIFY_MACRO("CRPIX3", 1, "");
+  PURIFY_MACRO("CRPIX4", 1, "");
+  PURIFY_MACRO("CRVAL1", static_cast<t_real>(header.ra * 180. / purify::constant::pi), "");
+  PURIFY_MACRO("CRVAL2", static_cast<t_real>(header.dec * 180. / purify::constant::pi), "");
+  PURIFY_MACRO("CRVAL3", static_cast<t_real>(header.mean_frequency * std::pow(10, 6) * 1.), "");
+  PURIFY_MACRO("CRVAL4", static_cast<t_real>(1), "");
+  PURIFY_MACRO("CTYPE1", "RA---SIN", "");
+  PURIFY_MACRO("CTYPE2", "DEC---SIN", "");
+  PURIFY_MACRO("CTYPE3", "FREQ-OBS", "");
+  PURIFY_MACRO("CTYPE4", "STOKES", "");
+  PURIFY_MACRO("CDELT1", static_cast<t_real>(-header.cell_x / 3600.), "");
+  PURIFY_MACRO("CDELT2", static_cast<t_real>(header.cell_y / 3600.), "");
+  PURIFY_MACRO("CDELT3", static_cast<t_real>(header.channel_width * std::pow(10, 6) * 1.), "");
+  PURIFY_MACRO("CDELT4", 1., "");
+  PURIFY_MACRO("BTYPE", "intensity", "");
+  PURIFY_MACRO("EQUINOX", 2000, "");
+
+#undef PURIFY_MACRO
+  write_history(fptr, "SOFTWARE", "Purify", &status);
+  write_history(fptr, "PURIFY-NITERS", header.niters, &status);
+  if(header.hasconverged) {
+    write_history(fptr, "PURIFY-CONVERGED", "True", &status);
+  } else {
+    write_history(fptr, "PURIFY-CONVERGED", "False", &status);
+  }
+  write_history(fptr, "PURIFY-RELATIVEVARIATION", header.relative_variation, &status);
+  write_history(fptr, "PURIFY-RESIDUALCONVERGENCE", header.residual_convergence, &status);
+  write_history(fptr, "PURIFY-EPSILON", header.epsilon, &status);
+  if(fits_write_date(fptr, &status))
+    throw std::runtime_error("Problem writing date in fits file:" + header.fits_name);
+
+  if(fits_close_file(fptr, &status))
+    throw std::runtime_error("Problem closing fits file:" + header.fits_name);
 }
 
 void write3d(const std::vector<Image<t_real>> &eigen_images, const std::string &fits_name,
