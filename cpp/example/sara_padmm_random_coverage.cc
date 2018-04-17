bcd14e2bf3738f9afcbff8192305c4bec08ee168
luke <Luke.Pratley.15@ucl.ac.uk>
2018-04-17 14:57:16 +0100

    updated old padmm examples

--- cpp/example/sara_padmm_random_coverage.cc
+++ cpp/example/sara_padmm_random_coverage.cc
@@ -9,7 +9,7 @@
 #include <sopt/utilities.h>
 #include <sopt/wavelets.h>
 #include <sopt/wavelets/sara.h>
-#include "purify/MeasurementOperator.h"
+#include "purify/operators.h"
 #include "purify/directories.h"
 #include "purify/pfitsio.h"
 #include "purify/types.h"
@@ -45,10 +45,10 @@ int main(int, char **) {
   uv_data.units = utilities::vis_units::radians;
   utilities::write_visibility(uv_data, output_vis_file);
   SOPT_NOTICE("Number of measurements / number of pixels: {}", uv_data.u.size() * 1. / M31.size());
-  // uv_data = utilities::uv_symmetry(uv_data); //reflect uv measurements
-  auto const measurements = std::make_shared<MeasurementOperator const>(
-      uv_data, 4, 4, "kb", M31.cols(), M31.rows(), 20, over_sample);
-  auto measurements_transform = linear_transform(measurements, uv_data.vis.size());
+  auto measurements_transform
+      = *measurementoperator::init_degrid_operator_2d<Vector<t_complex>>(
+          uv_data.u, uv_data.v, uv_data.w, uv_data.weights, M31.cols(), M31.rows(),
+          over_sample, 100, 1e-4, kernels::kernel::kb, 4, 4);
 
   sopt::wavelets::SARA const sara{
       std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u),
@@ -56,7 +56,7 @@ int main(int, char **) {
       std::make_tuple("DB6", 3u),   std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)};
 
   auto const Psi
-      = sopt::linear_transform<t_complex>(sara, measurements->imsizey(), measurements->imsizex());
+      = sopt::linear_transform<t_complex>(sara, M31.rows(), M31.cols());
 
   std::mt19937_64 mersenne;
   Vector<t_complex> const y0
@@ -70,10 +70,10 @@ int main(int, char **) {
   dimage = dimage / max_val;
   Vector<t_complex> initial_estimate = Vector<t_complex>::Zero(dimage.size());
   sopt::utilities::write_tiff(
-      Image<t_real>::Map(dimage.data(), measurements->imsizey(), measurements->imsizex()),
+      Image<t_real>::Map(dimage.data(), M31.rows(), M31.cols()),
       dirty_image);
   pfitsio::write2d(
-      Image<t_real>::Map(dimage.data(), measurements->imsizey(), measurements->imsizex()),
+      Image<t_real>::Map(dimage.data(), M31.rows(), M31.cols()),
       dirty_image_fits);
 
   auto const epsilon = utilities::calculate_l2_radius(uv_data.vis, sigma);
@@ -111,11 +111,11 @@ int main(int, char **) {
           sopt::RelativeVariation<std::complex<t_real>>(1e-3));
   auto const diagnostic = reweighted();
   assert(diagnostic.algo.x.size() == M31.size());
-  Image<t_complex> image = Image<t_complex>::Map(diagnostic.algo.x.data(), measurements->imsizey(),
-                                                 measurements->imsizex());
+  Image<t_complex> image = Image<t_complex>::Map(diagnostic.algo.x.data(), M31.rows(),
+                                                 M31.cols());
   sopt::utilities::write_tiff(image.real(), outfile);
   pfitsio::write2d(image.real(), outfile_fits);
-  Image<t_complex> residual = measurements->grid(y0 - measurements->degrid(image));
+  Image<t_complex> residual = Image<t_complex>::Map((measurements_transform.adjoint() * (y0 - measurements_transform * Vector<t_complex>::Map(image.data(), image.size()))).eval().data(), M31.rows(), M31.cols() );
   pfitsio::write2d(residual.real(), residual_fits);
   return 0;
 }
