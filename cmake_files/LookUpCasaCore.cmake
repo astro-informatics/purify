b5d0816101cbb4ad6c842acf70fd51a74d0c7ffc
luke <Luke.Pratley.15@ucl.ac.uk>
2018-04-19 16:48:47 +0100

    added casacore lookup and added it to CMake build

---
+++ cmake_files/LookUpCasaCore.cmake
@@ -0,0 +1,56 @@
+# Looks up [CasaCore](http://basp-group.github.io/sopt/)
+#
+# - GIT_REPOSITORY: defaults to https://github.com/basp-group/sopt.git
+# - GIT_TAG: defaults to master
+# - BUILD_TYPE: defaults to Release
+#
+if(CasaCore_ARGUMENTS)
+  cmake_parse_arguments(CasaCore "" "GIT_REPOSITORY;GIT_TAG;BUILD_TYPE;OPENMPI;THREADS" "" ${CasaCore_ARGUMENTS})
+endif()
+if(NOT CasaCore_GIT_REPOSITORY)
+    set(CasaCore_GIT_REPOSITORY https://github.com/casacore/casacore.git)
+endif()
+if(NOT CasaCore_GIT_TAG)
+    set(CasaCore_GIT_TAG v2.4.1)
+endif()
+if(NOT CasaCore_BUILD_TYPE)
+    set(CasaCore_BUILD_TYPE Release)
+endif()
+if(NOT CasaCore_OPENMP)
+  set(CasaCore_OPENMP OFF)
+endif()
+if(NOT CasaCore_THREADS)
+  set(CasaCore_THREADS OFF)
+endif()
+
+# write subset of variables to cache for sopt to use
+include(PassonVariables)
+passon_variables(Lookup-CasaCore
+  FILENAME "${EXTERNAL_ROOT}/src/CasaCoreVariables.cmake"
+  PATTERNS
+      "CMAKE_[^_]*_R?PATH" "CMAKE_C_.*"
+      "BLAS_.*" "FFTW3_.*" "TIFF_.*"
+      "GreatCMakeCookOff_DIR"
+  ALSOADD
+      "\nset(CMAKE_INSTALL_PREFIX \"${EXTERNAL_ROOT}\" CACHE STRING \"\")\n"
+)
+ExternalProject_Add(
+    Lookup-CasaCore
+    PREFIX ${EXTERNAL_ROOT}
+    GIT_REPOSITORY ${CasaCore_GIT_REPOSITORY}
+    GIT_TAG ${CasaCore_GIT_TAG}
+    CMAKE_ARGS
+      -C "${EXTERNAL_ROOT}/src/CasaCoreVariables.cmake"
+      -DBUILD_SHARED_LIBS=OFF
+      -DCMAKE_BUILD_TYPE=${CasaCore_BUILD_TYPE}
+      -DUSE_FFTW3=ON
+      -DUSE_OPENMP=CasaCore_OPENMP
+      -DBUILD_PYTHON=OFF
+      -DUSE_THREADS=CasaCore_THREADS
+    INSTALL_DIR ${EXTERNAL_ROOT}
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+)
+add_recursive_cmake_step(Lookup-CasaCore DEPENDEES install)
+
