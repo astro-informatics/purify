342aa9f542cacbdf5013b3f7064284d7e75d57f7
Roland Guichard <r.guichard@ucl.ac.uk>
2018-04-20 11:44:37 +0000

    Change datatype

--- src/yaml-parser.h
+++ src/yaml-parser.h
@@ -21,7 +21,7 @@ class YamlParser {
   std::string filename;
   std::string logging;
   int iterations;
-  int epsilonScaling;
+  double epsilonScaling;
   std::string gamma;
   std::string output_prefix;
   std::string skymodel;
