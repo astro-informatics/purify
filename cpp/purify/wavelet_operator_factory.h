bf10124d8584af0fd3ca24e49f54d98a7fca5c8b
luke <Luke.Pratley.15@ucl.ac.uk>
2018-05-16 16:58:43 +0100

    Fixed possible error in wavelet factory, when building with MPI but not using MPI.

--- cpp/purify/wavelet_operator_factory.h
+++ cpp/purify/wavelet_operator_factory.h
@@ -20,20 +20,14 @@ std::shared_ptr<sopt::LinearTransform<T> const>
 wavelet_operator_factory(const distributed_wavelet_operator distribute, const std::vector<std::tuple<std::string, t_uint>> & wavelets,
     const t_uint imsizey, const t_uint imsizex){
   const auto sara = sopt::wavelets::SARA(wavelets.begin(), wavelets.end());
-    #ifdef PURIFY_MPI
-  auto const comm = sopt::mpi::Communicator::World();
-    #endif
   switch(distribute){
     case(distributed_wavelet_operator::serial):{
-    #ifdef PURIFY_MPI
-    if(comm.size() > 1)
-      throw std::runtime_error("Using serial wavelet operator with more than one MPI process.");
-    #endif
       PURIFY_LOW_LOG("Using serial wavelet operator.");
       return  std::make_shared<sopt::LinearTransform<T>>(sopt::linear_transform<typename T::Scalar>(sara, imsizey, imsizex));
     }
 #ifdef PURIFY_MPI
     case(distributed_wavelet_operator::mpi_sara):{
+      auto const comm = sopt::mpi::Communicator::World();
       PURIFY_LOW_LOG("Using distributed image MPI wavelet operator.");
       auto const dsara = sopt::wavelets::distribute_sara(sara, comm);
       return std::make_shared<sopt::LinearTransform<T>>(sopt::linear_transform<typename T::Scalar>(dsara, imsizey, imsizex, comm));
