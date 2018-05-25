a16a3d3cde8baeed1d5aad244089524af9484e6a
luke <Luke.Pratley.15@ucl.ac.uk>
2018-05-25 17:41:43 +0100

    Restructured creation of lambda using switch case. Fixed large bug, where relative variation object was reset, so convergence was never reached. Now the `conv` object is captured.

--- cpp/purify/convergence_factory.h
+++ cpp/purify/convergence_factory.h
@@ -3,46 +3,73 @@
 
 #include <sopt/imaging_padmm.h>
 #include <sopt/mpi/communicator.h>
+#include <sopt/relative_variation.h>
 
 namespace purify {
   namespace factory {
     enum class ConvergenceType {mpi_local, mpi_global};
 
-    template <class T>
+    template <class T, class Algo>
       std::function<bool(Vector<T> const &, Vector<T> const &)>
-      residual_convergence_factory(const ConvergenceType algo,
-				   std::weak_ptr<sopt::algorithm::ImagingProximalADMM<T>> const padmm_weak) {
-      return [algo, padmm_weak](Vector<T> const &, Vector<T> const & residual) {
-	auto const padmm = padmm_weak.lock();
-	sopt::mpi::Communicator comm = sopt::mpi::Communicator::World();
-	if (algo==ConvergenceType::mpi_local) {
-	  auto const residual_norm = sopt::l2_norm(residual, padmm->l2ball_proximal_weights());
-	  return bool(comm.all_reduce<int8_t>(residual_norm < padmm->residual_tolerance(), MPI_LAND));
-	}
-	else if (algo==ConvergenceType::mpi_global) {
-	  auto const residual_norm = sopt::mpi::l2_norm(residual, padmm->l2ball_proximal_weights(), comm);
-	  return (residual_norm<padmm->residual_tolerance());
-	}
-	else
-	  throw std::runtime_error("Unknown type of distributed PADMM algorithm.");
+      l2_convergence_factory(const ConvergenceType algo,	std::weak_ptr<Algo> const padmm_weak) {
+	const auto comm = sopt::mpi::Communicator::World();
+  switch(algo){
+    case(ConvergenceType::mpi_local):
+      {
+      return [algo, padmm_weak, comm](Vector<T> const &, Vector<T> const & residual) {
+	      auto const padmm = padmm_weak.lock();
+	      auto const residual_norm = sopt::l2_norm(residual, padmm->l2ball_proximal_weights());
+        SOPT_LOW_LOG("    - [PADMM] Residuals: {} <? {}", residual_norm, padmm->residual_tolerance());
+	      return static_cast<bool>(comm.all_reduce<int8_t>(
+              residual_norm < padmm->residual_tolerance(), MPI_LAND));
       };
+      break;
+    }
+    case (ConvergenceType::mpi_global):
+    {
+      return [algo, padmm_weak, comm](Vector<T> const &, Vector<T> const & residual) {
+	      auto const padmm = padmm_weak.lock();
+	      auto const residual_norm = sopt::mpi::l2_norm(residual, padmm->l2ball_proximal_weights(), comm);
+        SOPT_LOW_LOG("    - [PADMM] Residuals: {} <? {}", residual_norm, padmm->residual_tolerance());
+	      return static_cast<bool>(residual_norm < padmm->residual_tolerance());
+	    };
+      break;
+    }
+    default:
+	  throw std::runtime_error("Unknown type of distributed MPI convergence algorithm.");
+    }
     }
 
-    template <class T>
+    template <class T, class Algo>
       std::function<bool(Vector<T> const &, Vector<T> const &)>
-      objective_convergence_factory(std::weak_ptr<sopt::algorithm::ImagingProximalADMM<T>> const padmm_weak) {
-      
-      return [padmm_weak](Vector<T> const & x, Vector<T> const &) {
-	auto const padmm = padmm_weak.lock();
- 	sopt::mpi::Communicator comm = sopt::mpi::Communicator::World();
-	sopt::ScalarRelativeVariation<t_complex> conv(padmm->relative_variation(),
-						      padmm->relative_variation(), "Objective function");
-	auto const result = comm.all_reduce<uint8_t>(
-        conv(sopt::l1_norm(padmm->Psi().adjoint() * x, padmm->l1_proximal_weights())), MPI_LAND);
-    return result;
-      };
+      l1_convergence_factory(const ConvergenceType algo, std::weak_ptr<Algo> const padmm_weak) {
+ 	      auto const comm = sopt::mpi::Communicator::World();
+	      auto const padmm_temp = padmm_weak.lock();
+       const std::shared_ptr<sopt::ScalarRelativeVariation<T>> conv = std::make_shared<sopt::ScalarRelativeVariation<T>>(padmm_temp->relative_variation(),
+						      padmm_temp->relative_variation(), "Objective function");
+        switch(algo){
+          case(ConvergenceType::mpi_local):
+            {
+            return [padmm_weak, comm, conv](Vector<T> const & x, Vector<T> const &) {
+	            auto const padmm = padmm_weak.lock();
+	            return comm.all_reduce<uint8_t>(
+              (*conv)(sopt::l1_norm(padmm->Psi().adjoint() * x, padmm->l1_proximal_weights())), MPI_LAND);
+              };
+          break;
+            }
+          case(ConvergenceType::mpi_global):
+            {
+            return [padmm_weak, comm, conv](Vector<T> const & x, Vector<T> const &) {
+	            auto const padmm = padmm_weak.lock();
+	          return
+              (*conv)(sopt::mpi::l1_norm(padmm->Psi().adjoint() * x, padmm->l1_proximal_weights(), comm));
+              };
+          break;
+          }
+          default:
+	  throw std::runtime_error("Unknown type of distributed MPI convergence algorithm.");
+        }
     }
-    
   } // namespace factory
 } // namespace purify
 
