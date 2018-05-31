90bd1275fe6e0351a6762bd306ab7e3207c8b7af
luke <Luke.Pratley.15@ucl.ac.uk>
2018-05-31 20:27:58 +0100

    Added measurement set test for read_measurements

--- cpp/tests/read_measurements.cc
+++ cpp/tests/read_measurements.cc
@@ -23,13 +23,35 @@ TEST_CASE("uvfits") {
   }
   SECTION("two"){
     SECTION("uvfits") {
-     const auto uvfits = read_measurements::read_measurements({filename + ".uvfits", filename + ".uvfits"});
+     const auto uvfits = read_measurements::read_measurements(std::vector<std::string>{filename + ".uvfits", filename + ".uvfits"});
     }
     SECTION("vis"){
-     const auto vis = read_measurements::read_measurements({filename + ".vis", filename + ".vis"});
+     const auto vis = read_measurements::read_measurements(std::vector<std::string>{filename + ".vis", filename + ".vis"});
     }
     SECTION("vis_uvfits"){
-    CHECK_THROWS(read_measurements::read_measurements({filename + ".vis", filename + ".uvfits"}));
+    CHECK_THROWS(read_measurements::read_measurements(std::vector<std::string>{filename + ".vis", filename + ".uvfits"}));
     }
   }
+    SECTION("otherformat") {
+     CHECK_THROWS(read_measurements::read_measurements(filename + ".format"));
+    }
+}
+
+TEST_CASE("ms") {
+  purify::logging::set_level("debug");
+  const std::string filename = atca_filename("0332-391");
+  SECTION("one"){
+#ifdef PURIFY_CASACORE
+     const auto ms = read_measurements::read_measurements(filename + ".ms");
+#else
+     CHECK_THROWS(read_measurements::read_measurements(filename + ".ms"));
+#endif
+  }
+  SECTION("two"){
+#ifdef PURIFY_CASACORE
+     const auto uvfits = read_measurements::read_measurements(std::vector<std::string>{filename + ".ms", filename + ".ms"});
+#else
+     CHECK_THROWS(read_measurements::read_measurements(std::vector<std::string>{filename + ".ms", filename + ".ms"}));
+#endif
+    }
 }
