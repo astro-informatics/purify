f46becf0bf407335f5f91232df812c9ae01a0d52
luke <Luke.Pratley.15@ucl.ac.uk>
2018-06-01 19:08:29 +0100

    Added MPI test for reading in measurements

---
+++ cpp/tests/mpi_read_measurements.cc
@@ -0,0 +1,57 @@
+#include "purify/config.h"
+#include "catch.hpp"
+#include "purify/logging.h"
+#include "purify/types.h"
+
+#include <iostream>
+#include "purify/directories.h"
+#include "purify/read_measurements.h"
+
+using namespace purify;
+using namespace purify::notinstalled;
+
+TEST_CASE("uvfits") {
+  auto const comm = sopt::mpi::Communicator::World();
+  const std::string filename = atca_filename("0332-391");
+  SECTION("one"){
+    SECTION("uvfits") {
+     const auto uvfits = read_measurements::read_measurements(filename + ".uvfits", comm);
+     CAPTURE(comm.rank());
+     CHECK(comm.all_sum_all(uvfits.size()) == 245886);
+    }
+    SECTION("vis"){
+     const auto vis = read_measurements::read_measurements(filename + ".vis", comm);
+     CAPTURE(comm.rank());
+     CHECK(comm.all_sum_all(vis.size()) == 245886);
+    }
+  }
+  SECTION("two"){
+    SECTION("uvfits") {
+     const auto uvfits = read_measurements::read_measurements(std::vector<std::string>{filename + ".uvfits", filename + ".uvfits"}, comm);
+     CAPTURE(comm.rank());
+     CHECK(comm.all_sum_all(uvfits.size()) == 245886 * 2);
+    }
+    SECTION("vis"){
+     const auto vis = read_measurements::read_measurements(std::vector<std::string>{filename + ".vis", filename + ".vis"}, comm);
+     CAPTURE(comm.rank());
+     CHECK(comm.all_sum_all(vis.size()) == 245886 * 2);
+    }
+  }
+    SECTION("ms"){
+  SECTION("one"){
+#ifdef PURIFY_CASACORE
+     const auto ms = read_measurements::read_measurements(filename + ".ms", comm);
+     CAPTURE(comm.rank());
+     CHECK(comm.all_sum_all(ms.size()) == 245886);
+#endif
+  }
+  SECTION("two"){
+#ifdef PURIFY_CASACORE
+     const auto ms = read_measurements::read_measurements(std::vector<std::string>{filename + ".ms", filename + ".ms"}, comm);
+     CAPTURE(comm.rank());
+     CHECK(comm.all_sum_all(ms.size()) == 245886 * 2);
+#endif
+    }
+    }
+}
+
