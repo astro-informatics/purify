4e43cd482f28911b3aa33e27b13126e4900ffd09
David Pérez-Suárez <dps.helio@gmail.com>
2018-08-20 11:36:29 +0100

    Travis: :pencil2: steps to install CASA

--- .ci/build_casa.sh
+++ .ci/build_casa.sh
@@ -2,20 +2,20 @@
 
 cwd=$(pwd)
 cd /tmp
-curl -O https://raw.githubusercontent.com/casacore/casacore/master/.travis/before_script.sh
-
-echo "Building pre-casa stuff"
-bash /tmp/before_script.sh
 
 echo "Building casa"
 curl -OL https://github.com/casacore/casacore/archive/v2.4.1.tar.gz
 tar zxvf v2.4.1.tar.gz
 cd casacore-2.4.1
-mkdir build
-cd build
+
+echo "Building pre-casa stuff"
+curl -O https://raw.githubusercontent.com/casacore/casacore/master/.travis/before_script.sh
+bash /tmp/before_script.sh
+
+cd /tmp/casacore-2.4.1/build
 cmake .. -DUSE_FFTW3=ON -DUSE_OPENMP=ON \
       -DBUILD_PYTHON=OFF -DUSE_THREADS=ON
 make
-make install
+sudo make install
 
 cd $cwd
