8812b6ccc764fb28541e076e566fb97bb8caf583
David Pérez-Suárez <dps.helio@gmail.com>
2018-08-21 15:23:12 +0100

    Travis: Fixing script to build casa

--- .ci/build_casa.sh
+++ .ci/build_casa.sh
@@ -10,7 +10,7 @@ cd casacore-2.4.1
 
 echo "Building pre-casa stuff"
 curl -O https://raw.githubusercontent.com/casacore/casacore/master/.travis/before_script.sh
-bash /tmp/before_script.sh
+bash /tmp/casacore-2.4.2/before_script.sh
 
 cd /tmp/casacore-2.4.1/build
 cmake .. -DUSE_FFTW3=ON -DUSE_OPENMP=ON \
