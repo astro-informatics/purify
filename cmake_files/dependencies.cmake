894c79ccbc24f339c8a15840c04b124d139de06b
David Pérez-Suárez <dps.helio@gmail.com>
2018-08-24 19:35:16 +0100

    Revert "downloading boost when using GCC and OSX"
    This reverts commit c66f33504600f687a8c9820c0b780566cd6b11f0.

--- cmake_files/dependencies.cmake
+++ cmake_files/dependencies.cmake
@@ -55,11 +55,8 @@ if(dompi)
 endif()
 find_package(TIFF REQUIRED)
 
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" AND ${CMAKE_HOST_APPLE})
-    lookup_package(Boost REQUIRED DOWNLOAD_BY_DEFAULT COMPONENTS filesystem)
-else()
-    lookup_package(Boost REQUIRED COMPONENTS filesystem)
-endif()
+
+lookup_package(Boost REQUIRED COMPONENTS filesystem)
 
 lookup_package(Eigen3 REQUIRED DOWNLOAD_BY_DEFAULT ARGUMENTS URL "http://bitbucket.org/eigen/eigen/get/3.2.tar.gz" MD5 "035ccc791f046f48e90bb1fb42ce227e")
 
