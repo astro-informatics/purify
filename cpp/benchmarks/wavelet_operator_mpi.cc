e2c22ff04e5288241418693c8a6fafec320d6fd3
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2018-10-30 23:04:55 +0000

    added adjoint wavelet operator to benchmark

--- cpp/benchmarks/wavelet_operator_mpi.cc
+++ cpp/benchmarks/wavelet_operator_mpi.cc
@@ -56,6 +56,16 @@ class WaveletOperatorMPIFixture : public ::benchmark::Fixture {
   t_uint m_counter;
 };
 
+class WaveletOperatorAdjointMPIFixture : public ::benchmark::Fixture {
+ public:
+  void SetUp(const ::benchmark::State& state) {}
+
+  void TearDown(const ::benchmark::State& state) {}
+
+  // A bunch of useful variables
+  t_uint m_counter;
+};
+
 BENCHMARK_DEFINE_F(WaveletOperatorMPIFixture, Apply)(benchmark::State& state) {
   // Image size
   t_uint m_imsizex = state.range(0);
@@ -91,7 +101,51 @@ BENCHMARK_DEFINE_F(WaveletOperatorMPIFixture, Apply)(benchmark::State& state) {
   }
 }
 
+BENCHMARK_DEFINE_F(WaveletOperatorMPIFixture, Apply)(benchmark::State& state) {
+  // Image size
+  t_uint m_imsizex = state.range(0);
+  t_uint m_imsizey = state.range(0);
+
+  // MPI communicator
+  sopt::mpi::Communicator m_world = sopt::mpi::Communicator::World();
+
+  sopt::wavelets::SARA m_sara{
+      std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u),
+      std::make_tuple("DB3", 3u),   std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u),
+      std::make_tuple("DB6", 3u),   std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)};
+
+  sopt::wavelets::SARA const saraDistr = sopt::wavelets::distribute_sara(m_sara, m_world);
+
+  sopt::LinearTransform<Vector<t_complex>> m_Psi =
+      sopt::linear_transform<t_complex>(saraDistr, m_imsizey, m_imsizex, m_world);
+
+  // Benchmark the application of the operator
+
+  // Get the number of wavelet coefs
+  t_uint const n_wave_coeff = saraDistr.size() * m_imsizey * m_imsizex;
+
+  // Apply Psi to a temporary vector
+  Vector<t_complex> const  image = Vector<t_complex>::Ones(m_imsizey * m_imsizex);
+  Vector<t_complex>  wavelet_coeff = Vector<t_complex>::Zero(n_wave_coeff);
+
+  while (state.KeepRunning()) {
+    auto start = std::chrono::high_resolution_clock::now();
+    wavelet_coeff = m_Psi.adjoint() * image;
+    auto end = std::chrono::high_resolution_clock::now();
+    state.SetIterationTime(b_utilities::duration(start, end));
+  }
+}
+
 BENCHMARK_REGISTER_F(WaveletOperatorMPIFixture, Apply)
+    // //->Apply(b_utilities::Arguments)
+    ->RangeMultiplier(2)
+    ->Range(128, 128 << 3)
+    ->UseManualTime()
+    ->Repetitions(5)
+    ->ReportAggregatesOnly(true)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK_REGISTER_F(WaveletOperatorAdjointMPIFixture, Apply)
     // //->Apply(b_utilities::Arguments)
     ->RangeMultiplier(2)
     ->Range(128, 128 << 3)
