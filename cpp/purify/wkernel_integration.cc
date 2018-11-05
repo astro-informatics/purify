40aefa418a5bbdb611261d73c639e4a48fde5608
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2018-11-05 15:27:24 +0000

    added oversample ratio to wprojection kernel test

--- cpp/purify/wkernel_integration.cc
+++ cpp/purify/wkernel_integration.cc
@@ -17,7 +17,7 @@ t_complex exact_w_projection_integration_1d(const t_real u, const t_real v, cons
                                             const t_real &absolute_error,
                                             const t_real &relative_error,
                                             const integration::method method, t_uint &evaluations) {
-  if (oversample_ratio != 2)
+  if (std::abs(oversample_ratio - 2) > 1e-9)
     throw std::runtime_error("Oversample ratio != 2 not tested for wproj.");
   evaluations = 0;
   const auto func = [&](const Vector<t_real> &x) -> t_complex {
@@ -28,7 +28,7 @@ t_complex exact_w_projection_integration_1d(const t_real u, const t_real v, cons
     // Hankel transform
   };
   const Vector<t_real> xmin = Vector<t_real>::Zero(1);
-  const Vector<t_real> xmax = Vector<t_real>::Constant(1, std::max(oversample_ratio / 2., du));
+  const Vector<t_real> xmax = Vector<t_real>::Constant(1, std::min(oversample_ratio / 2., du / 2.));
   return 2. * constant::pi *
          integration::integrate(xmin, xmax, func, integration::norm_type::paired, absolute_error,
                                 relative_error, max_evaluations, method) /
@@ -43,21 +43,22 @@ t_complex exact_w_projection_integration(const t_real u, const t_real v, const t
                                          const t_uint &max_evaluations,
                                          const t_real &absolute_error, const t_real &relative_error,
                                          const integration::method method, t_uint &evaluations) {
-  if (oversample_ratio != 2)
+  if (std::abs(oversample_ratio - 2) > 1e-9)
     throw std::runtime_error("Oversample ratio != 2 not tested for wproj.");
   auto const func = [&](const Vector<t_real> &x) -> t_complex {
     evaluations++;
-    return ftkernelu(x(0)) * ftkernelv(x(1)) * chirp(w, 1. / du * x(0), 1. / dv * x(1)) *
-           phase(u, v, x(0), x(1)) *
+    return ftkernelu(x(0)) * ftkernelv(x(1)) * chirp(w, x(0) / du, x(1) / dv) *
+           phase(u, v, x(0), x(1))
            // checking bounds don't go past the horizon...
-           (((std::pow(x(0) / du, 2) + std::pow(x(1) / dv, 2)) < 1) ? 1. : 0.);
+           * (((std::pow(x(0) / du, 2) + std::pow(x(1) / dv, 2)) < 1) ? 1. : 0.);
   };
-  Vector<t_real> xmax = Vector<t_real>::Constant(2, oversample_ratio / 2.);
+  Vector<t_real> xmax = Vector<t_real>::Zero(2);
+  xmax(0) = std::min(oversample_ratio / 2., du / 2.);
+  xmax(1) = std::min(oversample_ratio / 2., dv / 2.);
   const Vector<t_real> xmin = -xmax;
   return integration::integrate(xmin, xmax, func, integration::norm_type::paired, absolute_error,
                                 relative_error, max_evaluations, method) /
-         std::max(oversample_ratio, oversample_ratio / du) /
-         std::max(oversample_ratio, oversample_ratio / dv);
+         (xmax(0) - xmin(0)) /(xmax(1) - xmin(1));
 }
 }  // namespace projection_kernels
 }  // namespace purify
