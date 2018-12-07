dcd5070402d9b2cada31b2dd0a1d55291aec4119
David Pérez-Suárez <dps.helio@gmail.com>
2018-12-07 17:26:50 +0000

    [travis] Include coverage on the run

---
+++ .ci/coverage.sh
@@ -0,0 +1,6 @@
+
+lcov --directory . --capture --output-file coverage.info # capture coverage info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
+lcov --list coverage.info #debug info
+# Uploading report to CodeCov
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
