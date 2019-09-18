bb265d88991303de28cc1be826b519af748d97c6
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2019-09-19 00:46:20 +0100

    added on the fly gridding for du and dv of different sizes

--- cpp/purify/fly_operators.h
+++ cpp/purify/fly_operators.h
@@ -147,6 +147,138 @@ std::tuple<sopt::OperatorFunction<T>, sopt::OperatorFunction<T>> init_on_the_fly
   };
   return std::make_tuple(degrid, grid);
 }
+
+template <class T>
+std::tuple<sopt::OperatorFunction<T>, sopt::OperatorFunction<T>> init_on_the_fly_gridding_matrix_2d(
+    const Vector<t_real> &u, const Vector<t_real> &v, const Vector<t_complex> &weights,
+    const t_uint &imsizey_, const t_uint &imsizex_, const t_real &oversample_ratio,
+    const std::function<t_real(t_real)> &kernelu, const std::function<t_real(t_real)> &kernelv,
+    const t_uint Ju, const t_uint Jv, const t_int total_samples) {
+  const t_uint ftsizev_ = std::floor(imsizey_ * oversample_ratio);
+  const t_uint ftsizeu_ = std::floor(imsizex_ * oversample_ratio);
+  const t_uint rows = u.size();
+  const t_uint cols = ftsizeu_ * ftsizev_;
+  if (u.size() != v.size())
+    throw std::runtime_error(
+        "Size of u and v vectors are not the same for creating gridding matrix.");
+
+  const std::shared_ptr<Vector<t_real>> u_ptr = std::make_shared<Vector<t_real>>(u);
+  const std::shared_ptr<Vector<t_real>> v_ptr = std::make_shared<Vector<t_real>>(v);
+  const std::shared_ptr<Vector<t_complex>> weights_ptr =
+      std::make_shared<Vector<t_complex>>(weights);
+  const t_complex I(0, 1);
+  const t_int ju_max = std::min(Ju, ftsizeu_);
+  const t_int jv_max = std::min(Jv, ftsizev_);
+  const auto samples_u = kernels::kernel_samples(
+      total_samples, [&](const t_real x) { return kernelu(x * ju_max * 0.5); });
+  const auto samples_v = kernels::kernel_samples(
+      total_samples, [&](const t_real x) { return kernelv(x * jv_max * 0.5); });
+  std::set<t_int> nonZeros_set;
+  for (t_int m = 0; m < rows; ++m) {
+    t_complex result = 0;
+    const t_real u_val = (*u_ptr)(m);
+    const t_real v_val = (*v_ptr)(m);
+    const t_real k_u = std::floor(u_val - ju_max * 0.5);
+    const t_real k_v = std::floor(v_val - jv_max * 0.5);
+    for (t_int jv = 1; jv < jv_max + 1; ++jv) {
+      const t_uint p = utilities::mod(k_v + jv, ftsizev_);
+      for (t_int ju = 1; ju < ju_max + 1; ++ju) {
+        const t_uint q = utilities::mod(k_u + ju, ftsizeu_);
+        const t_uint index = utilities::sub2ind(p, q, ftsizev_, ftsizeu_);
+        nonZeros_set.insert(index);
+      }
+    }
+  }
+
+  std::vector<t_int> nonZeros_vec(nonZeros_set.begin(), nonZeros_set.end());
+  std::sort(nonZeros_vec.data(), nonZeros_vec.data() + nonZeros_vec.size());
+  SparseVector<t_int> mapping(ftsizev_ * ftsizeu_);
+  mapping.reserve(nonZeros_vec.size());
+  for (t_int index = 0; index < nonZeros_vec.size(); index++)
+    mapping.coeffRef(nonZeros_vec[index]) = index;
+  PURIFY_LOW_LOG("Non Zero grid locations: {} ", mapping.nonZeros());
+
+  const auto degrid = [rows, ju_max, jv_max, I, u_ptr, v_ptr, weights_ptr, samples_u, samples_v,
+                       total_samples, ftsizeu_, ftsizev_](T &output, const T &input) {
+    output = T::Zero(u_ptr->size());
+    assert(input.size() == ftsizeu_ * ftsizev_);
+#pragma omp parallel for
+    for (t_int m = 0; m < rows; ++m) {
+      t_complex result = 0;
+      const t_real u_val = (*u_ptr)(m);
+      const t_real v_val = (*v_ptr)(m);
+      const t_real k_u = std::floor(u_val - ju_max * 0.5);
+      const t_real k_v = std::floor(v_val - jv_max * 0.5);
+      for (t_int jv = 1; jv < jv_max + 1; ++jv) {
+        const t_uint p = utilities::mod(k_v + jv, ftsizev_);
+        const t_real c_0 = static_cast<t_int>(
+            std::floor(2 * std::abs(v_val - (k_v + jv)) * total_samples / jv_max));
+        assert(c_0 >= 0);
+        assert(c_0 < total_samples);
+        const t_real kernelv_val = samples_v[c_0] * (1. - (2 * (p % 2)));
+        for (t_int ju = 1; ju < ju_max + 1; ++ju) {
+          const t_uint q = utilities::mod(k_u + ju, ftsizeu_);
+          const t_int i_0 = static_cast<t_int>(
+              std::floor(2 * std::abs(u_val - (k_u + ju)) * total_samples / ju_max));
+          assert(i_0 >= 0);
+          assert(i_0 < total_samples);
+          const t_real kernelu_val = samples_u[i_0] * (1. - (2 * (q % 2)));
+          const t_uint index = utilities::sub2ind(p, q, ftsizev_, ftsizeu_);
+          const t_real sign = kernelu_val * kernelv_val;
+          result += input(index) * sign;
+        }
+      }
+      output(m) = result;
+    }
+    output.array() *= (*weights_ptr).array();
+  };
+
+  const auto grid = [rows, ju_max, jv_max, I, u_ptr, v_ptr, weights_ptr, samples_u, samples_v,
+                     total_samples, ftsizeu_, ftsizev_, mapping,
+                     nonZeros_vec](T &output, const T &input) {
+    const t_int N = ftsizeu_ * ftsizev_;
+    output = T::Zero(N);
+    T output_compressed = T::Zero(nonZeros_vec.size() * omp_get_max_threads());
+    assert(output.size() == N);
+#pragma omp parallel for
+    for (t_int m = 0; m < rows; ++m) {
+      t_complex result = 0;
+      const t_int shift = omp_get_thread_num() * nonZeros_vec.size();
+      const t_real u_val = (*u_ptr)(m);
+      const t_real v_val = (*v_ptr)(m);
+      const t_real k_u = std::floor(u_val - ju_max * 0.5);
+      const t_real k_v = std::floor(v_val - jv_max * 0.5);
+      const t_complex vis = input(m) * std::conj((*weights_ptr)(m));
+      for (t_int jv = 1; jv < jv_max + 1; ++jv) {
+        const t_uint p = utilities::mod(k_v + jv, ftsizev_);
+        const t_real c_0 = static_cast<t_int>(
+            std::floor(2 * std::abs(v_val - (k_v + jv)) * total_samples / jv_max));
+        assert(c_0 >= 0);
+        assert(c_0 < total_samples);
+        const t_real kernelv_val = samples_v[c_0] * (1. - (2 * (p % 2)));
+        for (t_int ju = 1; ju < ju_max + 1; ++ju) {
+          const t_uint q = utilities::mod(k_u + ju, ftsizeu_);
+          const t_int i_0 = static_cast<t_int>(
+              std::floor(2 * std::abs(u_val - (k_u + ju)) * total_samples / ju_max));
+          assert(i_0 >= 0);
+          assert(i_0 < total_samples);
+          const t_real kernelu_val = samples_u[i_0] * (1. - (2 * (q % 2)));
+          const t_int index = utilities::sub2ind(p, q, ftsizev_, ftsizeu_);
+          const t_complex result = kernelu_val * kernelv_val * vis;
+          output_compressed(mapping.coeff(index) + shift) += result;
+        }
+      }
+    }
+    for (t_int m = 1; m < omp_get_max_threads(); m++) {
+      const t_int loop_shift = m * nonZeros_vec.size();
+      output_compressed.segment(0, nonZeros_vec.size()) +=
+          output_compressed.segment(loop_shift, nonZeros_vec.size());
+    }
+    for (t_int index = 0; index < nonZeros_vec.size(); index++)
+      output(nonZeros_vec[index]) += output_compressed(index);
+  };
+  return std::make_tuple(degrid, grid);
+}
 #ifdef PURIFY_MPI
 //! on the fly gridding matrix application with compression
 template <class T>
