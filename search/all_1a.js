1d67c83ee448e1d9dec05285b07ad66043bbc114
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-03 01:15:39 +0000

    Deploy astro-informatics/purify to github.com/astro-informatics/purify.git:gh-pages

--- search/all_1a.js
+++ search/all_1a.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['_7econjugategradient',['~ConjugateGradient',['classsopt_1_1ConjugateGradient.html#a4b3876b1b56c3256759c5a741803ddd8',1,'sopt::ConjugateGradient']]],
   ['_7eforwardbackward',['~ForwardBackward',['classsopt_1_1algorithm_1_1ForwardBackward.html#a05e2e99267735a28809c658a6fa08c03',1,'sopt::algorithm::ForwardBackward']]],
-  ['_7eimagingforwardbackward',['~ImagingForwardBackward',['classsopt_1_1algorithm_1_1ImagingForwardBackward.html#af2c1c67e818111aa1a3332d910a97ab4',1,'sopt::algorithm::ImagingForwardBackward']]],
-  ['_7eimagingprimaldual',['~ImagingPrimalDual',['classsopt_1_1algorithm_1_1ImagingPrimalDual.html#a3c3df825f7a6ef2d0e2992d088e9a862',1,'sopt::algorithm::ImagingPrimalDual']]],
+  ['_7eimagingforwardbackward',['~ImagingForwardBackward',['classsopt_1_1algorithm_1_1ImagingForwardBackward.html#af2c1c67e818111aa1a3332d910a97ab4',1,'sopt::algorithm::ImagingForwardBackward::~ImagingForwardBackward()'],['classsopt_1_1algorithm_1_1ImagingForwardBackward.html#af2c1c67e818111aa1a3332d910a97ab4',1,'sopt::algorithm::ImagingForwardBackward::~ImagingForwardBackward()']]],
+  ['_7eimagingprimaldual',['~ImagingPrimalDual',['classsopt_1_1algorithm_1_1ImagingPrimalDual.html#a3c3df825f7a6ef2d0e2992d088e9a862',1,'sopt::algorithm::ImagingPrimalDual::~ImagingPrimalDual()'],['classsopt_1_1algorithm_1_1ImagingPrimalDual.html#a3c3df825f7a6ef2d0e2992d088e9a862',1,'sopt::algorithm::ImagingPrimalDual::~ImagingPrimalDual()']]],
   ['_7eimagingproximaladmm',['~ImagingProximalADMM',['classsopt_1_1algorithm_1_1ImagingProximalADMM.html#a6a491f56cebaac4300a3a76827e07c33',1,'sopt::algorithm::ImagingProximalADMM']]],
   ['_7epowermethod',['~PowerMethod',['classsopt_1_1algorithm_1_1PowerMethod.html#a527c99f4991f119ec96003d356b9eac5',1,'sopt::algorithm::PowerMethod']]],
   ['_7eprimaldual',['~PrimalDual',['classsopt_1_1algorithm_1_1PrimalDual.html#a0843de67676f855c792b906936512499',1,'sopt::algorithm::PrimalDual']]],
--- search/files_b.js
+++ search/files_b.js
@@ -3,6 +3,8 @@ var searchData=
   ['l1_5fnorm_2ecc',['l1_norm.cc',['http://astro-informatics.github.io/sopt/l1__norm_8cc.html',1,'']]],
   ['l1_5fproximal_2ecc',['l1_proximal.cc',['http://astro-informatics.github.io/sopt/benchmarks_2l1__proximal_8cc.html',1,'(Global Namespace)'],['http://astro-informatics.github.io/sopt/examples_2l1__proximal_8cc.html',1,'(Global Namespace)'],['http://astro-informatics.github.io/sopt/regressions_2l1__proximal_8cc.html',1,'(Global Namespace)']]],
   ['l1_5fproximal_2eh',['l1_proximal.h',['http://astro-informatics.github.io/sopt/l1__proximal_8h.html',1,'']]],
+  ['l2_5fforward_5fbackward_2eh',['l2_forward_backward.h',['http://astro-informatics.github.io/sopt/l2__forward__backward_8h.html',1,'']]],
+  ['l2_5fprimal_5fdual_2eh',['l2_primal_dual.h',['http://astro-informatics.github.io/sopt/l2__primal__dual_8h.html',1,'']]],
   ['linear_5ftransform_2ecc',['linear_transform.cc',['http://astro-informatics.github.io/sopt/linear__transform_8cc.html',1,'']]],
   ['linear_5ftransform_2eh',['linear_transform.h',['http://astro-informatics.github.io/sopt/linear__transform_8h.html',1,'']]],
   ['logging_2edisabled_2eh',['logging.disabled.h',['../logging_8disabled_8h.html',1,'(Global Namespace)'],['http://astro-informatics.github.io/sopt/logging_8disabled_8h.html',1,'(Global Namespace)']]],
