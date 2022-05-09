70e9e82d25b4eb94aef66ab769e7e24fab533ccc
Tuomas Koskela <t.koskela@ucl.ac.uk>
2022-05-09 13:01:47 +0100

    Apply changes done in sopt

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,7 @@ from conans import ConanFile, CMake
 class PurifyConan(ConanFile):
     requires = ["eigen/3.3.7","spdlog/1.9.2","catch2/2.13.7","benchmark/1.6.0","yaml-cpp/0.6.3", "boost/1.78.0", "cfitsio/4.0.0"]
     generators = "cmake"
-    options = {"regressions": ['on','off'],
-               "docs":['on','off'],
+    options = {"docs":['on','off'],
                "examples":['on','off'],
                "tests":['on','off'],
                "benchmarks":['on','off'],
@@ -15,8 +14,7 @@ class PurifyConan(ConanFile):
                "af": ['on', 'off'],
                "cimg": ['on','off'],
                "casa": ['on','off']}
-    default_options = {"regressions": 'off',
-                       "docs": 'off',
+    default_options = {"docs": 'off',
                        "examples":'off',
                        "tests": 'on',
                        "benchmarks": 'off',
@@ -29,17 +27,28 @@ class PurifyConan(ConanFile):
                        "casa": 'off'}
     
     def requirements(self):
+
         if self.options.regressions == 'on':
             self.requires("fftw/3.3.9")
 
+        if self.options.docs == 'on' or self.options.examples == 'on':
+            # To prevent a conflict in the version of zlib required by libtiff and
+            # doxygen, override the version of zlib when either of them is required
+            self.requires("zlib/1.2.12", override=True)
+
         if self.options.examples == 'on' or self.options.regressions == 'on':
             self.requires("libtiff/4.0.9")
+
+        if self.options.logging == 'on':
+            self.requires("spdlog/1.9.2")
+
+        if self.options.docs == 'on':
+            self.requires("doxygen/1.9.2")
 
 
     def build(self):
       cmake = CMake(self)
       
-      cmake.definitions['regressions'] = self.options.regressions
       cmake.definitions['docs'] = self.options.docs
       cmake.definitions['examples'] = self.options.examples
       cmake.definitions['tests'] = self.options.tests
@@ -52,11 +61,11 @@ class PurifyConan(ConanFile):
       cmake.definitions['docimg'] = self.options.cimg
       cmake.definitions['docasa'] = self.options.casa
 
-      cmake.definitions['CMAKE_C_COMPILER_LAUNCHER'] = "ccache"
-      cmake.definitions['CMAKE_CXX_COMPILER_LAUNCHER'] = "ccache"
+      if self.options.docs == 'off':
+          cmake.definitions['CMAKE_C_COMPILER_LAUNCHER'] = "ccache"
+          cmake.definitions['CMAKE_CXX_COMPILER_LAUNCHER'] = "ccache"
 
       # TODO: Switch this on for Debug, off for Release
-      cmake.definitions['CMAKE_VERBOSE_MAKEFILE:BOOL'] = "ON"
-      
+      cmake.definitions['CMAKE_VERBOSE_MAKEFILE:BOOL'] = "ON"      
       cmake.configure()
       cmake.build()
