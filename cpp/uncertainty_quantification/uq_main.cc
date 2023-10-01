0920091a657caa11e72b56238f2cfc6713b3ed4c
Michael McLeod <michaelmcleod@ucl.ac.uk>
2023-10-01 20:12:56 +0100

    Add Purify config file read to UQ

--- cpp/uncertainty_quantification/uq_main.cc
+++ cpp/uncertainty_quantification/uq_main.cc
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include "yaml-cpp/yaml.h"
 #include "purify/yaml-parser.h"
+#include "purify/setup_utils.h"
 
 using VectorC = sopt::Vector<std::complex<double>>;
 
@@ -24,10 +25,74 @@ int main(int argc, char **argv)
     const std::string config_path = argv[1];
     const YAML::Node UQ_config = YAML::LoadFile(config_path);
 
-    const std::string measurements_path = UQ_config["measurements_path"].as<std::string>();
+    // Load the Reference and Surrogate images
     const std::string ref_image_path = UQ_config["reference_image_path"].as<std::string>();
     const std::string surrogate_image_path = UQ_config["surrogate_image_path"].as<std::string>();
-    const std::string purify_config_path = UQ_config["purify_config_path"].as<std::string>();
+    const auto reference_image = purify::pfitsio::read2d(ref_image_path);
+    const VectorC reference_vector = VectorC::Map(reference_image.data(), reference_image.size());
+    const auto surrogate_image = purify::pfitsio::read2d(surrogate_image_path);
+    const VectorC surrogate_vector = VectorC::Map(surrogate_image.data(), surrogate_image.size());
+
+
+    const uint imsize_x = reference_image.cols();
+    const uint imsize_y = reference_image.rows();
+
+    // Prepare operators and data using either purify config 
+    // If no purify config use basic version for now
+    purify::utilities::vis_params measurement_data;
+    std::shared_ptr<sopt::LinearTransform<Vector<t_complex>>> measurement_operator;
+    if(UQ_config["purify_config_file"])
+    {
+        YamlParser purify_config = YamlParser(UQ_config["purify_config_file"].as<std::string>());
+    
+        const auto [mop_algo, wop_algo, using_mpi] = selectOperators(purify_config);
+        auto [uv_data, sigma, measurement_op_eigen_vector, image_index, w_stacks] = getInputData(purify_config,
+                                                                                                 mop_algo,
+                                                                                                 wop_algo,
+                                                                                                 using_mpi);
+    
+        auto [transform, operator_norm] = createMeasurementOperator(purify_config,
+                                                                    mop_algo,
+                                                                    wop_algo,
+                                                                    using_mpi,
+                                                                    image_index,
+                                                                    w_stacks,
+                                                                    uv_data,
+                                                                    measurement_op_eigen_vector);
+    
+        const waveletInfo wavelets = createWaveletOperator(purify_config, wop_algo);
+    
+        t_real const flux_scale = 1.;
+        uv_data.vis = uv_data.vis.array() * uv_data.weights.array() / flux_scale;
+
+        measurement_data = uv_data;
+        measurement_operator = transform;
+    }
+    else
+    {
+        const std::string measurements_path = UQ_config["measurements_path"].as<std::string>();
+        // Load the images and measurements
+        const purify::utilities::vis_params uv_data = purify::utilities::read_visibility(measurements_path, false);
+
+        // This is the measurement operator used in the test but this should probably be selectable
+        auto const transform = 
+        purify::factory::measurement_operator_factory<sopt::Vector<std::complex<double>>>(
+            purify::factory::distributed_measurement_operator::serial,
+            uv_data,
+            imsize_y,
+            imsize_x,
+            1,
+            1,
+            2,
+            kernels::kernel_from_string.at("kb"),
+            4,
+            4);
+
+        measurement_operator = transform;
+        measurement_data = uv_data;
+    }
+
+    // Set up confidence and objective function params
     double confidence;
     double alpha;
     if((UQ_config["confidence_interval"]) && (UQ_config["alpha"]))
@@ -52,19 +117,7 @@ int main(int argc, char **argv)
     }
     const double sigma = UQ_config["sigma"].as<double>();
     const double gamma = UQ_config["gamma"].as<double>();
-    purify::YamlParser purify_config = purify::YamlParser(purify_config_path);
 
-    // Load the images and measurements
-    const purify::utilities::vis_params measurement_data = purify::utilities::read_visibility(measurements_path, false);
-
-    const auto reference_image = purify::pfitsio::read2d(ref_image_path);
-    const VectorC reference_vector = VectorC::Map(reference_image.data(), reference_image.size());
-    const auto surrogate_image = purify::pfitsio::read2d(surrogate_image_path);
-    const VectorC surrogate_vector = VectorC::Map(surrogate_image.data(), surrogate_image.size());
-
-
-    const uint imsize_x = reference_image.cols();
-    const uint imsize_y = reference_image.rows();
 
     if((imsize_x != surrogate_image.cols()) || (imsize_y != surrogate_image.rows()))
     {
@@ -72,25 +125,14 @@ int main(int argc, char **argv)
         return 2;
     } 
 
-    // This is the measurement operator used in the test but this should probably be selectable
-    auto const measurement_operator = 
-        purify::factory::measurement_operator_factory<sopt::Vector<std::complex<double>>>(
-            purify::factory::distributed_measurement_operator::serial,
-            measurement_data,
-            imsize_y,
-            imsize_x,
-            purify_config.cellsizey(),
-            purify_config.cellsizex(),
-            purify_config.oversampling(),
-            purify::kernels::kernel_from_string.at(purify_config.kernel()),
-            purify_config.sim_J()
-        );
-
+    
     if( ((*measurement_operator) * reference_vector).size() != measurement_data.vis.size())
     {
         std::cout << "Image size is not compatible with the measurement operator and data provided." << std::endl;
         return 3;
     }
+
+
 
     // Calculate the posterior function for the reference image
     // posterior = likelihood + prior
