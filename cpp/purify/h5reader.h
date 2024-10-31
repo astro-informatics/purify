9fdbb56f7bfa448b09068be0cdb45af28dae68c7
Christian Gutschow <chris.g@cern.ch>
2024-10-31 10:00:32 +0000

    utter nonsense

--- cpp/purify/h5reader.h
+++ cpp/purify/h5reader.h
@@ -22,11 +22,9 @@ namespace purify::H5 {
 
 /// @brief Purify interface class to handle HDF5 input files
 class H5Handler {
+  using DatsetMap = std::map<std::string, HighFive::DataSet>;
 
-  using DatsetMap = std::map<std::string,HighFive::DataSet>;
-
-  public:
-
+ public:
   H5Handler() = delete;
 
   H5Handler(const std::string& filename) : _file(filename) {}
@@ -40,16 +38,14 @@ class H5Handler {
 
   /// Method to read a dataset slice with
   /// slices evenly split across MPI ranks
-  template<typename T = double>
-  std::vector<T> distread(const std::string& label,
-                          const sopt::mpi::Communicator& comm) {
-    if (_ds.find(label) == _ds.end()) { // load the dataset
+  template <typename T = double>
+  std::vector<T> distread(const std::string& label, const sopt::mpi::Communicator& comm) {
+    if (_ds.find(label) == _ds.end()) {  // load the dataset
       _ds[label] = std::move(_file.getDataSet(label));
     }
     const auto& dims = _ds[label].getDimensions();
     size_t datalen = dims.at(0);
-    if (datalen < comm.size())
-      throw std::runtime_error("Not enough data for each MPI rank!");
+    if (datalen < comm.size()) throw std::runtime_error("Not enough data for each MPI rank!");
 
     // Read the relevant slice of the dataset
     // @todo Cache the calculation of starting point/slice length?
@@ -58,28 +54,27 @@ class H5Handler {
       len += datalen % comm.size();
     }
     size_t pos = comm.rank() * len;
-    std::vector<T> data; data.reserve(len);
+    std::vector<T> data;
+    data.reserve(len);
     _ds[label].select({pos}, {len}).read(data);
     return data;
   }
 
   /// Method to stochastically draw a subset
   /// of the distributed dataset slice
-  template<typename T = double>
+  template <typename T = double>
   std::vector<T> stochread(const std::string& label, size_t N,
                            const sopt::mpi::Communicator& comm) {
     std::vector<T> data = distread(label, comm);
-    if (N > data.size())
-      throw std::runtime_error("Not enough data for requested dataset size!");
+    if (N > data.size()) throw std::runtime_error("Not enough data for requested dataset size!");
     // stochastic shuffle
     std::mt19937 rng(rnd_device());
     std::shuffle(std::begin(data), std::end(data), rng);
-    data.resize(N); // clip dataset to first N elements
+    data.resize(N);  // clip dataset to first N elements
     return data;
   }
 
-  private:
-
+ private:
   /// HDF5 file
   const HighFive::File _file;
 
