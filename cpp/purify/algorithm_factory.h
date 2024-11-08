873b58fa48030ccb10de795dd44469f424c467cd
Michael McLeod <michaelmcleod@ucl.ac.uk>
2024-11-08 14:35:10 +0000

    Fix normalisation of initial guess (PD)

--- cpp/purify/algorithm_factory.h
+++ cpp/purify/algorithm_factory.h
@@ -274,6 +274,7 @@ primaldual_factory(
       .Phi(*measurements)
       .tau(0.5 / (op_norm * op_norm + 1))
       .xi(1.)
+      .nu(op_norm * op_norm)
       .sigma(1.);
 #ifdef PURIFY_MPI
   ConvergenceType obj_conv = ConvergenceType::mpi_global;
