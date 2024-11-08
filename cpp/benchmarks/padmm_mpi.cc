06f47320349cbf56873a5dfb06ff1ced38940487
Tuomas Koskela <t.koskela@ucl.ac.uk>
2024-11-08 11:56:57 +0000

    Clarify names, call the right padmm object

--- cpp/benchmarks/padmm_mpi.cc
+++ cpp/benchmarks/padmm_mpi.cc
@@ -11,6 +11,7 @@
 #include "purify/operators.h"
 #include "purify/utilities.h"
 #include "purify/wavelet_operator_factory.h"
+#include "purify/measurement_operator_factory.h"
 #include <sopt/imaging_padmm.h>
 #include <sopt/mpi/communicator.h>
 #include <sopt/mpi/session.h>
@@ -38,26 +39,29 @@ class PadmmFixtureMPI : public ::benchmark::Fixture {
     const t_real FoV = 1;  // deg
     const t_real cellsize = FoV / m_imsizex * 60. * 60.;
     const bool w_term = false;
-    // algorithm 1
-    if (state.range(4) == 1)
-      m_measurements1 = measurementoperator::init_degrid_operator_2d_mpi<Vector<t_complex>>(
-          m_world, m_uv_data, m_image.rows(), m_image.cols(), cellsize, cellsize, 2,
+    if (state.range(4) == 1) {
+      PURIFY_INFO("Using distributed image MPI algorithm");
+      m_measurements_distribute_image = factory::measurement_operator_factory<Vector<t_complex>>(
+	 factory::distributed_measurement_operator::mpi_distribute_image,
+	 m_uv_data, m_image.rows(), m_image.cols(), cellsize, cellsize, 2,
+	 kernels::kernel::kb, m_kernel, m_kernel, w_term);
+    }
+
+    if (state.range(4) == 2) {
+      PURIFY_INFO("Using distributed grid MPI algorithm");
+      m_measurements_distribute_grid = factory::measurement_operator_factory<Vector<t_complex>>(
+          factory::distributed_measurement_operator::mpi_distribute_grid,
+	  m_uv_data, m_image.rows(), m_image.cols(), cellsize, cellsize, 2,
           kernels::kernel::kb, m_kernel, m_kernel, w_term);
-    // algorithm 3
-    if (state.range(4) == 3)
-      m_measurements3 = measurementoperator::init_degrid_operator_2d<Vector<t_complex>>(
-          m_world, m_uv_data, m_image.rows(), m_image.cols(), cellsize, cellsize, 2,
-          kernels::kernel::kb, m_kernel, m_kernel, w_term);
+    }
+
+    m_sigma = 0.016820222945913496 * std::sqrt(2);  // see test_parameters file
+
   }
 
   void TearDown(const ::benchmark::State &state) {}
 
   sopt::mpi::Communicator m_world;
-
-  // const sopt::wavelets::SARA m_sara{
-  //     std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u),
-  //     std::make_tuple("DB3", 3u),   std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u),
-  //     std::make_tuple("DB6", 3u),   std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)};
 
   std::vector<std::tuple<std::string, t_uint>> const m_sara{
       std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u),
@@ -70,59 +74,22 @@ class PadmmFixtureMPI : public ::benchmark::Fixture {
 
   utilities::vis_params m_uv_data;
   t_real m_epsilon;
+  t_real m_sigma;
   t_uint m_kernel;
 
-  std::shared_ptr<sopt::LinearTransform<Vector<t_complex>> const> m_measurements1;
-  std::shared_ptr<sopt::LinearTransform<Vector<t_complex>> const> m_measurements3;
+  std::shared_ptr<sopt::LinearTransform<Vector<t_complex>> const> m_measurements_distribute_image;
+  std::shared_ptr<sopt::LinearTransform<Vector<t_complex>> const> m_measurements_distribute_grid;
   std::shared_ptr<sopt::algorithm::ImagingProximalADMM<t_complex>> m_padmm;
 };
 
-BENCHMARK_DEFINE_F(PadmmFixtureMPI, ApplyAlgo1)(benchmark::State &state) {
+BENCHMARK_DEFINE_F(PadmmFixtureMPI, DistributeImage)(benchmark::State &state) {
   // Create the algorithm - has to be done there to reset the internal state.
   // If done in the fixture repeats would start at the solution and converge immediately.
-
-  // sopt::wavelets::SARA saraDistr = sopt::wavelets::distribute_sara(m_sara, m_world);
-  // auto const Psi =
-  //     sopt::linear_transform<t_complex>(saraDistr, m_image.rows(), m_image.cols(), m_world);
-  // t_real gamma =
-  //     utilities::step_size(m_uv_data.vis, m_measurements1,
-  //                          std::make_shared<sopt::LinearTransform<Vector<t_complex>> const>(Psi),
-  //                          saraDistr.size()) *
-  //     1e-3;
-  // gamma = m_world.all_reduce(gamma, MPI_MAX);
-  // std::shared_ptr<sopt::algorithm::ImagingProximalADMM<t_complex>> padmm =
-  //     std::make_shared<sopt::algorithm::ImagingProximalADMM<t_complex>>(m_uv_data.vis);
-  // padmm->itermax(state.range(3) + 1)
-  //     .gamma(gamma)
-  //     .relative_variation(1e-3)
-  //     .l2ball_proximal_epsilon(m_epsilon)
-  //     // communicator ensuring l1 norm in l1 proximal is global
-  //     .l1_proximal_adjoint_space_comm(m_world)
-  //     .tight_frame(false)
-  //     .l1_proximal_tolerance(1e-2)
-  //     .l1_proximal_nu(1)
-  //     .l1_proximal_itermax(20)
-  //     .l1_proximal_positivity_constraint(true)
-  //     .l1_proximal_real_constraint(true)
-  //     .residual_tolerance(m_epsilon)
-  //     .lagrange_update_scale(0.9)
-  //     .nu(1e0)
-  //     .Psi(Psi)
-  //     .Phi(*m_measurements1);
-  // std::weak_ptr<decltype(padmm)::element_type> const padmm_weak(padmm);
-  // padmm->residual_convergence(
-  //     factory::l2_convergence_factory<t_complex>(factory::ConvergenceType::mpi_local,
-  //     padmm_weak));
-  // padmm->objective_convergence(
-  //     factory::l1_convergence_factory<t_complex>(factory::ConvergenceType::mpi_local,
-  //     padmm_weak));
-
   auto const wavelets = factory::wavelet_operator_factory<Vector<t_complex>>(
       factory::distributed_wavelet_operator::mpi_sara, m_sara, m_imsizey, m_imsizex);
-  t_real const sigma =
-      m_world.broadcast(0.016820222945913496) * std::sqrt(2);  // see test_parameters file
-  auto const padmm = factory::padmm_factory<sopt::algorithm::ImagingProximalADMM<t_complex>>(
-      factory::algo_distribution::mpi_distributed, m_measurements1, wavelets, m_uv_data, sigma,
+
+  m_padmm = factory::padmm_factory<sopt::algorithm::ImagingProximalADMM<t_complex>>(
+      factory::algo_distribution::mpi_distributed, m_measurements_distribute_image, wavelets, m_uv_data, m_sigma,
       m_imsizey, m_imsizex, m_sara.size(), state.range(3) + 1, true, true, false, 1e-3, 1e-2, 50,
       1.0, 1.0);
 
@@ -136,52 +103,14 @@ BENCHMARK_DEFINE_F(PadmmFixtureMPI, ApplyAlgo1)(benchmark::State &state) {
   }
 }
 
-BENCHMARK_DEFINE_F(PadmmFixtureMPI, ApplyAlgo3)(benchmark::State &state) {
+BENCHMARK_DEFINE_F(PadmmFixtureMPI, DistributeGrid)(benchmark::State &state) {
   // Create the algorithm - has to be done there to reset the internal state.
   // If done in the fixture repeats would start at the solution and converge immediately.
 
-  // sopt::wavelets::SARA saraDistr = sopt::wavelets::distribute_sara(m_sara, m_world);
-  // auto const Psi =
-  //     sopt::linear_transform<t_complex>(saraDistr, m_image.rows(), m_image.cols(), m_world);
-  // t_real gamma =
-  //     utilities::step_size(m_uv_data.vis, m_measurements3,
-  //                          std::make_shared<sopt::LinearTransform<Vector<t_complex>> const>(Psi),
-  //                          saraDistr.size()) *
-  //     1e-3;
-  // gamma = m_world.all_reduce(gamma, MPI_MAX);
-  // std::shared_ptr<sopt::algorithm::ImagingProximalADMM<t_complex>> padmm =
-  //     std::make_shared<sopt::algorithm::ImagingProximalADMM<t_complex>>(m_uv_data.vis);
-  // padmm->itermax(state.range(3) + 1)
-  //     .gamma(gamma)
-  //     .relative_variation(1e-3)
-  //     .l2ball_proximal_epsilon(m_epsilon)
-  //     // communicator ensuring l1 norm in l1 proximal is global
-  //     .l1_proximal_adjoint_space_comm(m_world)
-  //     .tight_frame(false)
-  //     .l1_proximal_tolerance(1e-2)
-  //     .l1_proximal_nu(1)
-  //     .l1_proximal_itermax(10)
-  //     .l1_proximal_positivity_constraint(true)
-  //     .l1_proximal_real_constraint(true)
-  //     .residual_tolerance(m_epsilon)
-  //     .lagrange_update_scale(0.9)
-  //     .nu(1e0)
-  //     .Psi(Psi)
-  //     .Phi(*m_measurements3);
-  // std::weak_ptr<decltype(padmm)::element_type> const padmm_weak(padmm);
-  // padmm->residual_convergence(
-  //     factory::l2_convergence_factory<t_complex>(factory::ConvergenceType::mpi_local,
-  //     padmm_weak));
-  // padmm->objective_convergence(
-  //     factory::l1_convergence_factory<t_complex>(factory::ConvergenceType::mpi_local,
-  //     padmm_weak));
-
   auto const wavelets = factory::wavelet_operator_factory<Vector<t_complex>>(
       factory::distributed_wavelet_operator::mpi_sara, m_sara, m_imsizey, m_imsizex);
-  t_real const sigma =
-      m_world.broadcast(0.016820222945913496) * std::sqrt(2);  // see test_parameters file
   auto const padmm = factory::padmm_factory<sopt::algorithm::ImagingProximalADMM<t_complex>>(
-      factory::algo_distribution::mpi_distributed, m_measurements3, wavelets, m_uv_data, sigma,
+											     factory::algo_distribution::mpi_distributed, m_measurements_distribute_grid, wavelets, m_uv_data, m_sigma,
       m_imsizey, m_imsizex, m_sara.size(), state.range(3) + 1, true, true, false, 1e-3, 1e-2, 50,
       1.0, 1.0);
 
@@ -195,7 +124,7 @@ BENCHMARK_DEFINE_F(PadmmFixtureMPI, ApplyAlgo3)(benchmark::State &state) {
   }
 }
 
-BENCHMARK_REGISTER_F(PadmmFixtureMPI, ApplyAlgo1)
+BENCHMARK_REGISTER_F(PadmmFixtureMPI, DistributeImage)
     //->Apply(b_utilities::Arguments)
     ->Args({1024, static_cast<t_int>(1e6), 4, 10, 1})
     ->Args({1024, static_cast<t_int>(1e7), 4, 10, 1})
@@ -205,10 +134,10 @@ BENCHMARK_REGISTER_F(PadmmFixtureMPI, ApplyAlgo1)
     ->Repetitions(3)  //->ReportAggregatesOnly(true)
     ->Unit(benchmark::kMillisecond);
 
-BENCHMARK_REGISTER_F(PadmmFixtureMPI, ApplyAlgo3)
+BENCHMARK_REGISTER_F(PadmmFixtureMPI, DistributeGrid)
     //->Apply(b_utilities::Arguments)
-    ->Args({1024, static_cast<t_int>(1e6), 4, 10, 3})
-    ->Args({1024, static_cast<t_int>(1e7), 4, 10, 3})
+    ->Args({1024, static_cast<t_int>(1e6), 4, 10, 2})
+    ->Args({1024, static_cast<t_int>(1e7), 4, 10, 2})
     ->UseManualTime()
     ->MinTime(10.0)
     ->MinWarmUpTime(5.0)
