b4e48424d31845981a1cc5cbc8c4a474362ed15c
Michael McLeod <michaelmcleod@ucl.ac.uk>
2024-11-22 13:06:24 +0000

    Match new namespacing

--- cpp/purify/algorithm_factory.h
+++ cpp/purify/algorithm_factory.h
@@ -217,7 +217,7 @@ fb_factory(const algo_distribution dist,
 #endif
   }
   case (g_proximal_type::Indicator): {
-    g = std::make_shared<RealIndicator<t_scalar>>();
+    g = std::make_shared<sopt::algorithm::RealIndicator<t_scalar>>();
     break;
   }
   default: {
