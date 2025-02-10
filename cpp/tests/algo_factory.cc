b190f74cbe30ff8ce6be43baa01aedccea7b1142
Michael McLeod <michaelmcleod@ucl.ac.uk>
2025-02-10 16:00:28 +0000

    single core stochastic test

--- cpp/tests/algo_factory.cc
+++ cpp/tests/algo_factory.cc
@@ -17,6 +17,10 @@
 #include <sopt/onnx_differentiable_func.h>
 #endif
 
+#ifdef PURIFY_H5
+#include "purify/h5reader.h"
+#endif
+
 #include <sopt/power_method.h>
 
 #include "purify/test_data.h"
@@ -169,12 +173,118 @@ TEST_CASE("fb_factory") {
 
   auto const diagnostic = (*fb)();
   const Image<t_complex> image = Image<t_complex>::Map(diagnostic.x.data(), imsizey, imsizex);
-  // pfitsio::write2d(image.real(), result_path);
+  pfitsio::write2d(image.real(), result_path);
   // pfitsio::write2d(residual_image.real(), expected_residual_path);
 
   double average_intensity = diagnostic.x.real().sum() / diagnostic.x.size();
   SOPT_HIGH_LOG("Average intensity = {}", average_intensity);
   double mse = (Vector<t_complex>::Map(solution.data(), solution.size()) - diagnostic.x)
+                   .real()
+                   .squaredNorm() /
+               solution.size();
+  SOPT_HIGH_LOG("MSE = {}", mse);
+  CHECK(mse <= average_intensity * 1e-3);
+}
+
+TEST_CASE("fb_factory_stochastic") {
+  const std::string &test_dir = "expected/fb/";
+  const std::string &input_data_path = data_filename(test_dir + "input_data.vis");
+  const std::string &expected_solution_path = data_filename(test_dir + "solution.fits");
+  const std::string &expected_residual_path = data_filename(test_dir + "residual.fits");
+  const std::string &result_path = data_filename(test_dir + "fb_result_stochastic.fits");
+
+  auto uv_data = utilities::read_visibility(input_data_path, false);
+  uv_data.units = utilities::vis_units::radians;
+  CAPTURE(uv_data.vis.head(5));
+  REQUIRE(uv_data.size() == 13107);
+
+  t_uint const imsizey = 128;
+  t_uint const imsizex = 128;
+
+  // This functor would be defined in Purify
+  std::mt19937 rng(0);
+  const size_t N = 1000;
+  std::function<std::shared_ptr<sopt::IterationState<Vector<t_complex>>>()> random_updater = [&input_data_path, imsizex, imsizey, &rng, &N]() {
+    utilities::vis_params uv_data = utilities::read_visibility(input_data_path, false);
+    uv_data.units = utilities::vis_units::radians;
+    
+    // Get random subset
+    std::vector<size_t> indices(uv_data.size());
+    size_t i = 0;
+    for(auto &x : indices)
+    {
+        x = i++;
+    }
+
+    std::shuffle(indices.begin(), indices.end(), rng);
+    Vector<t_real> u_fragment(N);
+    Vector<t_real> v_fragment(N);
+    Vector<t_real> w_fragment(N);
+    Vector<t_complex> vis_fragment(N);
+    Vector<t_complex> weights_fragment(N);
+    for(i = 0; i < N; i++)
+    {
+        size_t j = indices[i];
+        u_fragment[i] = uv_data.u[j];
+        v_fragment[i] = uv_data.v[j];
+        w_fragment[i] = uv_data.w[j];
+        vis_fragment[i] = uv_data.vis[j];
+        weights_fragment[i] = uv_data.weights[j];
+    }
+    utilities::vis_params uv_data_fragment(u_fragment, v_fragment, w_fragment, vis_fragment, weights_fragment, uv_data.units, uv_data.ra, uv_data.dec, uv_data.average_frequency);
+    
+    auto phi = factory::measurement_operator_factory<Vector<t_complex>>(
+      factory::distributed_measurement_operator::serial, uv_data_fragment, imsizey, imsizex, 1, 1, 2,
+      kernels::kernel_from_string.at("kb"), 4, 4);
+
+    return std::make_shared<sopt::IterationState<Vector<t_complex>>>(uv_data_fragment.vis, phi);
+  };
+
+  Vector<t_complex> const init = Vector<t_complex>::Ones(imsizex * imsizey);
+  auto const measurements_transform = factory::measurement_operator_factory<Vector<t_complex>>(
+      factory::distributed_measurement_operator::serial, uv_data, imsizey, imsizex, 1, 1, 2,
+      kernels::kernel_from_string.at("kb"), 4, 4);
+  auto const power_method_stuff =
+      sopt::algorithm::power_method<Vector<t_complex>>(*measurements_transform, 1000, 1e-5, init);
+  const t_real op_norm = std::get<0>(power_method_stuff);
+
+  auto IS = random_updater();
+  auto Phi = IS->Phi();
+  auto const power_method_stuff2 =
+      sopt::algorithm::power_method<Vector<t_complex>>(Phi, 1000, 1e-5, init);
+  const t_real op_norm2 = std::get<0>(power_method_stuff2);
+
+  const auto solution = pfitsio::read2d(expected_solution_path);
+
+  //wavelets
+  std::vector<std::tuple<std::string, t_uint>> const sara{
+      std::make_tuple("Dirac", 3u), std::make_tuple("DB1", 3u), std::make_tuple("DB2", 3u),
+      std::make_tuple("DB3", 3u),   std::make_tuple("DB4", 3u), std::make_tuple("DB5", 3u),
+      std::make_tuple("DB6", 3u),   std::make_tuple("DB7", 3u), std::make_tuple("DB8", 3u)};
+  auto const wavelets = factory::wavelet_operator_factory<Vector<t_complex>>(
+      factory::distributed_wavelet_operator::serial, sara, imsizey, imsizex);
+  
+  //algorithm
+  t_real const sigma = 0.016820222945913496 * std::sqrt(2);  // see test_parameters file
+  t_real const beta = sigma * sigma;
+  t_real const gamma = 0.0001;
+
+  sopt::algorithm::ImagingForwardBackward<t_complex> fb(random_updater);
+  fb.itermax(1000).step_size(beta*sqrt(2)).sigma(sigma*sqrt(2)).regulariser_strength(gamma).relative_variation(1e-3).residual_tolerance(0).tight_frame(true).sq_op_norm(op_norm*op_norm);
+
+  auto gp = std::make_shared<sopt::algorithm::L1GProximal<t_complex>>(false);
+  gp->l1_proximal_tolerance(1e-4).l1_proximal_nu(1).l1_proximal_itermax(50).l1_proximal_positivity_constraint(true).l1_proximal_real_constraint(true).Psi(*wavelets);
+  fb.g_function(gp);
+
+  auto const diagnostic = fb();
+  const Image<t_complex> image = Image<t_complex>::Map(diagnostic.x.data(), imsizey, imsizex);
+  pfitsio::write2d(image.real(), result_path);
+  //pfitsio::write2d(residual_image.real(), expected_residual_path);
+
+  auto soln_flat = Vector<t_complex>::Map(solution.data(), solution.size());
+  double average_intensity = soln_flat.real().sum() / soln_flat.size();
+  SOPT_HIGH_LOG("Average intensity = {}", average_intensity);
+  double mse = (soln_flat - diagnostic.x)
                    .real()
                    .squaredNorm() /
                solution.size();
