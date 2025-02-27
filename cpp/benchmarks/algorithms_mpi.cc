24092be156348e9bf40d9434d320eee6cb1f7bdf
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-02-27 13:01:08 +0000

    Fix nondiff_func_type

--- cpp/benchmarks/algorithms_mpi.cc
+++ cpp/benchmarks/algorithms_mpi.cc
@@ -216,7 +216,7 @@ BENCHMARK_DEFINE_F(AlgoFixtureMPI, FbOnnxDistributeGrid)(benchmark::State &state
   m_fb = factory::fb_factory<sopt::algorithm::ImagingForwardBackward<t_complex>>(
       factory::algo_distribution::mpi_serial, m_measurements_distribute_grid, wavelets, m_uv_data,
       m_sigma, beta, gamma, m_imsizey, m_imsizex, m_sara.size(), state.range(3), true, true, false,
-      1e-3, 1e-2, 50, 1.0, tf_model_path, factory::g_proximal_type::TFGProximal);
+      1e-3, 1e-2, 50, 1.0, tf_model_path, nondiff_func_type::Denoiser);
 
   // Benchmark the application of the algorithm
   while (state.KeepRunning()) {
